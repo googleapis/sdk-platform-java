96db72a6e4c818ad463a539d7a9960ca290a4734
aozarov <ozarov@google.com>
2015-11-10 09:09:24 -0800

    scope appliation default credentials

--- java-core/google-cloud-core/src/main/java/com/google/gcloud/AuthCredentials.java
+++ java-core/google-cloud-core/src/main/java/com/google/gcloud/AuthCredentials.java
@@ -244,7 +244,7 @@ public abstract class AuthCredentials implements Restorable<AuthCredentials> {
     @Override
     protected HttpRequestInitializer httpRequestInitializer(HttpTransport transport,
         Set<String> scopes) {
-      return new HttpCredentialsAdapter(googleCredentials);
+      return new HttpCredentialsAdapter(googleCredentials.createScoped(scopes));
     }
 
     @Override
