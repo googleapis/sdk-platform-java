ae23e357caad7f1bd5a535eddeb02462aba564b4
Mira Leung <miraleung@google.com>
2020-05-22 00:12:09 +0000

    feat: add invalid symbol representation

--- src/main/java/com/google/api/generator/engine/lexicon/Operator.java
+++ src/main/java/com/google/api/generator/engine/lexicon/Operator.java
@@ -1,14 +1,10 @@
 package com.google.api.generator.engine.lexicon;
 
-import com.google.common.collect.ImmutableList;
 import java.util.regex.Pattern;
 
 // TODO(miraleung): Add logical representations for each operator so we can use them in expression
 // parsing.
 public class Operator {
-  private static final ImmutableList<String> OPERATORS =
-      ImmutableList.of("+", "-", "*", "/", "%", "=", "!", "&", "|", "<", ">", "?", ":", "^");
-
   private static final Pattern OPERATOR_PATTERN =
       Pattern.compile("[\\+\\-\\*\\/%\\=\\!\\&\\|<>\\?\\:\\^]");
 
