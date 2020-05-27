28b24876f31905787ae3ab599fce82103992ad0a
Mira Leung <miraleung@google.com>
2020-05-27 20:32:54 +0000

    feat: add identifiers

--- src/main/java/com/google/api/generator/engine/lexicon/InvalidSymbol.java
+++ src/main/java/com/google/api/generator/engine/lexicon/InvalidSymbol.java
@@ -3,7 +3,7 @@ package com.google.api.generator.engine.lexicon;
 import java.util.regex.Pattern;
 
 public class InvalidSymbol {
-  private static final Pattern INVALID_SYMBOL_PATTERN = Pattern.compile("[#`~'\"\\\\]");
+  private static final Pattern INVALID_SYMBOL_PATTERN = Pattern.compile("[#`~'\"\\\\,$]");
 
   public static boolean containsInvalidSymbol(String str) {
     return INVALID_SYMBOL_PATTERN.matcher(str).find();
