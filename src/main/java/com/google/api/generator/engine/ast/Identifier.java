0790896438a73c640bda8a8fd1be5d4bfe8b722a
Mira Leung <miraleung@google.com>
2020-06-12 00:49:22 +0000

    chore: convert Identifier to AutoValue

--- src/main/java/com/google/api/generator/engine/ast/Identifier.java
+++ src/main/java/com/google/api/generator/engine/ast/Identifier.java
@@ -19,10 +19,12 @@ import com.google.api.generator.engine.lexicon.Keyword;
 import com.google.api.generator.engine.lexicon.Literal;
 import com.google.api.generator.engine.lexicon.Operator;
 import com.google.api.generator.engine.lexicon.Separator;
+import com.google.auto.value.AutoValue;
+import com.google.common.base.Preconditions;
 import java.util.regex.Pattern;
-import javax.validation.constraints.NotNull;
 
-public class Identifier implements AstNode {
+@AutoValue
+public abstract class Identifier implements AstNode {
   static class InvalidIdentifierException extends RuntimeException {
     public InvalidIdentifierException(String errorMessage) {
       super(errorMessage);
@@ -31,44 +33,10 @@ public class Identifier implements AstNode {
 
   private static final Pattern NUMERIC_PATTERN = Pattern.compile("^[0-9]");
 
-  private final String identifierName;
+  public abstract String name();
 
-  private Identifier(String identifierName) {
-    this.identifierName = identifierName;
-  }
-
-  public static Identifier create(@NotNull String identifierName)
-      throws InvalidIdentifierException {
-    if (identifierName.isEmpty()) {
-      throw new InvalidIdentifierException("Name cannot be empty.");
-    }
-
-    if (NUMERIC_PATTERN.matcher(identifierName.substring(0, 1)).matches()) {
-      throw new InvalidIdentifierException("Name cannot begin with a number.");
-    }
-
-    if (Literal.isLiteral(identifierName)) {
-      throw new InvalidIdentifierException(
-          String.format("Name %s cannot be a literal", identifierName));
-    }
-
-    if (InvalidSymbol.containsInvalidSymbol(identifierName)
-        || Operator.containsOperator(identifierName)
-        || Separator.containsSeparator(identifierName)) {
-      throw new InvalidIdentifierException(
-          String.format("Name %s cannot contain non-alphanumeric characters", identifierName));
-    }
-
-    if (Keyword.isKeyword(identifierName)) {
-      throw new InvalidIdentifierException(
-          String.format("Name %s cannot be a keyword.", identifierName));
-    }
-
-    return new Identifier(identifierName);
-  }
-
-  public String name() {
-    return identifierName;
+  public static Builder builder() {
+    return new AutoValue_Identifier.Builder();
   }
 
   @Override
@@ -78,6 +46,46 @@ public class Identifier implements AstNode {
 
   @Override
   public String toString() {
-    return identifierName;
+    return name();
+  }
+
+  @AutoValue.Builder
+  public abstract static class Builder {
+    public abstract Builder setName(String name);
+
+    abstract Identifier autoBuild();
+
+    public Identifier build() throws InvalidIdentifierException {
+      Identifier identifier = autoBuild();
+      String identifierName = identifier.name();
+      Preconditions.checkNotNull(identifierName);
+
+      if (identifierName.isEmpty()) {
+        throw new InvalidIdentifierException("Name cannot be empty.");
+      }
+
+      if (NUMERIC_PATTERN.matcher(identifierName.substring(0, 1)).matches()) {
+        throw new InvalidIdentifierException("Name cannot begin with a number.");
+      }
+
+      if (Literal.isLiteral(identifierName)) {
+        throw new InvalidIdentifierException(
+            String.format("Name %s cannot be a literal", identifierName));
+      }
+
+      if (InvalidSymbol.containsInvalidSymbol(identifierName)
+          || Operator.containsOperator(identifierName)
+          || Separator.containsSeparator(identifierName)) {
+        throw new InvalidIdentifierException(
+            String.format("Name %s cannot contain non-alphanumeric characters", identifierName));
+      }
+
+      if (Keyword.isKeyword(identifierName)) {
+        throw new InvalidIdentifierException(
+            String.format("Name %s cannot be a keyword.", identifierName));
+      }
+
+      return identifier;
+    }
   }
 }
