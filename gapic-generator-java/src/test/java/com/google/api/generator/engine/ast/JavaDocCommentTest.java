9cb0b8542d8ee54d6cbb13d755cad4b73a28394d
xiaozhenliugg <xiaozhenliu@google.com>
2020-06-25 23:40:11 -0700

    clean

--- gapic-generator-java/src/test/java/com/google/api/generator/engine/ast/JavaDocCommentTest.java
+++ gapic-generator-java/src/test/java/com/google/api/generator/engine/ast/JavaDocCommentTest.java
@@ -14,65 +14,55 @@
 
 package com.google.api.generator.engine.ast;
 
+import static com.google.common.truth.Truth.assertThat;
+
+import java.util.Arrays;
+import java.util.List;
+import org.junit.Test;
+
 public class JavaDocCommentTest {
-    @Test
-    public void simpleJavaDocComment(){
-      String content = "this is a test comment";
-      String deprecatedText = "Use the {@link ArchivedBookName} class instead.";
-      String paramName = "shelfName";
-      String paramDescription =  "The name of the shelf where books are published to.";
-      String htmlP1 = "This class provides the ability to make remote calls to the backing service through method calls that map to API methods. Sample code to get started:";
-      String htmlP2 = "The surface of this class includes several types of Java methods for each of the API's methods:";
-  
-      TryCatchStatement tryCatch =
-          TryCatchStatement.builder()
-              .setTryResourceExpr(createAssignmentExpr("condition", "false", TypeNode.BOOLEAN))
-              .setTryBody(
-                  Arrays.asList(ExprStatement.withExpr(createAssignmentExpr("x", "3", TypeNode.INT))))
-              .setIsSampleCode(true)
-              .build();
-  
-      tryCatch.accept(writerVisitor);
-      String sampleCode = writerVisitor.write();
-      List<String> htmlList = Arrays.asList("A flattened method.", " A request object method.", "A callable method.");
-      String throwText = "com.google.api.gax.rpc.ApiException if the remote call fails.";
-      JavaDocComment javaDocComment =
-          JavaDocComment.builder()
-              .addComment(content)
-              .addParagraph(htmlP1)
-              .addSampleCode(sampleCode)
-              .addParagraph(htmlP2)
-              .addOrderedList(htmlList)
-              .addSampleCode(sampleCode)
-              .addParam(paramName, paramDescription)
-              .setDeprecated(deprecatedText)
-              .setThrowsText(throwText)
-              .build();
-      String expected =
-          "/**\n"
-              + "* this is a test comment\n"
-              + "* <p> This class provides the ability to make remote calls to the backing service through method calls that map to API methods. Sample code to get started:\n"
-              + "* <pre><code>\n"
-              + "* try (boolean condition = false) {\n"
-              + "* int x = 3;\n"
-              + "* }\n"
-              + "* </code></pre>\n"
-              + "* <p> The surface of this class includes several types of Java methods for each of the API's methods:\n"
-              + "* <ol>\n"
-              + "* <li> A flattened method.\n"
-              + "* <li>  A request object method.\n"
-              + "* <li> A callable method.\n"
-              + "* </ol>\n"
-              + "* <pre><code>\n"
-              + "* try (boolean condition = false) {\n"
-              + "* int x = 3;\n"
-              + "* }\n"
-              + "* </code></pre>\n"
-              + "* @param shelfName The name of the shelf where books are published to.\n"
-              + "* @deprecated Use the {@link ArchivedBookName} class instead.\n"
-              + "* @throws com.google.api.gax.rpc.ApiException if the remote call fails.\n"
-              + "*/\n";
-      String formattedComment = javaDocComment.accept(writerVisitor);
-      assertThat(formattedComment).isEqualTo(expected);
-    }
+  @Test
+  public void JavaDocComment_basic() {
+    String content = "this is a test comment";
+    JavaDocComment javaDocComment = JavaDocComment.builder().addComment(content).build();
+    assertThat(javaDocComment.comment()).isEqualTo(content);
+  }
+
+  @Test
+  public void simpleJavaDocComment() {
+    String content = "this is a test comment";
+    String deprecatedText = "Use the {@link ArchivedBookName} class instead.";
+    String paramName = "shelfName";
+    String paramDescription = "The name of the shelf where books are published to.";
+    String htmlP1 =
+        "This class provides the ability to make remote calls to the backing service through method calls that map to API methods. Sample code to get started:";
+    String htmlP2 =
+        "The surface of this class includes several types of Java methods for each of the API's methods:";
+    List<String> htmlList =
+        Arrays.asList("A flattened method.", "A request object method.", "A callable method.");
+    String throwText = "com.google.api.gax.rpc.ApiException if the remote call fails.";
+    JavaDocComment javaDocComment =
+        JavaDocComment.builder()
+            .addComment(content)
+            .addParagraph(htmlP1)
+            .addParagraph(htmlP2)
+            .addOrderedList(htmlList)
+            .addParam(paramName, paramDescription)
+            .setDeprecated(deprecatedText)
+            .setThrowsText(throwText)
+            .build();
+    String expected =
+        "this is a test comment\n"
+            + "<p> This class provides the ability to make remote calls to the backing service through method calls that map to API methods. Sample code to get started:\n"
+            + "<p> The surface of this class includes several types of Java methods for each of the API's methods:\n"
+            + "<ol>\n"
+            + "<li> A flattened method.\n"
+            + "<li> A request object method.\n"
+            + "<li> A callable method.\n"
+            + "</ol>\n"
+            + "@param shelfName The name of the shelf where books are published to.\n"
+            + "@deprecated Use the {@link ArchivedBookName} class instead.\n"
+            + "@throws com.google.api.gax.rpc.ApiException if the remote call fails.";
+    assertThat(javaDocComment.comment()).isEqualTo(expected);
+  }
 }
