2c5ece564191359e2ce66aa74f40bd88e641cc46
Mira Leung <miraleung@google.com>
2020-07-11 18:41:03 -0700

    feat: add streaming parsing and proto test starters

---
+++ gapic-generator-java/src/test/java/com/google/api/generator/gapic/protoparser/ParserTest.java
@@ -0,0 +1,105 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.google.api.generator.gapic.protoparser;
+
+import static com.google.common.truth.Truth.assertThat;
+import static junit.framework.Assert.assertEquals;
+
+import com.google.api.generator.engine.ast.TypeNode;
+import com.google.api.generator.engine.ast.VaporReference;
+import com.google.api.generator.gapic.model.Field;
+import com.google.api.generator.gapic.model.Message;
+import com.google.api.generator.gapic.model.Method;
+import com.google.protobuf.Descriptors.FileDescriptor;
+import com.google.protobuf.Descriptors.ServiceDescriptor;
+import com.google.showcase.v1beta1.EchoOuterClass;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ParserTest {
+  private static final String ECHO_PACKAGE = "com.google.showcase.v1beta1";
+  // TODO(miraleung): Backfill with more tests (e.g. field, message, methods) for Parser.java.
+  private ServiceDescriptor echoService;
+  private FileDescriptor echoFileDescriptor;
+
+  @Before
+  public void setUp() {
+    echoFileDescriptor = EchoOuterClass.getDescriptor();
+    echoService = echoFileDescriptor.getServices().get(0);
+    assertEquals(echoService.getName(), "Echo");
+  }
+
+  @Test
+  public void parseMessages_basic() {
+    // TODO(miraleung): Add more tests for oneofs and other message-parsing edge cases.
+    Map<String, Message> messageTypes = Parser.parseMessages(echoFileDescriptor);
+    String echoResponseName = "EchoResponse";
+    Field echoResponseContentField =
+        Field.builder().setName("content").setType(TypeNode.STRING).build();
+    Field echoResponseSeverityField =
+        Field.builder()
+            .setName("severity")
+            .setType(
+                TypeNode.withReference(
+                    VaporReference.builder().setName("Severity").setPakkage(ECHO_PACKAGE).build()))
+            .build();
+    TypeNode echoResponseType =
+        TypeNode.withReference(
+            VaporReference.builder().setName(echoResponseName).setPakkage(ECHO_PACKAGE).build());
+
+    Message echoResponseMessage =
+        Message.builder()
+            .setType(echoResponseType)
+            .setName(echoResponseName)
+            .setFields(Arrays.asList(echoResponseContentField, echoResponseSeverityField))
+            .build();
+    assertThat(messageTypes.get(echoResponseName)).isEqualTo(echoResponseMessage);
+  }
+
+  @Test
+  public void parseMethods_basic() {
+    Map<String, Message> messageTypes = Parser.parseMessages(echoFileDescriptor);
+    List<Method> methods = Parser.parseMethods(echoService, messageTypes);
+    assertThat(methods.size()).isEqualTo(7);
+
+    // Methods should appear in the same order as in the protobuf file.
+    Method echoMethod = methods.get(0);
+    assertEquals(echoMethod.name(), "Echo");
+    assertEquals(echoMethod.stream(), Method.Stream.NONE);
+
+    Method expandMethod = methods.get(1);
+    assertEquals(expandMethod.name(), "Expand");
+    assertEquals(
+        expandMethod.inputType(),
+        TypeNode.withReference(
+            VaporReference.builder().setName("ExpandRequest").setPakkage(ECHO_PACKAGE).build()));
+    assertEquals(
+        expandMethod.outputType(),
+        TypeNode.withReference(
+            VaporReference.builder().setName("EchoResponse").setPakkage(ECHO_PACKAGE).build()));
+    assertEquals(expandMethod.stream(), Method.Stream.SERVER);
+
+    Method collectMethod = methods.get(2);
+    assertEquals(collectMethod.name(), "Collect");
+    assertEquals(collectMethod.stream(), Method.Stream.CLIENT);
+
+    Method chatMethod = methods.get(3);
+    assertEquals(chatMethod.name(), "Chat");
+    assertEquals(chatMethod.stream(), Method.Stream.BIDI);
+  }
+}
