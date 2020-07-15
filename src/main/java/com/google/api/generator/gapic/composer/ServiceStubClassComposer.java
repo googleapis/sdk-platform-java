5f02e5533267463366e67ca7eadacbc72fbcbcf5
Mira Leung <miraleung@google.com>
2020-07-15 13:06:12 -0700

    fix: add methodOperationCallable generation

--- src/main/java/com/google/api/generator/gapic/composer/ServiceStubClassComposer.java
+++ src/main/java/com/google/api/generator/gapic/composer/ServiceStubClassComposer.java
@@ -101,47 +101,66 @@ public class ServiceStubClassComposer implements ClassComposer {
 
   private static List<MethodDefinition> createCallableGetters(
       Service service, Map<String, Message> messageTypes, Map<String, TypeNode> types) {
-    return service.methods().stream()
-        .map(
-            method -> {
-              TypeNode returnType;
-              switch (method.stream()) {
-                case CLIENT:
-                  returnType = types.get("ClientStreamingCallable");
-                  break;
-                case SERVER:
-                  returnType = types.get("ServerStreamingCallable");
-                  break;
-                case BIDI:
-                  returnType = types.get("BidiStreamingCallable");
-                  break;
-                case NONE:
-                  // Fall through.
-                default:
-                  returnType = types.get("UnaryCallable");
-              }
+    // Use a traditional for-loop since the output cardinality is not necessarily 1:1 with that of
+    // service.methods().
+    List<MethodDefinition> javaMethods = new ArrayList<>();
+    for (Method method : service.methods()) {
+      if (method.hasLro()) {
+        javaMethods.add(createOperationCallableGetter(method, types));
+      }
+      javaMethods.add(createCallableGetter(method, types));
+    }
+    return javaMethods;
+  }
 
-              String methodName =
-                  String.format("%sCallable", JavaStyle.toLowerCamelCase(method.name()));
-              List<Reference> genericRefs = new ArrayList<>();
-              genericRefs.add(method.inputType().reference());
-              if (method.hasLro()) {
-                genericRefs.add(method.lro().responseType().reference());
-                genericRefs.add(method.lro().metadataType().reference());
-              } else {
-                genericRefs.add(method.outputType().reference());
-              }
-              returnType =
-                  TypeNode.withReference(returnType.reference().copyAndSetGenerics(genericRefs));
+  private static MethodDefinition createOperationCallableGetter(
+      Method method, Map<String, TypeNode> types) {
+    return createCallableGetterHelper(method, types, true);
+  }
 
-              return MethodDefinition.builder()
-                  .setScope(ScopeNode.PUBLIC)
-                  .setReturnType(returnType)
-                  .setName(methodName)
-                  .setBody(createThrowUOEBody(methodName, types))
-                  .build();
-            })
-        .collect(Collectors.toList());
+  private static MethodDefinition createCallableGetter(Method method, Map<String, TypeNode> types) {
+    return createCallableGetterHelper(method, types, false);
+  }
+
+  private static MethodDefinition createCallableGetterHelper(
+      Method method, Map<String, TypeNode> types, boolean isLroCallable) {
+    TypeNode returnType;
+    switch (method.stream()) {
+      case CLIENT:
+        returnType = types.get("ClientStreamingCallable");
+        break;
+      case SERVER:
+        returnType = types.get("ServerStreamingCallable");
+        break;
+      case BIDI:
+        returnType = types.get("BidiStreamingCallable");
+        break;
+      case NONE:
+        // Fall through.
+      default:
+        returnType = types.get(isLroCallable ? "OperationCallable" : "UnaryCallable");
+    }
+
+    String methodName =
+        String.format(
+            "%s%sCallable",
+            JavaStyle.toLowerCamelCase(method.name()), (isLroCallable ? "Operation" : ""));
+    List<Reference> genericRefs = new ArrayList<>();
+    genericRefs.add(method.inputType().reference());
+    if (method.hasLro()) {
+      genericRefs.add(method.lro().responseType().reference());
+      genericRefs.add(method.lro().metadataType().reference());
+    } else {
+      genericRefs.add(method.outputType().reference());
+    }
+    returnType = TypeNode.withReference(returnType.reference().copyAndSetGenerics(genericRefs));
+
+    return MethodDefinition.builder()
+        .setScope(ScopeNode.PUBLIC)
+        .setReturnType(returnType)
+        .setName(methodName)
+        .setBody(createThrowUOEBody(methodName, types))
+        .build();
   }
 
   private static MethodDefinition createOperationsStubGetter(Map<String, TypeNode> types) {
