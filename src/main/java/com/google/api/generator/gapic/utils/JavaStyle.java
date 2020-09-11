687a4871aa0b6439566d8e3bcd522827c7d36100
Mira Leung <miraleung@users.noreply.github.com>
2020-09-11 16:58:30 -0700

    fix: handle post-digit caps, add test, and robustify JavaStyle (#288)

--- src/main/java/com/google/api/generator/gapic/utils/JavaStyle.java
+++ src/main/java/com/google/api/generator/gapic/utils/JavaStyle.java
@@ -15,25 +15,50 @@
 package com.google.api.generator.gapic.utils;
 
 import com.google.common.base.CaseFormat;
+import com.google.common.base.Strings;
+import java.util.stream.IntStream;
 
 public class JavaStyle {
   private static final String UNDERSCORE = "_";
 
   public static String toLowerCamelCase(String s) {
+    if (Strings.isNullOrEmpty(s)) {
+      return s;
+    }
+
     if (s.indexOf(UNDERSCORE) >= 0) {
       s = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, s);
     }
-    return String.format("%s%s", s.substring(0, 1).toLowerCase(), s.substring(1));
+    return capitalizeLettersAfterDigits(
+        String.format("%s%s", s.substring(0, 1).toLowerCase(), s.substring(1)));
   }
 
   public static String toUpperCamelCase(String s) {
+    if (Strings.isNullOrEmpty(s)) {
+      return s;
+    }
+
     if (s.indexOf(UNDERSCORE) >= 0) {
       s = CaseFormat.LOWER_UNDERSCORE.to(CaseFormat.UPPER_CAMEL, s);
     }
-    return String.format("%s%s", s.substring(0, 1).toUpperCase(), s.substring(1));
+    return capitalizeLettersAfterDigits(
+        String.format("%s%s", s.substring(0, 1).toUpperCase(), s.substring(1)));
   }
 
   public static String toUpperSnakeCase(String s) {
     return CaseFormat.UPPER_CAMEL.to(CaseFormat.UPPER_UNDERSCORE, toUpperCamelCase(s));
+  }
+
+  private static String capitalizeLettersAfterDigits(String s) {
+    return IntStream.range(0, s.length())
+        .collect(
+            StringBuilder::new,
+            (sb, i) ->
+                sb.append(
+                    i > 0 && Character.isDigit(s.charAt(i - 1))
+                        ? Character.toUpperCase(s.charAt(i))
+                        : s.charAt(i)),
+            StringBuilder::append)
+        .toString();
   }
 }
