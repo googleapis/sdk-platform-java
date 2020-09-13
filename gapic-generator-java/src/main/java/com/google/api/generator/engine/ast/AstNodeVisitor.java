81c9b2dbb45e6f3e6f2fb2a6ffddc9edbc7de64a
Summer Ji <summerji@google.com>
2020-09-13 01:02:35 -0700

    [javanaut]feat: Add Logical operation expr and unit test (#293)
    * Add LogicalOperationExpr and unit test
    * Enable (&&) and (!!) operator

--- gapic-generator-java/src/main/java/com/google/api/generator/engine/ast/AstNodeVisitor.java
+++ gapic-generator-java/src/main/java/com/google/api/generator/engine/ast/AstNodeVisitor.java
@@ -55,6 +55,8 @@ public interface AstNodeVisitor {
 
   public void visit(UnaryOperationExpr unaryOperationExpr);
 
+  public void visit(LogicalOperationExpr logicalOperationExpr);
+
   /** =============================== COMMENT =============================== */
   public void visit(LineComment lineComment);
 
