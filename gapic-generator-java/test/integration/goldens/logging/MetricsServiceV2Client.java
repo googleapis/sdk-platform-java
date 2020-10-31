01564bb3b66f9e4d5da2243a5d6c72bd6e54dfe8
Mira Leung <miraleung@users.noreply.github.com>
2020-10-31 14:56:24 -0700

    [ggj][codegen] fix: use serviceClient varname in ServiceClient codegen comments (#445)
    * fix: support non-name fields with res-refs in resname def parsing
    
    * fix: add workaround for missing default_host and oauth_scopes annotation
    
    * fix: clarify LRO parsing error messages
    
    * feat: support deeply-nested types in AST and proto message parsing
    
    * fix: prevent resname tokens from matching subcomponents
    
    * fix: use TypeParser for proto message parsing
    
    * fix: use generic types in field instantiation in ServiceClientTest
    
    * fix: prevent descension into map types in nested message parsing
    
    * fix: merge master
    
    * fix: use both map generics in ServiceClientTest codegen
    
    * fix: dir structure of generated files
    
    * test: add asset API gradle pkg rules
    
    * fix: remove unused dep
    
    * test: add logging integration target and goldens, consolidate rules
    
    * fix: fix asset_java_gapic build
    
    * fix: fix test src packaging, update integration goldens
    
    * fix: pass all tokens to instansiate in resname 1-pattern case
    
    * fix: update goldens
    
    * fix: update goldens
    
    * build: add all integration tests to pre-commit hook
    
    * build: run pre-commit when goldens have changed
    
    * build: add integration golden tests to CircleCI
    
    * fix: preserve newlines and parse itemized lists in protobuf comments
    
    * fix: remove extraneous escaper
    
    * fix: update integration goldens for ServiceClient comment names
    
    * fix: use serviceClient varname in ServiceClient codegen comments

--- gapic-generator-java/test/integration/goldens/logging/MetricsServiceV2Client.java
+++ gapic-generator-java/test/integration/goldens/logging/MetricsServiceV2Client.java
@@ -43,8 +43,9 @@ import javax.annotation.Generated;
  * <p>This class provides the ability to make remote calls to the backing service through method
  * calls that map to API methods. Sample code to get started:
  *
- * <p>Note: close() needs to be called on the echoClient object to clean up resources such as
- * threads. In the example above, try-with-resources is used, which automatically calls close().
+ * <p>Note: close() needs to be called on the metricsServiceV2Client object to clean up resources
+ * such as threads. In the example above, try-with-resources is used, which automatically calls
+ * close().
  *
  * <p>The surface of this class includes several types of Java methods for each of the API's
  * methods:
