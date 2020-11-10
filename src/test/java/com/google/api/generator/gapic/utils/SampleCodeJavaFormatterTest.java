2bf804e24d8350d1e6e20c8d392c96b571560404
summerji <summerji@google.com>
2020-11-09 18:09:45 -0800

    Add unit test for long line

--- src/test/java/com/google/api/generator/gapic/utils/SampleCodeJavaFormatterTest.java
+++ src/test/java/com/google/api/generator/gapic/utils/SampleCodeJavaFormatterTest.java
@@ -5,15 +5,19 @@ import static junit.framework.TestCase.assertEquals;
 import com.google.api.generator.engine.ast.AssignmentExpr;
 import com.google.api.generator.engine.ast.Expr;
 import com.google.api.generator.engine.ast.ExprStatement;
+import com.google.api.generator.engine.ast.MethodInvocationExpr;
 import com.google.api.generator.engine.ast.PrimitiveValue;
 import com.google.api.generator.engine.ast.Statement;
 import com.google.api.generator.engine.ast.TryCatchStatement;
 import com.google.api.generator.engine.ast.TypeNode;
 import com.google.api.generator.engine.ast.Value;
 import com.google.api.generator.engine.ast.ValueExpr;
+import com.google.api.generator.engine.ast.VaporReference;
 import com.google.api.generator.engine.ast.Variable;
 import com.google.api.generator.engine.ast.VariableExpr;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import org.junit.Test;
 
 public class SampleCodeJavaFormatterTest {
@@ -23,12 +27,50 @@ public class SampleCodeJavaFormatterTest {
     String result = SampleCodeJavaFormatter.format(Arrays.asList(createSampleCode()));
     String expected =
         String.format(
-            createLines(3), "try (boolean condition = false) {\n", "  int x = 3;\n", "}\n");
-    assertEquals(result, expected);
+            createLines(3), "try (boolean condition = false) {\n", "  int x = 3;\n", "}");
+    assertEquals(expected, result);
   }
 
   @Test
-  public void formatLongLineStatement() {}
+  public void formatLongLineStatement() {
+    TypeNode type = TypeNode.withReference(
+        VaporReference.builder()
+            .setPakkage("com.google.pubsub.v1")
+            .setName("SubscriptionAdminSettings")
+            .build());
+    VariableExpr varDclExpr = createVariableDeclExpr("subscriptionAdminSettings", type);
+    VariableExpr varExpr = createVariableExpr(
+        "SubscriptionAdminSettings", type);
+    MethodInvocationExpr firstMethodExpr =
+        MethodInvocationExpr.builder()
+            .setMethodName("newBuilder")
+            .setExprReferenceExpr(varExpr)
+            .build();
+    MethodInvocationExpr secondMethodExpr =
+        MethodInvocationExpr.builder()
+            .setMethodName("setEndpoint")
+            .setExprReferenceExpr(firstMethodExpr)
+            .setArguments(Arrays.asList(createVariableExpr("myEndpoint", TypeNode.STRING)))
+            .build();
+    MethodInvocationExpr methodExpr =
+        MethodInvocationExpr.builder()
+            .setMethodName("build")
+            .setExprReferenceExpr(secondMethodExpr)
+            .setReturnType(type)
+            .build();
+    List<Statement> statements =
+        Arrays.asList(
+            ExprStatement.withExpr(
+                AssignmentExpr.builder().setVariableExpr(varDclExpr).setValueExpr(methodExpr).build())
+        );
+    String result = SampleCodeJavaFormatter.format(statements);
+    String expected =
+        String.format(
+            createLines(2),
+            "SubscriptionAdminSettings subscriptionAdminSettings =\n",
+            "    SubscriptionAdminSettings.newBuilder().setEndpoint(myEndpoint).build();\n");
+    assertEquals(expected, result);
+  }
 
   /** =============================== HELPERS =============================== */
   private static String createLines(int numLines) {
