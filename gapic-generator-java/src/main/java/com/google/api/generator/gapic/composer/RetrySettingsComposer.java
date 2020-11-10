bcfc7280f3704581aaec00736ad229d894052a3d
Mira Leung <miraleung@users.noreply.github.com>
2020-11-10 13:11:42 -0800

    [ggj][codegen][lro] fix: update LRO values to new proposed values (#472)
    * fix: update LRO values to new proposed values
    
    * fix: update tests

--- gapic-generator-java/src/main/java/com/google/api/generator/gapic/composer/RetrySettingsComposer.java
+++ gapic-generator-java/src/main/java/com/google/api/generator/gapic/composer/RetrySettingsComposer.java
@@ -63,12 +63,11 @@ public class RetrySettingsComposer {
   private static final TypeNode STATUS_CODE_CODE_TYPE =
       TypeNode.withReference(ConcreteReference.withClazz(StatusCode.Code.class));
 
-  // TODO(miraleung): Determine defaults here.
   // Default values for LongRunningConfig fields.
-  private static final long LRO_DEFAULT_INITIAL_POLL_DELAY_MILLIS = 500;
+  private static final long LRO_DEFAULT_INITIAL_POLL_DELAY_MILLIS = 20000;
   private static final double LRO_DEFAULT_POLL_DELAY_MULTIPLIER = 1.5;
-  private static final long LRO_DEFAULT_MAX_POLL_DELAY_MILLIS = 5000;
-  private static final long LRO_DEFAULT_TOTAL_POLL_TIMEOUT_MILLS = 300000;
+  private static final long LRO_DEFAULT_MAX_POLL_DELAY_MILLIS = 45000;
+  private static final long LRO_DEFAULT_TOTAL_POLL_TIMEOUT_MILLS = 86400000; // 24 hours.
   private static final double LRO_DEFAULT_MAX_RPC_TIMEOUT = 1.0;
 
   public static BlockStatement createRetryParamDefinitionsBlock(
