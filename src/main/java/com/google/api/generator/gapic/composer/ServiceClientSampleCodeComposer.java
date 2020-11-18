0bca2823fb805d1437289b112a47f467bb9731de
summerji <summerji@google.com>
2020-11-18 00:41:34 -0800

    use utils samplecodewriter

--- src/main/java/com/google/api/generator/gapic/composer/ServiceClientSampleCodeComposer.java
+++ src/main/java/com/google/api/generator/gapic/composer/ServiceClientSampleCodeComposer.java
@@ -20,18 +20,15 @@ import com.google.api.generator.engine.ast.ConcreteReference;
 import com.google.api.generator.engine.ast.Expr;
 import com.google.api.generator.engine.ast.ExprStatement;
 import com.google.api.generator.engine.ast.MethodInvocationExpr;
-import com.google.api.generator.engine.ast.Statement;
 import com.google.api.generator.engine.ast.StringObjectValue;
 import com.google.api.generator.engine.ast.TypeNode;
 import com.google.api.generator.engine.ast.ValueExpr;
 import com.google.api.generator.engine.ast.Variable;
 import com.google.api.generator.engine.ast.VariableExpr;
 import com.google.api.generator.engine.writer.JavaWriterVisitor;
-import com.google.api.generator.gapic.composer.samplecode.SampleCodeJavaFormatter;
+import com.google.api.generator.gapic.composer.samplecode.SampleCodeWriter;
 import com.google.api.generator.gapic.utils.JavaStyle;
 import java.util.Arrays;
-import java.util.List;
-import java.util.stream.Collectors;
 
 public class ServiceClientSampleCodeComposer {
   // TODO(summerji): Add unit tests for ServiceClientSampleCodeComposer.
@@ -91,21 +88,13 @@ public class ServiceClientSampleCodeComposer {
             .setValueExpr(createMethodExpr)
             .build();
 
-    return writeSampleCode(Arrays.asList(initSettingsVarExpr, initClientVarExpr));
+    return SampleCodeWriter.write(
+        Arrays.asList(
+            ExprStatement.withExpr(initSettingsVarExpr),
+            ExprStatement.withExpr(initClientVarExpr)));
   }
 
   // ======================================== Helpers ==========================================//
-  // TODO(summerji): Use writeSampleCode method in new class once PR#499 merged.
-  private static String writeSampleCode(List<Expr> exprs) {
-    List<Statement> statements =
-        exprs.stream().map(e -> ExprStatement.withExpr(e)).collect(Collectors.toList());
-    JavaWriterVisitor visitor = new JavaWriterVisitor();
-    for (Statement statement : statements) {
-      statement.accept(visitor);
-    }
-    return SampleCodeJavaFormatter.format(visitor.write());
-  }
-
   private static VariableExpr createVariableExpr(String variableName, TypeNode type) {
     return VariableExpr.withVariable(
         Variable.builder().setName(variableName).setType(type).build());
