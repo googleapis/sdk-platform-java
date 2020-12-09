f4ec8418c9f2e33130d4679810421f898c7dce56
summerji <summerji@google.com>
2020-12-08 23:53:51 -0800

    rebase master

--- test/integration/goldens/logging/MetricsClient.java
+++ test/integration/goldens/logging/MetricsClient.java
@@ -223,7 +223,7 @@ public class MetricsClient implements BackgroundResource {
    * }
    * }</pre>
    *
-   * @param metric_name Required. The resource name of the desired metric:
+   * @param metricName Required. The resource name of the desired metric:
    *     <p>"projects/[PROJECT_ID]/metrics/[METRIC_ID]"
    * @throws com.google.api.gax.rpc.ApiException if the remote call fails
    */
@@ -248,7 +248,7 @@ public class MetricsClient implements BackgroundResource {
    * }
    * }</pre>
    *
-   * @param metric_name Required. The resource name of the desired metric:
+   * @param metricName Required. The resource name of the desired metric:
    *     <p>"projects/[PROJECT_ID]/metrics/[METRIC_ID]"
    * @throws com.google.api.gax.rpc.ApiException if the remote call fails
    */
@@ -371,7 +371,7 @@ public class MetricsClient implements BackgroundResource {
    * }
    * }</pre>
    *
-   * @param metric_name Required. The resource name of the metric to update:
+   * @param metricName Required. The resource name of the metric to update:
    *     <p>"projects/[PROJECT_ID]/metrics/[METRIC_ID]"
    *     <p>The updated metric must be provided in the request and it's `name` field must be the
    *     same as `[METRIC_ID]` If the metric does not exist in `[PROJECT_ID]`, then a new metric is
@@ -402,7 +402,7 @@ public class MetricsClient implements BackgroundResource {
    * }
    * }</pre>
    *
-   * @param metric_name Required. The resource name of the metric to update:
+   * @param metricName Required. The resource name of the metric to update:
    *     <p>"projects/[PROJECT_ID]/metrics/[METRIC_ID]"
    *     <p>The updated metric must be provided in the request and it's `name` field must be the
    *     same as `[METRIC_ID]` If the metric does not exist in `[PROJECT_ID]`, then a new metric is
@@ -450,7 +450,7 @@ public class MetricsClient implements BackgroundResource {
    * }
    * }</pre>
    *
-   * @param metric_name Required. The resource name of the metric to delete:
+   * @param metricName Required. The resource name of the metric to delete:
    *     <p>"projects/[PROJECT_ID]/metrics/[METRIC_ID]"
    * @throws com.google.api.gax.rpc.ApiException if the remote call fails
    */
@@ -475,7 +475,7 @@ public class MetricsClient implements BackgroundResource {
    * }
    * }</pre>
    *
-   * @param metric_name Required. The resource name of the metric to delete:
+   * @param metricName Required. The resource name of the metric to delete:
    *     <p>"projects/[PROJECT_ID]/metrics/[METRIC_ID]"
    * @throws com.google.api.gax.rpc.ApiException if the remote call fails
    */
