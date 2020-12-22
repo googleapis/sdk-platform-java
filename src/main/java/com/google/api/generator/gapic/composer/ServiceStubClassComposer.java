3a428a9c8d556d96b22ef7521b8610bcff90cf19
Mira Leung <miraleung@google.com>
2020-12-22 11:32:47 -0800

    fix: merge master

--- src/main/java/com/google/api/generator/gapic/composer/ServiceStubClassComposer.java
+++ src/main/java/com/google/api/generator/gapic/composer/ServiceStubClassComposer.java
@@ -39,7 +39,6 @@ import com.google.api.generator.gapic.utils.JavaStyle;
 import com.google.longrunning.Operation;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
