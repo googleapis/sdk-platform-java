6ba223a227aad613aa8730fea7c908053dc710fc
summerji <summerji@google.com>
2021-01-08 15:37:51 -0800

    Implement unary callable sample code

--- src/main/java/com/google/api/generator/gapic/composer/samplecode/ServiceClientSampleCodeComposer.java
+++ src/main/java/com/google/api/generator/gapic/composer/samplecode/ServiceClientSampleCodeComposer.java
@@ -46,6 +46,7 @@ import com.google.api.generator.gapic.model.MethodArgument;
 import com.google.api.generator.gapic.model.ResourceName;
 import com.google.api.generator.gapic.utils.JavaStyle;
 import com.google.common.base.Preconditions;
+import com.google.longrunning.Operation;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -621,6 +622,113 @@ public class ServiceClientSampleCodeComposer {
       bodyStatements.addAll(
           composeStreamClientSampleCodeBodyStatements(
               method, clientVarExpr, requestAssignmentExpr));
+    }
+
+    return SampleCodeWriter.write(
+        TryCatchStatement.builder()
+            .setTryResourceExpr(assignClientVariableWithCreateMethodExpr(clientVarExpr))
+            .setTryBody(bodyStatements)
+            .setIsSampleCode(true)
+            .build());
+  }
+
+  public static String composeRegularCallableMethodHeaderSampleCode(
+      Method method,
+      TypeNode clientType,
+      Map<String, ResourceName> resourceNames,
+      Map<String, Message> messageTypes) {
+    VariableExpr clientVarExpr =
+        VariableExpr.withVariable(
+            Variable.builder()
+                .setName(JavaStyle.toLowerCamelCase(clientType.reference().name()))
+                .setType(clientType)
+                .build());
+
+    // Assign method's request variable with the default value.
+    VariableExpr requestVarExpr =
+        VariableExpr.withVariable(
+            Variable.builder().setName("request").setType(method.inputType()).build());
+    Message requestMessage = messageTypes.get(method.inputType().reference().simpleName());
+    Preconditions.checkNotNull(
+        requestMessage,
+        String.format(
+            "Could not find the message type %s.", method.inputType().reference().simpleName()));
+    Expr requestBuilderExpr =
+        DefaultValueComposer.createSimpleMessageBuilderExpr(
+            requestMessage, resourceNames, messageTypes);
+    AssignmentExpr requestAssignmentExpr =
+        AssignmentExpr.builder()
+            .setVariableExpr(requestVarExpr.toBuilder().setIsDecl(true).build())
+            .setValueExpr(requestBuilderExpr)
+            .build();
+
+    List<Expr> bodyExprs = new ArrayList<>();
+    bodyExprs.add(requestAssignmentExpr);
+
+    List<Statement> bodyStatements = new ArrayList<>();
+
+    if (!method.isPaged() && !method.hasLro()) {
+      // Create api future variable expression, and assign it with a value by invoking callable
+      // method.
+      // e.g. ApiFuture<EchoResponse> future = echoClient.echoCallable().futureCall(request);
+      TypeNode apiFutureType =
+          TypeNode.withReference(
+              ConcreteReference.builder()
+                  .setClazz(ApiFuture.class)
+                  .setGenerics(
+                      method.hasLro()
+                          ? ConcreteReference.withClazz(Operation.class)
+                          : method.outputType().reference())
+                  .build());
+      VariableExpr apiFutureVarExpr =
+          VariableExpr.withVariable(
+              Variable.builder().setName("future").setType(apiFutureType).build());
+      MethodInvocationExpr callableMethodInvocationExpr =
+          MethodInvocationExpr.builder()
+              .setExprReferenceExpr(clientVarExpr)
+              .setMethodName(JavaStyle.toLowerCamelCase(String.format("%sCallable", method.name())))
+              .build();
+      callableMethodInvocationExpr =
+          MethodInvocationExpr.builder()
+              .setExprReferenceExpr(callableMethodInvocationExpr)
+              .setMethodName("futureCall")
+              .setArguments(requestVarExpr)
+              .setReturnType(apiFutureType)
+              .build();
+      AssignmentExpr futureAssignmentExpr =
+          AssignmentExpr.builder()
+              .setVariableExpr(apiFutureVarExpr.toBuilder().setIsDecl(true).build())
+              .setValueExpr(callableMethodInvocationExpr)
+              .build();
+      bodyExprs.add(futureAssignmentExpr);
+      bodyStatements.addAll(
+          bodyExprs.stream().map(e -> ExprStatement.withExpr(e)).collect(Collectors.toList()));
+      bodyExprs.clear();
+      bodyStatements.add(CommentStatement.withComment(LineComment.withComment("Do something.")));
+
+      MethodInvocationExpr getMethodInvocationExpr =
+          MethodInvocationExpr.builder()
+              .setExprReferenceExpr(apiFutureVarExpr)
+              .setMethodName("get")
+              .setReturnType(method.outputType())
+              .build();
+      boolean returnVoid = isProtoEmptyType(method.outputType());
+      if (returnVoid) {
+        bodyStatements.add(ExprStatement.withExpr(getMethodInvocationExpr));
+      } else {
+        VariableExpr responseVarExpr =
+            VariableExpr.builder()
+                .setVariable(
+                    Variable.builder().setType(method.outputType()).setName("response").build())
+                .setIsDecl(true)
+                .build();
+        AssignmentExpr responseAssignmentExpr =
+            AssignmentExpr.builder()
+                .setVariableExpr(responseVarExpr)
+                .setValueExpr(getMethodInvocationExpr)
+                .build();
+        bodyStatements.add(ExprStatement.withExpr(responseAssignmentExpr));
+      }
     }
 
     return SampleCodeWriter.write(
