2808ba761956e1992fa04b7cbfcce36bb354d0c6
summerji <summerji@google.com>
2021-01-08 23:05:49 -0800

    add return void for lro

--- src/main/java/com/google/api/generator/gapic/composer/samplecode/ServiceClientSampleCodeComposer.java
+++ src/main/java/com/google/api/generator/gapic/composer/samplecode/ServiceClientSampleCodeComposer.java
@@ -620,8 +620,6 @@ public class ServiceClientSampleCodeComposer {
     } else if (method.stream().equals(Stream.CLIENT)) {
       bodyStatements.addAll(
           composeStreamClientBodyStatements(method, clientVarExpr, requestAssignmentExpr));
-          composeStreamClientSampleCodeBodyStatements(
-              method, clientVarExpr, requestAssignmentExpr));
     }
 
     return SampleCodeWriter.write(
