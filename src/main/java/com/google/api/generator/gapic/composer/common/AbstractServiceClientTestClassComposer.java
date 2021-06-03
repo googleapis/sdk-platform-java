fe4f2c948724803484ae438cdf1d820cb7d87030
Mira Leung <miraleung@google.com>
2021-06-03 16:15:31 -0700

    fix: test

--- src/main/java/com/google/api/generator/gapic/composer/common/AbstractServiceClientTestClassComposer.java
+++ src/main/java/com/google/api/generator/gapic/composer/common/AbstractServiceClientTestClassComposer.java
@@ -166,6 +166,16 @@ public abstract class AbstractServiceClientTestClassComposer implements ClassCom
                             .build()))
             .collect(Collectors.toList()));
 
+    fieldDeclStatements.add(
+        CommentStatement.withComment(
+            LineComment.withComment(
+                "DEL: Num mocks: "
+                    + classMemberVarExprs.size()
+                    + ", "
+                    + classMemberVarExprs.values().stream()
+                        .map(v -> v.variable().identifier())
+                        .collect(Collectors.toList()))));
+
     fieldDeclStatements.addAll(
         classMemberVarExprs.values().stream()
             .filter(v -> !isMockVarExprFn.apply(v))
