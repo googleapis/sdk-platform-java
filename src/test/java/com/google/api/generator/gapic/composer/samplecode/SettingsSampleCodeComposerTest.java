b7d93993aed676668b224ddc7167cc70de6d15cc
Vadym Matsishevskyi <25311427+vam-google@users.noreply.github.com>
2021-06-15 21:56:14 -0700

    chore: fix formatting and formatting validation logic (#768)

--- src/test/java/com/google/api/generator/gapic/composer/samplecode/SettingsSampleCodeComposerTest.java
+++ src/test/java/com/google/api/generator/gapic/composer/samplecode/SettingsSampleCodeComposerTest.java
@@ -25,56 +25,68 @@ import org.junit.Test;
 public class SettingsSampleCodeComposerTest {
   @Test
   public void composeSettingsSampleCode_noMethods() {
-    TypeNode classType = TypeNode.withReference(VaporReference.builder()
-    .setName("EchoSettings")
-    .setPakkage("com.google.showcase.v1beta1")
-    .build());
-    Optional<String> results =  SettingsSampleCodeComposer.composeSampleCode(Optional.empty(), "EchoSettings", classType);
+    TypeNode classType =
+        TypeNode.withReference(
+            VaporReference.builder()
+                .setName("EchoSettings")
+                .setPakkage("com.google.showcase.v1beta1")
+                .build());
+    Optional<String> results =
+        SettingsSampleCodeComposer.composeSampleCode(Optional.empty(), "EchoSettings", classType);
     assertEquals(results, Optional.empty());
   }
 
   @Test
   public void composeSettingsSampleCode_serviceSettingsClass() {
-    TypeNode classType = TypeNode.withReference(VaporReference.builder()
-        .setName("EchoSettings")
-        .setPakkage("com.google.showcase.v1beta1")
-        .build());
-    Optional<String> results = SettingsSampleCodeComposer.composeSampleCode(Optional.of("Echo"), "EchoSettings", classType);
-    String expected = LineFormatter.lines(
-        "EchoSettings.Builder echoSettingsBuilder = EchoSettings.newBuilder();\n",
-        "echoSettingsBuilder\n",
-        "    .echoSettings()\n",
-        "    .setRetrySettings(\n",
-        "        echoSettingsBuilder\n",
-        "            .echoSettings()\n",
-        "            .getRetrySettings()\n",
-        "            .toBuilder()\n",
-        "            .setTotalTimeout(Duration.ofSeconds(30))\n",
-        "            .build());\n",
-        "EchoSettings echoSettings = echoSettingsBuilder.build();");
+    TypeNode classType =
+        TypeNode.withReference(
+            VaporReference.builder()
+                .setName("EchoSettings")
+                .setPakkage("com.google.showcase.v1beta1")
+                .build());
+    Optional<String> results =
+        SettingsSampleCodeComposer.composeSampleCode(
+            Optional.of("Echo"), "EchoSettings", classType);
+    String expected =
+        LineFormatter.lines(
+            "EchoSettings.Builder echoSettingsBuilder = EchoSettings.newBuilder();\n",
+            "echoSettingsBuilder\n",
+            "    .echoSettings()\n",
+            "    .setRetrySettings(\n",
+            "        echoSettingsBuilder\n",
+            "            .echoSettings()\n",
+            "            .getRetrySettings()\n",
+            "            .toBuilder()\n",
+            "            .setTotalTimeout(Duration.ofSeconds(30))\n",
+            "            .build());\n",
+            "EchoSettings echoSettings = echoSettingsBuilder.build();");
     assertEquals(results.get(), expected);
   }
 
   @Test
   public void composeSettingsSampleCode_serviceStubClass() {
-    TypeNode classType = TypeNode.withReference(VaporReference.builder()
-        .setName("EchoStubSettings")
-        .setPakkage("com.google.showcase.v1beta1")
-        .build());
-    Optional<String> results = SettingsSampleCodeComposer.composeSampleCode(Optional.of("Echo"), "EchoSettings", classType);
-    String expected = LineFormatter.lines(
-        "EchoStubSettings.Builder echoSettingsBuilder = EchoStubSettings.newBuilder();\n",
-        "echoSettingsBuilder\n",
-        "    .echoSettings()\n",
-        "    .setRetrySettings(\n",
-        "        echoSettingsBuilder\n",
-        "            .echoSettings()\n",
-        "            .getRetrySettings()\n",
-        "            .toBuilder()\n",
-        "            .setTotalTimeout(Duration.ofSeconds(30))\n",
-        "            .build());\n",
-        "EchoStubSettings echoSettings = echoSettingsBuilder.build();");
+    TypeNode classType =
+        TypeNode.withReference(
+            VaporReference.builder()
+                .setName("EchoStubSettings")
+                .setPakkage("com.google.showcase.v1beta1")
+                .build());
+    Optional<String> results =
+        SettingsSampleCodeComposer.composeSampleCode(
+            Optional.of("Echo"), "EchoSettings", classType);
+    String expected =
+        LineFormatter.lines(
+            "EchoStubSettings.Builder echoSettingsBuilder = EchoStubSettings.newBuilder();\n",
+            "echoSettingsBuilder\n",
+            "    .echoSettings()\n",
+            "    .setRetrySettings(\n",
+            "        echoSettingsBuilder\n",
+            "            .echoSettings()\n",
+            "            .getRetrySettings()\n",
+            "            .toBuilder()\n",
+            "            .setTotalTimeout(Duration.ofSeconds(30))\n",
+            "            .build());\n",
+            "EchoStubSettings echoSettings = echoSettingsBuilder.build();");
     assertEquals(results.get(), expected);
   }
-
 }
