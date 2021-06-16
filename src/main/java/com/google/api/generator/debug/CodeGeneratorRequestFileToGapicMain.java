4f187b1b6849cfc18167bdd04fa2eb34901d710e
Vadym Matsishevskyi <25311427+vam-google@users.noreply.github.com>
2021-06-15 23:17:21 -0700

    feat: Add code request generator dumper and an ability to run generator from dumped file (#765)
    For debugging purposes.
    
    Usage example:
    
    ```bzl
    load("@gapic_generator_java//rules_java_gapic:java_gapic.bzl", "java_generator_request_dump")
    java_generator_request_dump(
        name = "compute_small_request_dump",
        srcs = [":compute_small_proto_with_info"],
        transport = "rest",
    )

--- src/main/java/com/google/api/generator/debug/CodeGeneratorRequestFileToGapicMain.java
+++ src/main/java/com/google/api/generator/debug/CodeGeneratorRequestFileToGapicMain.java
@@ -1,4 +1,4 @@
-// Copyright 2020 Google LLC
+// Copyright 2021 Google LLC
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
@@ -12,20 +12,34 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-package com.google.api.generator;
+package com.google.api.generator.debug;
 
+import com.google.api.generator.ProtoRegistry;
 import com.google.api.generator.gapic.Generator;
 import com.google.protobuf.ExtensionRegistry;
 import com.google.protobuf.compiler.PluginProtos.CodeGeneratorRequest;
 import com.google.protobuf.compiler.PluginProtos.CodeGeneratorResponse;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
-public class Main {
+// A generator entry point class, similar to Main but reads the CodeGeneratorRequest directly from a
+// file instead of relying on protoc to pipe it in.
+public class CodeGeneratorRequestFileToGapicMain {
   public static void main(String[] args) throws IOException {
     ExtensionRegistry registry = ExtensionRegistry.newInstance();
     ProtoRegistry.registerAllExtensions(registry);
-    CodeGeneratorRequest request = CodeGeneratorRequest.parseFrom(System.in, registry);
-    CodeGeneratorResponse response = Generator.generateGapic(request);
-    response.writeTo(System.out);
+
+    String inputFile = args[0];
+    String outputFile = args[1];
+
+    try (InputStream inputStream = new FileInputStream(inputFile);
+        OutputStream outputStream = new FileOutputStream(outputFile)) {
+      CodeGeneratorRequest request = CodeGeneratorRequest.parseFrom(inputStream, registry);
+      CodeGeneratorResponse response = Generator.generateGapic(request);
+      response.writeTo(outputStream);
+    }
   }
 }
