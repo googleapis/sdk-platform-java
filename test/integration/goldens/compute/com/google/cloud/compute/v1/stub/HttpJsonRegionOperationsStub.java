428db97c1534255a60530a8ed6137efc17ed56f4
Mira Leung <miraleung@users.noreply.github.com>
2021-08-02 15:24:17 -0700

    fix: prevent hanging by call backgroundResources.close() on stub.close() [ggj] (#804)
    * feat(ast): Add support for throwable causes
    
    * fix: call backgroundResources.close() on stub.close()
    
    * fix: make Throwable a static final in TypeNode
    
    * fix: update goldens
    
    * feat(ast): support throwing all kinds of expressions
    
    * fix: call backgroundResources.close() on stub.close()
    
    * fix: update goldens
    
    * feat(ast): Add support for multi-catch blocks
    
    * fix: add extra catch block
    
    * fix: isolate stub.close change to another PR
    
    * fix: merge branches

--- test/integration/goldens/compute/com/google/cloud/compute/v1/stub/HttpJsonRegionOperationsStub.java
+++ test/integration/goldens/compute/com/google/cloud/compute/v1/stub/HttpJsonRegionOperationsStub.java
@@ -167,7 +167,13 @@ public class HttpJsonRegionOperationsStub extends RegionOperationsStub {
 
   @Override
   public final void close() {
-    shutdown();
+    try {
+      backgroundResources.close();
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new IllegalStateException("Failed to close resource", e);
+    }
   }
 
   @Override
--- test/integration/goldens/pubsub/com/google/cloud/pubsub/v1/stub/GrpcSubscriberStub.java
+++ test/integration/goldens/pubsub/com/google/cloud/pubsub/v1/stub/GrpcSubscriberStub.java
@@ -762,7 +762,13 @@ public class GrpcSubscriberStub extends SubscriberStub {
 
   @Override
   public final void close() {
-    shutdown();
+    try {
+      backgroundResources.close();
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new IllegalStateException("Failed to close resource", e);
+    }
   }
 
   @Override
