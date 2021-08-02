e944f21ffebcad0694640ac99931248ce4822668
Mira Leung <miraleung@users.noreply.github.com>
2021-08-02 15:24:17 -0700

    fix: prevent hanging by call backgroundResources.close() on stub.close() [ggj] (#804)
    * feat(ast): Add support for throwable causes
    
    * fix: call backgroundResources.close() on stub.close()
    
    * fix: make Throwable a static final in TypeNode
    
    * fix: update goldens
    
    * feat(ast): support throwing all kinds of expressions
    
    * fix: call backgroundResources.close() on stub.close()
    
    * fix: update goldens
    
    * feat(ast): Add support for multi-catch blocks
    
    * fix: add extra catch block
    
    * fix: isolate stub.close change to another PR
    
    * fix: merge branches

--- gapic-generator-java/test/integration/goldens/kms/com/google/cloud/kms/v1/stub/GrpcKeyManagementServiceStub.java
+++ gapic-generator-java/test/integration/goldens/kms/com/google/cloud/kms/v1/stub/GrpcKeyManagementServiceStub.java
@@ -1119,7 +1119,13 @@ public class GrpcKeyManagementServiceStub extends KeyManagementServiceStub {
 
   @Override
   public final void close() {
-    shutdown();
+    try {
+      backgroundResources.close();
+    } catch (RuntimeException e) {
+      throw e;
+    } catch (Exception e) {
+      throw new IllegalStateException("Failed to close resource", e);
+    }
   }
 
   @Override
