381f1c7205fa97b70666a2a62b26b46eaae10ffd
Chanseok Oh <chanseok@google.com>
2022-01-21 13:26:27 -0500

    chore: clean up code

--- scripts/diff_gen_and_golden.sh
+++ scripts/diff_gen_and_golden.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 set -o errexit
 set -o pipefail
@@ -12,7 +12,7 @@ cd unpacked
 unzip -q -c "../${RAW_SRCJAR}" temp-codegen.srcjar | jar x
 cp -rf src/main/java/* ../src
 cp -rf src/test/java/* ../src
-[ -d proto ] && cp -rf proto/src/main/java/* ../src
+[[ -d proto ]] && cp -rf proto/src/main/java/* ../src
 cd ..
 
 # Remove unneeded non-Java files, like MANIFEST
@@ -20,8 +20,6 @@ find src -type f ! -name '*.java' -a ! -name '*gapic_metadata.json' -delete
 find src -type f -name 'PlaceholderFile.java' -delete
 find src -type d -empty -delete
 
-tree src
-tree test/integration/goldens/asset
 # This will not print diff_output to the console unless `--test_output=all` option
 # is enabled, it only emits the comparison results to the test.log.
 diff -ru src test/integration/goldens/${API_NAME}
