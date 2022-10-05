3cbad40ba1916ef28cb315b0277f1e280949011f
Emily Wang <emmwang@google.com>
2022-10-05 21:24:15 +0000

    Remove golden updating from SpringComposerTest

--- src/test/java/com/google/api/generator/spring/composer/SpringComposerTest.java
+++ src/test/java/com/google/api/generator/spring/composer/SpringComposerTest.java
@@ -15,10 +15,14 @@
 package com.google.api.generator.spring.composer;
 
 import com.google.api.generator.engine.ast.ClassDefinition;
+import com.google.api.generator.engine.writer.JavaWriterVisitor;
 import com.google.api.generator.gapic.composer.common.TestProtoLoader;
 import com.google.api.generator.gapic.model.GapicClass;
 import com.google.api.generator.gapic.model.GapicContext;
 import com.google.api.generator.test.framework.Assert;
+import com.google.api.generator.test.framework.Utils;
+import java.nio.file.Path;
+import java.nio.file.Paths;
 import java.util.List;
 import org.junit.Before;
 import org.junit.Test;
@@ -37,10 +41,17 @@ public class SpringComposerTest {
     List<GapicClass> gapicClasses = SpringComposer.composeServiceAutoConfigClasses(context);
 
     // write to verify result for now
+    // This unit test does not update or write to any golden files, but checks against the same ones
+    // as those generated and updated by individual class composer tests
+    // TODO: may need to revisit this approach as more components are added to SpringComposer
+    JavaWriterVisitor codeWriter = new JavaWriterVisitor();
     for (GapicClass gapicClazz : gapicClasses) {
       ClassDefinition clazz = gapicClazz.classDefinition();
-      String fileName = clazz.classIdentifier() + ".golden";
-      Assert.assertGoldenClass(this.getClass(), gapicClazz, fileName);
+      codeWriter.clear();
+      clazz.accept(codeWriter);
+      Path goldenFilePath =
+          Paths.get(Utils.getGoldenDir(this.getClass()), clazz.classIdentifier() + ".golden");
+      Assert.assertCodeEquals(goldenFilePath, codeWriter.write());
     }
   }
 }
