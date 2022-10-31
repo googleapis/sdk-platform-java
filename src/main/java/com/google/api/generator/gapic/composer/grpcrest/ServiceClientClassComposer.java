c6920496ee2405ffcf235518ac95a0c5e1c79eb6
vam-google <vam@google.com>
2022-10-31 16:55:57 -0700

    fix: fix REST transport client creation generated javadoc sample

--- src/main/java/com/google/api/generator/gapic/composer/grpcrest/ServiceClientClassComposer.java
+++ src/main/java/com/google/api/generator/gapic/composer/grpcrest/ServiceClientClassComposer.java
@@ -59,7 +59,7 @@ public class ServiceClientClassComposer extends AbstractServiceClientClassCompos
         ServiceClientHeaderSampleComposer.composeSetEndpointSample(clientType, settingsType);
     Sample transportSampleCode =
         ServiceClientHeaderSampleComposer.composeTransportSample(
-            clientType, settingsType, "defaultHttpJsonTransportProviderBuilder");
+            clientType, settingsType, "newHttpJsonBuilder");
     samples.addAll(
         Arrays.asList(
             classMethodSampleCode, credentialsSampleCode, endpointSampleCode, transportSampleCode));
