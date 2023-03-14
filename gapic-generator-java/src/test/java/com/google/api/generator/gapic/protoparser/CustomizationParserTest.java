07006e0ced91dd6ee84e503741ec6c2a96ae47dd
Joe Wang <joewa@google.com>
2023-03-14 19:55:03 -0400

    fix format issues

--- gapic-generator-java/src/test/java/com/google/api/generator/gapic/protoparser/CustomizationParserTest.java
+++ gapic-generator-java/src/test/java/com/google/api/generator/gapic/protoparser/CustomizationParserTest.java
@@ -1,3 +1,17 @@
+// Copyright 2023 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package com.google.api.generator.gapic.protoparser;
 
 import static com.google.api.generator.gapic.protoparser.CustomizationParser.parseAddBaseResourceNameCustomization;
@@ -17,9 +31,12 @@ public class CustomizationParserTest {
   public void parseAddBaseResourceNameCustomizationTest() {
     List<String> messages = parseAddBaseResourceNameCustomization(YAML_PATH);
     assertThat(messages.size()).isEqualTo(2);
-    assertThat(messages).containsAnyIn(new String[] {
-        "com.google.cloud.monitoring.v3.ListAlertPoliciesRequest",
-        "com.google.cloud.monitoring.v3.AnotherRequest"});
+    assertThat(messages)
+        .containsAnyIn(
+            new String[] {
+              "com.google.cloud.monitoring.v3.ListAlertPoliciesRequest",
+              "com.google.cloud.monitoring.v3.AnotherRequest"
+            });
   }
 
   @Test
