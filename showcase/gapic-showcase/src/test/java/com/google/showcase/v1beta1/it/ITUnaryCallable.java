eb55bfb79e76527a8d665d6e05f7cdf49af969aa
Mridula Peddada <mpeddada@google.com>
2023-03-15 12:48:01 -0400

    chore: add simple unary callable showcase test for gRPC support

---
+++ showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/ITUnaryCallable.java
@@ -0,0 +1,116 @@
+/*
+ * Copyright 2022 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.google.showcase.v1beta1.it;
+
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.gax.core.NoCredentialsProvider;
+import com.google.api.gax.grpc.GrpcStatusCode;
+import com.google.api.gax.grpc.InstantiatingGrpcChannelProvider;
+import com.google.api.gax.rpc.CancelledException;
+import com.google.api.gax.rpc.StatusCode;
+import com.google.rpc.Status;
+import com.google.showcase.v1beta1.EchoClient;
+import com.google.showcase.v1beta1.EchoRequest;
+import com.google.showcase.v1beta1.EchoResponse;
+import com.google.showcase.v1beta1.EchoSettings;
+import io.grpc.ManagedChannelBuilder;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ITUnaryCallable {
+
+  private static EchoClient grpcClient;
+
+  private static EchoClient httpJsonClient;
+
+  @Before
+  public void createClients() throws IOException, GeneralSecurityException {
+    // Create gRPC Echo Client
+    EchoSettings grpcEchoSettings =
+        EchoSettings.newBuilder()
+            .setCredentialsProvider(NoCredentialsProvider.create())
+            .setTransportChannelProvider(
+                InstantiatingGrpcChannelProvider.newBuilder()
+                    .setChannelConfigurator(ManagedChannelBuilder::usePlaintext)
+                    .build())
+            .build();
+    grpcClient = EchoClient.create(grpcEchoSettings);
+
+    // Create Http JSON Echo Client
+    EchoSettings httpJsonEchoSettings =
+            EchoSettings.newHttpJsonBuilder()
+                    .setCredentialsProvider(NoCredentialsProvider.create())
+                    .setTransportChannelProvider(
+                            EchoSettings.defaultHttpJsonTransportProviderBuilder()
+                                    .setHttpTransport(
+                                            new NetHttpTransport.Builder().doNotValidateCertificate().build())
+                                    .setEndpoint("http://localhost:7469")
+                                    .build())
+                    .build();
+    httpJsonClient = EchoClient.create(httpJsonEchoSettings);
+  }
+
+  @After
+  public void destroyClient() {
+    grpcClient.close();
+    httpJsonClient.close();
+  }
+
+  @Test
+  public void testGrpc() {
+    assertEquals("grpc-echo?", echoGrpc("grpc-echo?"));
+    assertEquals("grpc-echo!", echoGrpc("grpc-echo!"));
+  }
+
+  @Test
+  public void testGrpc_cancelledError() {
+    Status cancelledStatus = Status.newBuilder().setCode(StatusCode.Code.CANCELLED.ordinal()).build();
+    EchoRequest requestWithError = EchoRequest.newBuilder().setError(cancelledStatus).build();
+    CancelledException exception = assertThrows(CancelledException.class, () -> grpcClient.echo(requestWithError));
+    assertEquals(GrpcStatusCode.Code.CANCELLED, exception.getStatusCode().getCode());
+  }
+
+  @Test
+  public void testGrpc_shutdown() {
+    assertFalse(grpcClient.isShutdown());
+    grpcClient.shutdown();
+    assertTrue(grpcClient.isShutdown());
+  }
+
+
+  @Test
+  public void testHttpJson() {
+    assertEquals("http-echo?", echoHttpJson("http-echo?"));
+    assertEquals("http-echo!", echoHttpJson("http-echo!"));
+  }
+
+  private String echoGrpc(String value) {
+    EchoResponse response = grpcClient.echo(EchoRequest.newBuilder().setContent(value).build());
+    return response.getContent();
+  }
+
+  private String echoHttpJson(String value) {
+    EchoResponse response = httpJsonClient.echo(EchoRequest.newBuilder().setContent(value).build());
+    return response.getContent();
+  }
+}
