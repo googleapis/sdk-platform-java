989eb78545759e4c46e36b81ff1323225d347944
Mridula Peddada <mpeddada@google.com>
2023-03-16 17:12:33 -0400

    chore: add test for server-side streaming with gRPC transport

--- showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/ITServerSideStreaming.java
+++ showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/ITServerSideStreaming.java
@@ -1,5 +1,8 @@
 package com.google.showcase.v1beta1.it;
 
+import static com.google.common.truth.Truth.assertThat;
+import static org.junit.Assert.assertThrows;
+
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.gax.core.NoCredentialsProvider;
 import com.google.api.gax.grpc.InstantiatingGrpcChannelProvider;
@@ -10,74 +13,85 @@ import com.google.common.collect.ImmutableList;
 import com.google.rpc.Status;
 import com.google.showcase.v1beta1.*;
 import io.grpc.ManagedChannelBuilder;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.util.ArrayList;
+import java.util.Iterator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import java.io.IOException;
-import java.security.GeneralSecurityException;
-import java.util.ArrayList;
-
-import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertThrows;
-
 public class ITServerSideStreaming {
 
-    private static EchoClient grpcClient;
+  private static EchoClient grpcClient;
 
-    private static EchoClient httpJsonClient;
+  private static EchoClient httpJsonClient;
 
-    @Before
-    public void createClients() throws IOException, GeneralSecurityException {
-        // Create gRPC Echo Client
-        EchoSettings grpcEchoSettings =
-                EchoSettings.newBuilder()
-                        .setCredentialsProvider(NoCredentialsProvider.create())
-                        .setTransportChannelProvider(
-                                InstantiatingGrpcChannelProvider.newBuilder()
-                                        .setChannelConfigurator(ManagedChannelBuilder::usePlaintext)
-                                        .build())
-                        .build();
-        grpcClient = EchoClient.create(grpcEchoSettings);
+  @Before
+  public void createClients() throws IOException, GeneralSecurityException {
+    // Create gRPC Echo Client
+    EchoSettings grpcEchoSettings =
+        EchoSettings.newBuilder()
+            .setCredentialsProvider(NoCredentialsProvider.create())
+            .setTransportChannelProvider(
+                InstantiatingGrpcChannelProvider.newBuilder()
+                    .setChannelConfigurator(ManagedChannelBuilder::usePlaintext)
+                    .build())
+            .build();
+    grpcClient = EchoClient.create(grpcEchoSettings);
 
-        // Create Http JSON Echo Client
-        EchoSettings httpJsonEchoSettings =
-                EchoSettings.newHttpJsonBuilder()
-                        .setCredentialsProvider(NoCredentialsProvider.create())
-                        .setTransportChannelProvider(
-                                EchoSettings.defaultHttpJsonTransportProviderBuilder()
-                                        .setHttpTransport(
-                                                new NetHttpTransport.Builder().doNotValidateCertificate().build())
-                                        .setEndpoint("http://localhost:7469")
-                                        .build())
-                        .build();
-        httpJsonClient = EchoClient.create(httpJsonEchoSettings);
+    // Create Http JSON Echo Client
+    EchoSettings httpJsonEchoSettings =
+        EchoSettings.newHttpJsonBuilder()
+            .setCredentialsProvider(NoCredentialsProvider.create())
+            .setTransportChannelProvider(
+                EchoSettings.defaultHttpJsonTransportProviderBuilder()
+                    .setHttpTransport(
+                        new NetHttpTransport.Builder().doNotValidateCertificate().build())
+                    .setEndpoint("http://localhost:7469")
+                    .build())
+            .build();
+    httpJsonClient = EchoClient.create(httpJsonEchoSettings);
+  }
+
+  @After
+  public void destroyClient() {
+    grpcClient.close();
+    httpJsonClient.close();
+  }
+
+  @Test
+  public void testGrpc_receiveStreamedContent() {
+    String content = "The rain in Spain stays mainly on the plain!";
+    ServerStream<EchoResponse> responseStream =
+        grpcClient.expandCallable().call(ExpandRequest.newBuilder().setContent(content).build());
+    ArrayList<String> responses = new ArrayList<>();
+    for (EchoResponse response : responseStream) {
+      responses.add(response.getContent());
     }
 
-    @After
-    public void destroyClient() {
-        grpcClient.close();
-        httpJsonClient.close();
-    }
+    assertThat(responses)
+        .containsExactlyElementsIn(
+            ImmutableList.of(
+                "The", "rain", "in", "Spain", "stays", "mainly", "on", "the", "plain!"));
+  }
 
-    @Test
-    public void testGrpc_consumesAllResponses() {
-        String content = "The early bird catches the worm";
-        ServerStream<EchoResponse> responseStream = grpcClient.expandCallable().call(ExpandRequest.newBuilder().setContent(content).build());
-        ArrayList<String> responses = new ArrayList<>();
-        for (EchoResponse response : responseStream) {
-            responses.add(response.getContent());
-        }
-
-        assertThat(responses).containsExactlyElementsIn(ImmutableList.of("The", "early", "bird", "catches", "the", "worm"));
-    }
-
-    @Test
-    public void testGrpc_errorHandling() {
-        String content = "The early bird catches the worm";
-        Status cancelledStatus = Status.newBuilder().setCode(StatusCode.Code.CANCELLED.ordinal()).build();
-        ServerStream<EchoResponse> responseStream = grpcClient.expandCallable().call(
-                ExpandRequest.newBuilder().setContent(content).setError(cancelledStatus).build());
-    }
-
+  @Test
+  public void testGrpc_serverError_receiveErrorAfterLastWordInStream() {
+    String content = "The rain in Spain";
+    Status cancelledStatus =
+        Status.newBuilder().setCode(StatusCode.Code.CANCELLED.ordinal()).build();
+    ServerStream<EchoResponse> responseStream =
+        grpcClient
+            .expandCallable()
+            .call(ExpandRequest.newBuilder().setContent(content).setError(cancelledStatus).build());
+    Iterator<EchoResponse> echoResponseIterator = responseStream.iterator();
+    assertThat(echoResponseIterator.next().getContent()).isEqualTo("The");
+    assertThat(echoResponseIterator.next().getContent()).isEqualTo("rain");
+    assertThat(echoResponseIterator.next().getContent()).isEqualTo("in");
+    assertThat(echoResponseIterator.next().getContent()).isEqualTo("Spain");
+    CancelledException cancelledException =
+        assertThrows(CancelledException.class, echoResponseIterator::next);
+    assertThat(cancelledException.getStatusCode().getCode()).isEqualTo(StatusCode.Code.CANCELLED);
+  }
 }
