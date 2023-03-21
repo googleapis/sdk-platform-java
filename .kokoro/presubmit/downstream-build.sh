9f4f1a006373b09995e5c5c238f0baf140218fad
Mridula Peddada <mpeddada@google.com>
2023-03-21 17:16:16 -0400

    chore: modify downstream check for shared-dependencies and java-core migration

--- .kokoro/presubmit/downstream-build.sh
+++ .kokoro/presubmit/downstream-build.sh
@@ -40,46 +40,47 @@ GAPIC_BOM_VERSION=$(sed -e 's/xmlns=".*"//' gapic-generator-java-bom/pom.xml | x
 ### Round 2
 # Run the updated GAPIC BOM against HEAD of java-shared-dependencies
 git clone "https://github.com/googleapis/google-cloud-java.git" --depth=1
-pushd google-cloud-java/java-shared-dependencies/first-party-dependencies
+
+#pushd google-cloud-java/java-shared-dependencies/first-party-dependencies
 
 # Replace GAPIC BOM version
-xmllint --shell pom.xml <<EOF
-setns x=http://maven.apache.org/POM/4.0.0
-cd .//x:artifactId[text()="gapic-generator-java-bom"]
-cd ../x:version
-set ${GAPIC_BOM_VERSION}
-save pom.xml
-EOF
-
-echo "Modifications to java-shared-dependencies:"
-git diff
-echo
-
-# Namespace (xmlns) prevents xmllint from specifying tag names in XPath
-SHARED_DEPS_VERSION=$(sed -e 's/xmlns=".*"//' pom.xml | xmllint --xpath '/project/version/text()' -)
-if [ -z "${SHARED_DEPS_VERSION}" ]; then
-  echo "Shared dependencies version is not found in pom.xml"
-  exit 1
-fi
-
-popd
-pushd google-cloud-java
-mvn -B -V -ntp install --also-make --projects \
-    java-shared-dependencies,java-shared-dependencies/first-party-dependencies,java-shared-dependencies/third-party-dependencies \
-    -DskipTests=true \
-    -Dmaven.javadoc.skip=true \
-    -Dgcloud.download.skip=true \
-    -Denforcer.skip=true
+#xmllint --shell pom.xml <<EOF
+#setns x=http://maven.apache.org/POM/4.0.0
+#cd .//x:artifactId[text()="gapic-generator-java-bom"]
+#cd ../x:version
+#set ${GAPIC_BOM_VERSION}
+#save pom.xml
+#EOF
+#
+#echo "Modifications to java-shared-dependencies:"
+#git diff
+#echo
+#
+## Namespace (xmlns) prevents xmllint from specifying tag names in XPath
+#SHARED_DEPS_VERSION=$(sed -e 's/xmlns=".*"//' pom.xml | xmllint --xpath '/project/version/text()' -)
+#if [ -z "${SHARED_DEPS_VERSION}" ]; then
+#  echo "Shared dependencies version is not found in pom.xml"
+#  exit 1
+#fi
+#
+#popd
+#pushd google-cloud-java
+#mvn -B -V -ntp install --also-make --projects \
+#    java-shared-dependencies,java-shared-dependencies/first-party-dependencies,java-shared-dependencies/third-party-dependencies \
+#    -DskipTests=true \
+#    -Dmaven.javadoc.skip=true \
+#    -Dgcloud.download.skip=true \
+#    -Denforcer.skip=true
 
 ### Round 3
 # Install google-cloud-core-bom (part of java-shared-dependencies)
-echo "Installing java-core"
-mvn -B -V -ntp install -DskipTests --also-make -f java-core \
-    -DskipTests=true \
-    -Dmaven.javadoc.skip=true \
-    -Dgcloud.download.skip=true \
-    -Denforcer.skip=true
-popd
+#echo "Installing java-core"
+#mvn -B -V -ntp install -DskipTests --also-make -f java-core \
+#    -DskipTests=true \
+#    -Dmaven.javadoc.skip=true \
+#    -Dgcloud.download.skip=true \
+#    -Denforcer.skip=true
+#popd
 
 ### Round 4
 # Run the updated java-shared-dependencies BOM against google-cloud-java
