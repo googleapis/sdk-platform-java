9d15fc5aea0dcf2536bd6fce0fbf8f1ed5299a78
Mridula Peddada <mpeddada@google.com>
2023-03-23 16:22:28 -0400

    clean up imports and pom

--- showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/MessagingClientHttpJsonTest.java
+++ showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/MessagingClientHttpJsonTest.java
@@ -47,7 +47,6 @@ import org.junit.AfterClass;
 import org.junit.Assert;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Ignore;
 import org.junit.Test;
 
 @Generated("by gapic-generator-java")
