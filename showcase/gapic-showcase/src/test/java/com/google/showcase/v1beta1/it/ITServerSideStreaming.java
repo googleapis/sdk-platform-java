8ec304addf185a4714b9dc9418e3508e4dd80ec0
Lawrence Qiu <lawrenceqiu@google.com>
2023-03-31 21:43:51 +0000

    chore: Add httpjson server-side streaming showcase tests (#1588)

--- showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/ITServerSideStreaming.java
+++ showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/ITServerSideStreaming.java
@@ -19,6 +19,7 @@ package com.google.showcase.v1beta1.it;
 import static com.google.common.truth.Truth.assertThat;
 import static org.junit.Assert.assertThrows;
 
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.gax.core.NoCredentialsProvider;
 import com.google.api.gax.rpc.CancelledException;
 import com.google.api.gax.rpc.ServerStream;
@@ -35,11 +36,13 @@ import java.util.ArrayList;
 import java.util.Iterator;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 public class ITServerSideStreaming {
 
   private EchoClient grpcClient;
+  private EchoClient httpjsonClient;
 
   @Before
   public void createClients() throws IOException {
@@ -53,11 +56,24 @@ public class ITServerSideStreaming {
                     .build())
             .build();
     grpcClient = EchoClient.create(grpcEchoSettings);
+    // Create Http JSON Echo Client
+    EchoSettings httpJsonEchoSettings =
+        EchoSettings.newHttpJsonBuilder()
+            .setCredentialsProvider(NoCredentialsProvider.create())
+            .setTransportChannelProvider(
+                EchoSettings.defaultHttpJsonTransportProviderBuilder()
+                    .setHttpTransport(
+                        new NetHttpTransport.Builder().doNotValidateCertificate().build())
+                    .setEndpoint("http://localhost:7469")
+                    .build())
+            .build();
+    httpjsonClient = EchoClient.create(httpJsonEchoSettings);
   }
 
   @After
   public void destroyClient() {
     grpcClient.close();
+    httpjsonClient.close();
   }
 
   @Test
@@ -84,6 +100,47 @@ public class ITServerSideStreaming {
         Status.newBuilder().setCode(StatusCode.Code.CANCELLED.ordinal()).build();
     ServerStream<EchoResponse> responseStream =
         grpcClient
+            .expandCallable()
+            .call(ExpandRequest.newBuilder().setContent(content).setError(cancelledStatus).build());
+    Iterator<EchoResponse> echoResponseIterator = responseStream.iterator();
+
+    assertThat(echoResponseIterator.next().getContent()).isEqualTo("The");
+    assertThat(echoResponseIterator.next().getContent()).isEqualTo("rain");
+    assertThat(echoResponseIterator.next().getContent()).isEqualTo("in");
+    assertThat(echoResponseIterator.next().getContent()).isEqualTo("Spain");
+    CancelledException cancelledException =
+        assertThrows(CancelledException.class, echoResponseIterator::next);
+    assertThat(cancelledException.getStatusCode().getCode()).isEqualTo(StatusCode.Code.CANCELLED);
+  }
+
+  @Test
+  public void testHttpJson_receiveStreamedContent() {
+    String content = "The rain in Spain stays mainly on the plain!";
+    ServerStream<EchoResponse> responseStream =
+        httpjsonClient
+            .expandCallable()
+            .call(ExpandRequest.newBuilder().setContent(content).build());
+    ArrayList<String> responses = new ArrayList<>();
+    for (EchoResponse response : responseStream) {
+      responses.add(response.getContent());
+    }
+
+    assertThat(responses)
+        .containsExactlyElementsIn(
+            ImmutableList.of(
+                "The", "rain", "in", "Spain", "stays", "mainly", "on", "the", "plain!"))
+        .inOrder();
+  }
+
+  @Ignore(
+      value = "Ignore until https://github.com/googleapis/gapic-showcase/issues/1286 is resolved")
+  @Test
+  public void testHttpJson_serverError_receiveErrorAfterLastWordInStream() {
+    String content = "The rain in Spain";
+    Status cancelledStatus =
+        Status.newBuilder().setCode(StatusCode.Code.CANCELLED.ordinal()).build();
+    ServerStream<EchoResponse> responseStream =
+        httpjsonClient
             .expandCallable()
             .call(ExpandRequest.newBuilder().setContent(content).setError(cancelledStatus).build());
     Iterator<EchoResponse> echoResponseIterator = responseStream.iterator();
