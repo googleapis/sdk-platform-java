680b1c5b6c402565da537021afdd50314186b7be
Joe Wang <joewa@google.com>
2023-03-31 20:45:55 -0400

    fix format

--- gax-java/gax/src/main/java/com/google/api/gax/rpc/ServerStream.java
+++ gax-java/gax/src/main/java/com/google/api/gax/rpc/ServerStream.java
@@ -93,6 +93,7 @@ public class ServerStream<V> implements Iterable<V> {
 
   /**
    * Returns a sequential {@code Stream} with server responses as its source.
+   *
    * @return a sequential {@code Stream} over the elements in server responses
    */
   public Stream<V> stream() {
