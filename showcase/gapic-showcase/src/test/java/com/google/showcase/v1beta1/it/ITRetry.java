8be44c89511cbf505f4e05ae24f9328b2f59783d
Lawrence Qiu <lawrenceqiu@google.com>
2023-04-18 20:41:43 -0400

    chore: Set delay to be 30s

--- showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/ITRetry.java
+++ showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/ITRetry.java
@@ -485,10 +485,10 @@ public class ITRetry {
       BlockRequest blockRequest =
           BlockRequest.newBuilder()
               .setSuccess(
-                  BlockResponse.newBuilder().setContent("httpjsonBlockContent_110msDelay_Retry"))
+                  BlockResponse.newBuilder().setContent("httpjsonBlockContent_30sDelay_Retry"))
               // Set the timeout to be longer than the RPC timeout
               .setResponseDelay(
-                  com.google.protobuf.Duration.newBuilder().setNanos(110000000).build())
+                  com.google.protobuf.Duration.newBuilder().setSeconds(30).build())
               .build();
       RetryingFuture<BlockResponse> retryingFuture =
           (RetryingFuture<BlockResponse>) httpJsonClient.blockCallable().futureCall(blockRequest);
