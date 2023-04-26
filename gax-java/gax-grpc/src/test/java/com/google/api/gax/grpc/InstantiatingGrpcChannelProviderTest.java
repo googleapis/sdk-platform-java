ddf08a81e22d5ff1f1d567d061ff955d9a19b62a
mohanli-ml <mohanli@google.com>
2023-04-26 22:49:25 +0000

    feat: add an option to enable DirectPath xDS

--- gax-java/gax-grpc/src/test/java/com/google/api/gax/grpc/InstantiatingGrpcChannelProviderTest.java
+++ gax-java/gax-grpc/src/test/java/com/google/api/gax/grpc/InstantiatingGrpcChannelProviderTest.java
@@ -282,7 +282,6 @@ public class InstantiatingGrpcChannelProviderTest extends AbstractMtlsTransportC
             .setAttemptDirectPathXds()
             .build();
 
-
     assertThat(provider.isDirectPathXdsEnabled()).isTrue();
   }
 
