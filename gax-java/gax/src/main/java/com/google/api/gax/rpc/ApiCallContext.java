d034e0c29e2f1d9729b41f74d134357eb15e984f
Lawrence Qiu <lawrenceqiu@google.com>
2023-06-08 16:17:20 -0400

    chore: Revert BetaApi annotation for extraHeaders in gax

--- gax-java/gax/src/main/java/com/google/api/gax/rpc/ApiCallContext.java
+++ gax-java/gax/src/main/java/com/google/api/gax/rpc/ApiCallContext.java
@@ -233,6 +233,7 @@ public interface ApiCallContext extends RetryingContext {
   ApiCallContext merge(ApiCallContext inputCallContext);
 
   /** Return a new ApiCallContext with the extraHeaders merged into the present instance. */
+  @BetaApi("The surface for extra headers is not stable yet and may change in the future.")
   ApiCallContext withExtraHeaders(Map<String, List<String>> extraHeaders);
 
   /** Return the extra headers set for this context. */
