e29664e3696ddac15431ef9c46fc540f08256e5c
Lawrence Qiu <lawrenceqiu@google.com>
2023-06-09 14:54:18 -0400

    chore: Add grpc test cases

--- showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/ITPagination.java
+++ showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/ITPagination.java
@@ -18,7 +18,6 @@ import org.junit.Test;
 public class ITPagination {
 
   private static EchoClient grpcClient;
-
   private static EchoClient httpjsonClient;
 
   @BeforeClass
@@ -41,6 +40,24 @@ public class ITPagination {
   // This tests that pagination returns the correct number of pages + responses and the content is
   // correct. It tests pagination from Server-Side Streaming.
   @Test
+  public void testExpandGrpc() {
+    String content = "Testing the entire response is the same";
+    int expected = content.split(" ").length;
+    ServerStream<EchoResponse> echoResponses =
+        grpcClient.expandCallable().call(ExpandRequest.newBuilder().setContent(content).build());
+    List<String> values = new ArrayList<>();
+    for (EchoResponse echoResponse : echoResponses) {
+      values.add(echoResponse.getContent());
+    }
+
+    String response = String.join(" ", values);
+    assertThat(values.size()).isEqualTo(expected);
+    assertThat(response).isEqualTo(content);
+  }
+
+  // This tests that pagination returns the correct number of pages + responses and the content is
+  // correct. It tests pagination from Server-Side Streaming.
+  @Test
   public void testExpandHttpJson() {
     String content = "Testing the entire response is the same";
     int expected = content.split(" ").length;
@@ -53,9 +70,56 @@ public class ITPagination {
       values.add(echoResponse.getContent());
     }
 
-    String response = String.join(" ", values.toArray(new String[0]));
+    String response = String.join(" ", values);
     assertThat(values.size()).isEqualTo(expected);
     assertThat(response).isEqualTo(content);
+  }
+
+  // This tests that pagination returns the correct number of pages + responses and the content is
+  // correct. It tests pagination from ServerSideStreaming.
+  //
+  // The pageToken is where the streaming responses come back from and the page size denotes
+  // how many of the responses come back together. i.e for PageSize = 2 and PageToken = 1, see
+  // below:
+  //         | A | Series  | Of  | Words | That  | Will  | Be  | Sent  | Back  | One | By  | One
+  // Page #  | X | 1       | 1   | 2     | 2     | 3     | 3   | 4     | 4     | 5   | 5   | 6
+  // Token # | 0 | 1       | 2   | 3     | 4     | 5     | 6   | 7     | 8     | 9   | 10  | 11
+  @Test
+  public void testPagedExpandWithTokenGrpc() {
+    int pageSize = 2;
+    int pageToken = 1;
+    String content = "A series of words that will be sent back one by one";
+    int contentLength = content.split(" ").length;
+
+    EchoClient.PagedExpandPagedResponse pagedExpandPagedResponse =
+        grpcClient.pagedExpand(
+            PagedExpandRequest.newBuilder()
+                .setContent(content)
+                .setPageSize(pageSize)
+                .setPageToken(String.valueOf(pageToken))
+                .build());
+    String[] expected = content.split(" ");
+    int numResponses = 0;
+    int numPages = 0;
+    for (EchoClient.PagedExpandPage page : pagedExpandPagedResponse.iteratePages()) {
+      for (EchoResponse echoResponse : page.getValues()) {
+        // Add pageToken as offset to the expected array to start indexing at the pageToken
+        assertThat(echoResponse.getContent()).isEqualTo(expected[numResponses + pageToken]);
+        numResponses++;
+      }
+      numPages++;
+    }
+
+    int numExpectedPages = ((contentLength - pageToken) / pageSize);
+    // If the responses can't be evenly split into pages, then the extra responses
+    // will go to an additional page
+    if ((contentLength - pageToken) % pageSize != 0) {
+      numExpectedPages++;
+    }
+    int numExpectedResponses = contentLength - pageToken;
+
+    assertThat(numPages).isEqualTo(numExpectedPages);
+    assertThat(numResponses).isEqualTo(numExpectedResponses);
   }
 
   // This tests that pagination returns the correct number of pages + responses and the content is
