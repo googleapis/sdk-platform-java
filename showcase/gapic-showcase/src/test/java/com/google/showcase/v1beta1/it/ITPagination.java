d69cbf859b72c4dd6bddae5cb7ac6d0bc87de813
Lawrence Qiu <lawrenceqiu@google.com>
2023-06-12 21:44:17 -0400

    chore: Address PR comments

--- showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/ITPagination.java
+++ showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/ITPagination.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.google.showcase.v1beta1.it;
 
 import static com.google.common.truth.Truth.assertThat;
@@ -49,7 +65,7 @@ public class ITPagination {
       values.add(echoResponse.getContent());
     }
 
-    assertThat(values.size()).isEqualTo(splitContent.size());
+    assertThat(values).hasSize(splitContent.size());
     assertThat(splitContent).containsExactlyElementsIn(values).inOrder();
   }
 
@@ -64,7 +80,7 @@ public class ITPagination {
       values.add(echoResponse.getContent());
     }
 
-    assertThat(values.size()).isEqualTo(splitContent.size());
+    assertThat(values).hasSize(splitContent.size());
     assertThat(splitContent).containsExactlyElementsIn(values).inOrder();
   }
 
