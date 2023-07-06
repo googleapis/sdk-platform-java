c4f0c892caa0195201b7eb221499d89a0494d200
mpeddada1 <mpeddada@google.com>
2023-07-06 15:23:29 +0000

    rebase with origin/main

--- scripts/update_golden.sh
+++ scripts/update_golden.sh
@@ -19,10 +19,6 @@ find . -name '*.java' -delete
 find . -name 'gapic_metadata.json' -delete
 
 mkdir -p ./src
-echo "Hello"
-#ls ${UNPACK_DIR}/src/main/java/*
-#ls ${UNPACK_DIR}/src/main/java/google/
-ls ./src
 cp -r ${UNPACK_DIR}/src/main/java/* ./src
 cp -r ${UNPACK_DIR}/src/test/java/* ./src
 [ -d ${UNPACK_DIR}/proto ] && cp -r ${UNPACK_DIR}/proto/src/main/java/* ./src
