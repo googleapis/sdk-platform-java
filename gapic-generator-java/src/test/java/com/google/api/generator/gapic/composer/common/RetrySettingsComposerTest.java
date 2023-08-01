667f2da15d0800d87d0866e36627ac23955c0b01
Diego Alonso Marquez Palacios <diegomarquezp@google.com>
2023-08-01 13:06:50 -0400

    chore: revert changes in gapic-generator-java

--- gapic-generator-java/src/test/java/com/google/api/generator/gapic/composer/common/RetrySettingsComposerTest.java
+++ gapic-generator-java/src/test/java/com/google/api/generator/gapic/composer/common/RetrySettingsComposerTest.java
@@ -121,18 +121,18 @@ public class RetrySettingsComposerTest {
             "settings ="
                 + " RetrySettings.newBuilder().setInitialRetryDelay("
                 + "Duration.ofMillis(100L)).setRetryDelayMultiplier(2.0)"
-                + ".setMaxRetryDelay(java.time.Duration.ofMillis(3000L))"
-                + ".setInitialRpcTimeout(java.time.Duration.ofMillis(10000L))"
+                + ".setMaxRetryDelay(Duration.ofMillis(3000L))"
+                + ".setInitialRpcTimeout(Duration.ofMillis(10000L))"
                 + ".setRpcTimeoutMultiplier(1.0)"
-                + ".setMaxRpcTimeout(java.time.Duration.ofMillis(10000L))"
-                + ".setTotalTimeout(java.time.Duration.ofMillis(10000L)).build();\n",
+                + ".setMaxRpcTimeout(Duration.ofMillis(10000L))"
+                + ".setTotalTimeout(Duration.ofMillis(10000L)).build();\n",
             "definitions.put(\"retry_policy_1_params\", settings);\n",
             "settings ="
                 + " RetrySettings.newBuilder()"
-                + ".setInitialRpcTimeout(java.time.Duration.ofMillis(5000L))"
+                + ".setInitialRpcTimeout(Duration.ofMillis(5000L))"
                 + ".setRpcTimeoutMultiplier(1.0)"
-                + ".setMaxRpcTimeout(java.time.Duration.ofMillis(5000L))"
-                + ".setTotalTimeout(java.time.Duration.ofMillis(5000L)).build();\n",
+                + ".setMaxRpcTimeout(Duration.ofMillis(5000L))"
+                + ".setTotalTimeout(Duration.ofMillis(5000L)).build();\n",
             "definitions.put(\"no_retry_0_params\", settings);\n",
             "RETRY_PARAM_DEFINITIONS = definitions.build();\n",
             "}\n");
@@ -341,10 +341,10 @@ public class RetrySettingsComposerTest {
                 + "WaitResponse.class))"
                 + ".setMetadataTransformer(ProtoOperationTransformers.MetadataTransformer.create("
                 + "WaitMetadata.class)).setPollingAlgorithm(OperationTimedPollAlgorithm.create("
-                + "RetrySettings.newBuilder().setInitialRetryDelay(java.time.Duration.ofMillis(5000L))"
-                + ".setRetryDelayMultiplier(1.5).setMaxRetryDelay(java.time.Duration.ofMillis(45000L))"
-                + ".setInitialRpcTimeout(java.time.Duration.ZERO).setRpcTimeoutMultiplier(1.0)"
-                + ".setMaxRpcTimeout(java.time.Duration.ZERO).setTotalTimeout(java.time.Duration.ofMillis(300000L))"
+                + "RetrySettings.newBuilder().setInitialRetryDelay(Duration.ofMillis(5000L))"
+                + ".setRetryDelayMultiplier(1.5).setMaxRetryDelay(Duration.ofMillis(45000L))"
+                + ".setInitialRpcTimeout(Duration.ZERO).setRpcTimeoutMultiplier(1.0)"
+                + ".setMaxRpcTimeout(Duration.ZERO).setTotalTimeout(Duration.ofMillis(300000L))"
                 + ".build()))");
     assertEquals(expected, writerVisitor.write());
   }
@@ -394,7 +394,7 @@ public class RetrySettingsComposerTest {
             + "BatchingSettings.newBuilder()"
             + ".setElementCountThreshold(100L)"
             + ".setRequestByteThreshold(1048576L)"
-            + ".setDelayThreshold(java.time.Duration.ofMillis(10L))"
+            + ".setDelayThreshold(Duration.ofMillis(10L))"
             + ".setFlowControlSettings("
             + "FlowControlSettings.newBuilder()"
             + ".setLimitExceededBehavior(FlowController.LimitExceededBehavior.Ignore)"
@@ -451,7 +451,7 @@ public class RetrySettingsComposerTest {
             + "BatchingSettings.newBuilder()"
             + ".setElementCountThreshold(1000L)"
             + ".setRequestByteThreshold(1048576L)"
-            + ".setDelayThreshold(java.time.Duration.ofMillis(50L))"
+            + ".setDelayThreshold(Duration.ofMillis(50L))"
             + ".setFlowControlSettings("
             + "FlowControlSettings.newBuilder()"
             + ".setMaxOutstandingElementCount(100000L)"
--- gapic-generator-java/src/test/java/com/google/api/generator/gapic/composer/samplecode/SampleBodyJavaFormatterTest.java
+++ gapic-generator-java/src/test/java/com/google/api/generator/gapic/composer/samplecode/SampleBodyJavaFormatterTest.java
@@ -49,7 +49,7 @@ public class SampleBodyJavaFormatterTest {
     String sampleCode =
         "echoSettingsBuilder.echoSettings().setRetrySettings("
             + "echoSettingsBuilder.echoSettings().getRetrySettings().toBuilder()"
-            + ".setTotalTimeout(java.time.Duration.ofSeconds(30)).build());";
+            + ".setTotalTimeout(Duration.ofSeconds(30)).build());";
     String result = SampleBodyJavaFormatter.format(sampleCode);
     String expected =
         LineFormatter.lines(
@@ -60,7 +60,7 @@ public class SampleBodyJavaFormatterTest {
             "            .echoSettings()\n",
             "            .getRetrySettings()\n",
             "            .toBuilder()\n",
-            "            .setTotalTimeout(java.time.Duration.ofSeconds(30))\n",
+            "            .setTotalTimeout(Duration.ofSeconds(30))\n",
             "            .build());");
     assertEquals(expected, result);
   }
