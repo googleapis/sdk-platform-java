d966aa0cd9deca037c2414bd39902359627e7e33
Diego Marquez <diegomarquezp@google.com>
2023-08-05 00:51:43 +0000

    chore: adding extra post-processing steps

--- library_generation/generate_library.sh
+++ library_generation/generate_library.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 
 set -ex
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+
 GOOGLEAPIS_COMMIT=$1
 PROTOBUF_VERSION=$2
 GRPC_VERSION=$3
@@ -15,6 +17,7 @@ DESTINATION_PATH=${11}
 OWLBOT_SHA=${12}
 OWLBOT_PY_PATH=${13}
 REPO_METADATA_PATH=${14}
+MONOREPO_TAG=${15}
 
 if [ -z "${IS_GAPIC_LIBRARY}" ]; then
   IS_GAPIC_LIBRARY="true"
@@ -244,7 +247,7 @@ OWLBOT_IMAGE=gcr.io/cloud-devrel-public-resources/owlbot-java@sha256:$OWLBOT_SHA
 DISTRIBUTION_NAME=$(cat $REPO_METADATA_PATH | jq -r '.distribution_name // empty' | rev | cut -d: -f1 | rev)
 API_SHORTNAME=$(cat $REPO_METADATA_PATH | jq -r '.api_shortname // empty')
 
-if [ -z ${var+x} ]; then
+if [ -z ${DISTRIBUTION_NAME+x} ]; then
   echo "owlbot will not use copy regex"
 else
   OWLBOT_COPY_REGEX=$(cat <<-_EOT_
@@ -259,13 +262,13 @@ deep-preserve-regex:
 
 deep-copy-regex:
 - source: "/{{ proto_path }}/(v.*)/.*-java/proto-google-.*/src"
-  dest: "/owl-bot-staging/${MODULE_NAME}/$1/proto-${DISTRIBUTION_NAME}-$1/src"
+  dest: "/owl-bot-staging/${MODULE_NAME}/\$1/proto-${DISTRIBUTION_NAME}-\$1/src"
 - source: "/{{ proto_path }}/(v.*)/.*-java/grpc-google-.*/src"
-  dest: "/owl-bot-staging/${MODULE_NAME}/$1/grpc-${DISTRIBUTION_NAME}-$1/src"
+  dest: "/owl-bot-staging/${MODULE_NAME}/\$1/grpc-${DISTRIBUTION_NAME}-\$1/src"
 - source: "/{{ proto_path }}/(v.*)/.*-java/gapic-google-.*/src"
-  dest: "/owl-bot-staging/${MODULE_NAME}/$1/${DISTRIBUTION_NAME}/src"
+  dest: "/owl-bot-staging/${MODULE_NAME}/\$1/${DISTRIBUTION_NAME}/src"
 - source: "/{{ proto_path }}/(v.*)/.*-java/samples/snippets/generated"
-  dest: "/owl-bot-staging/${MODULE_NAME}/$1/samples/snippets/generated"
+  dest: "/owl-bot-staging/${MODULE_NAME}/\$1/samples/snippets/generated"
 _EOT_
 )
 fi
@@ -277,22 +280,53 @@ _EOT_
 )
 
 # render owlbot.py template
-OWLBOT_PY_CONTENT=$(cat <<-_EOT_
-import synthtool as s
-from synthtool.languages import java
-
-
-for library in s.get_staging_dirs():
-    # put any special-case replacements here
-    s.move(library)
-
-s.remove_staging_dirs()
-java.common_templates(monorepo=True)
-_EOT_
-)
+OWLBOT_PY_CONTENT=$(cat "$SCRIPT_DIR/post-processing/templates/owlbot.py.template")
 
 cp -r $GENERATED_LIBRARY_ROOT $OWLBOT_STAGING_FOLDER
 echo "$OWLBOT_YAML_CONTENT" > $OWLBOT_STAGING_FOLDER/.OwlBot.yaml
 echo "$OWLBOT_PY_CONTENT" > $WORKSPACE/owlbot.py
 docker run --rm -v $WORKSPACE:/workspace --user $(id -u):$(id -g) $OWLBOT_IMAGE
+
+# postprocessor cleanup
+bash $SCRIPT_DIR/post-processing/update_owlbot_postprocessor_config.sh $WORKSPACE
+bash $SCRIPT_DIR/post-processing/delete_non_generated_samples.sh $WORKSPACE
+bash $SCRIPT_DIR/post-processing/consolidate_config.sh $WORKSPACE
+bash $SCRIPT_DIR/post-processing/readme_update.sh $WORKSPACE
+rm $WORKSPACE/versions.txt
+if [ -z ${MONOREPO_TAG+x} ]; then
+  echo "Will not add parent project to pom"
+else
+  pushd $SCRIPT_DIR
+  [ ! -d google-cloud-java ] && git clone https://github.com/googleapis/google-cloud-java
+  pushd google-cloud-java
+  git checkout $MONOREPO_TAG
+  PARENT_POM="$(pwd)/google-cloud-jar-parent/pom.xml"
+  popd
+  # rm -rdf google-cloud-java
+  popd
+  bash $SCRIPT_DIR/post-processing/set_parent_pom.sh $WORKSPACE $PARENT_POM
+fi
+
+# rename folders properly (may not be necessary after all)
+pushd $WORKSPACE
+GAPIC_LIB_ORIGINAL_NAME=$(find . -name 'gapic-*')
+GAPIC_LIB_NEW_NAME=$(echo "$GAPIC_LIB_ORIGINAL_NAME" |\
+  sed 's/cloud-cloud/cloud/' | sed 's/-v[0-9a-zA-Z]-java//' | sed 's/gapic-//')
+PROTO_LIB_ORIGINAL_NAME=$(find . -name 'proto-*')
+PROTO_LIB_NEW_NAME=$(echo "$PROTO_LIB_ORIGINAL_NAME" |\
+  sed 's/cloud-cloud/cloud/' | sed 's/-java//')
+GRPC_LIB_ORIGINAL_NAME=$(find . -name 'grpc-*')
+GRPC_LIB_NEW_NAME=$(echo "$GRPC_LIB_ORIGINAL_NAME" |\
+  sed 's/cloud-cloud/cloud/' | sed 's/-java//')
+# two folders exist, move the contents of one to the other
+mv $GAPIC_LIB_ORIGINAL_NAME/* $GAPIC_LIB_NEW_NAME
+rm -rdf $GAPIC_LIB_ORIGINAL_NAME
+# just rename these two
+mv $PROTO_LIB_ORIGINAL_NAME $PROTO_LIB_NEW_NAME
+mv $GRPC_LIB_ORIGINAL_NAME $GRPC_LIB_NEW_NAME
+popd
+
+
+
+
 
--- library_generation/run.sh
+++ library_generation/run.sh
@@ -1,20 +1,21 @@
 #!/bin/bash
 # convenience script to test locally - remove when ready for merge
 
-GOOGLEAPIS_COMMIT=82c0f26dae2343cd1a39084283c169bf3415c950
+GOOGLEAPIS_COMMIT=7f2c9d1
 PROTOBUF_VERSION=23.2
 GRPC_VERSION=1.56.1
 GAPIC_GENERATOR_VERSION=2.24.0
-PROTO_PATH=google/cloud/language/v1
+PROTO_PATH=google/cloud/channel/v1
 CONTAINS_CLOUD=true
 TRANSPORT=grpc+rest
 REST_NUMERIC_ENUMS=true
 IS_GAPIC_LIBRARY=true
 INCLUDE_SAMPLES=true
 DESTINATION_PATH="null"
-OWLBOT_SHA=a57d2ea6d1a77aa96c17ad0850b779ec6295f88b6c1da3d214b2095d140a2066
+OWLBOT_SHA=3a95f1b9b1102865ca551b76be51d2bdb850900c4db2f6d79269e7af81ac8f84
 OWLBOT_PY_PATH="null"
-REPO_METADATA_PATH=/usr/local/google/home/diegomarquezp/Desktop/google-cloud-java/java-language/.repo-metadata.json
+REPO_METADATA_PATH=/usr/local/google/home/diegomarquezp/Desktop/google-cloud-java/java-channel/.repo-metadata.json
+MONOREPO_TAG=v1.16.0
 
 bash generate_library.sh \
 $GOOGLEAPIS_COMMIT \
@@ -30,4 +31,11 @@ $INCLUDE_SAMPLES \
 $DESTINATION_PATH \
 $OWLBOT_SHA \
 $OWLBOT_PY_PATH \
-$REPO_METADATA_PATH
+$REPO_METADATA_PATH \
+$MONOREPO_TAG
+
+cd google-cloud-java
+rm -rdf java-channel/*
+cp -r ../../library_gen_out/workspace/* java-channel/
+
+
