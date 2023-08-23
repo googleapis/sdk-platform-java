4d19614eb7392cdbe0d4732239222446a717fa12
Blake Li <blakeli@google.com>
2023-08-23 00:31:55 -0400

    Add gax thread count metric.

--- gax-java/gax/src/main/java/com/google/api/gax/tracing/ClientMetricsTracer.java
+++ gax-java/gax/src/main/java/com/google/api/gax/tracing/ClientMetricsTracer.java
@@ -4,6 +4,8 @@ public interface ClientMetricsTracer {
 
   void recordCurrentChannelSize(int channelSize);
 
+  default void recordGaxThread(int threadCount) {};
+
   default String channelSizeName() {
     return "channel_size";
   };
--- gax-java/gax/src/main/java/com/google/api/gax/tracing/OpenTelemetryClientMetricsTracer.java
+++ gax-java/gax/src/main/java/com/google/api/gax/tracing/OpenTelemetryClientMetricsTracer.java
@@ -5,18 +5,31 @@ import io.opentelemetry.api.metrics.Meter;
 
 public class OpenTelemetryClientMetricsTracer implements ClientMetricsTracer {
 
-  private final LongGaugeBuilder longGaugeBuilder;
+  private final LongGaugeBuilder channelSizeRecorder;
+  private final LongGaugeBuilder threadCountRecorder;
   private Meter meter;
 
   public OpenTelemetryClientMetricsTracer(Meter meter) {
     this.meter = meter;
-    longGaugeBuilder =
+    channelSizeRecorder =
         meter.gaugeBuilder(channelSizeName()).setDescription("Channel Size").setUnit("1").ofLongs();
+    threadCountRecorder =
+        meter
+            .gaugeBuilder("client_thread_count")
+            .setDescription("Current Thread Count created by Client")
+            .setUnit("1")
+            .ofLongs();
   }
 
   @Override
   public void recordCurrentChannelSize(int channelSize) {
-    longGaugeBuilder.buildWithCallback(
+    channelSizeRecorder.buildWithCallback(
         observableLongMeasurement -> observableLongMeasurement.record(channelSize));
+  }
+
+  @Override
+  public void recordGaxThread(int threadCount) {
+    threadCountRecorder.buildWithCallback(
+        observableLongMeasurement -> observableLongMeasurement.record(threadCount));
   }
 }
