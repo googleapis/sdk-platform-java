808a281490e7d363f9bf06d0fb0483f96e50e8cb
Diego Marquez <diegomarquezp@google.com>
2023-08-30 20:35:04 +0000

    fix local dev script

--- library_generation/run.sh
+++ library_generation/run.sh
@@ -4,7 +4,11 @@ pushd google-cloud-java
 git reset --hard
 git clean -d -f
 popd
+workspace=$(pwd)
 bash generate_library_integration_test.sh \
   -p google/cloud/channel/v1 \
   -m java-channel \
   -d google-cloud-channel-v1
+cd $workspace
+cp -r $workspace/google-cloud-channel-v1/workspace/* $workspace/google-cloud-java/java-channel
+
