10aeec14324b5e5db1c04aca4b3fad3f45d35de1
Diego Marquez <diegomarquezp@google.com>
2023-09-01 18:00:39 +0000

    remove unused functions in verify.sh

--- showcase/scripts/verify.sh
+++ showcase/scripts/verify.sh
@@ -5,25 +5,6 @@ set -o errexit
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 SHOWCASE_DIR="$SCRIPT_DIR/.."
 
-clear_existing() {
-  cd "$SHOWCASE_DIR/$1"
-  find . -name '*.java' -not -path '*/it/*' -delete
-  find . -name 'gapic_metadata.json' -delete
-  find . -name 'reflect-config.json' -delete
-  cd -
-}
-create_unpack_dir() {
-  cd "$BAZEL_ROOT"
-  rm -rf "$1"
-  mkdir "$1"
-  cd "$1"
-}
-delete_unneeded() {
-  find . -name '.DS_Store' -delete
-  find . -name 'PlaceholderFile.java' -delete
-  find . -type d -empty -delete
-}
-
 case $1 in
   proto)
     PROTO_PROJECT_DIR='proto-gapic-showcase-v1beta1'
