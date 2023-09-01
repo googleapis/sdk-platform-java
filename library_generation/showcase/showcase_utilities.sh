ede73ff8255e1b4da32749746cf97f5b15491f92
Diego Marquez <diegomarquezp@google.com>
2023-09-01 01:41:02 +0000

    explicit output folder path check

--- library_generation/showcase/showcase_utilities.sh
+++ library_generation/showcase/showcase_utilities.sh
@@ -3,7 +3,7 @@ set -ex
 script_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 function generateShowcaseIfMissing {
-  if [ ! -d showcase-output ]; then
+  if [ ! -d $script_dir/showcase-output ]; then
     bash $script_dir/generate_showcase.sh
   fi
 }
