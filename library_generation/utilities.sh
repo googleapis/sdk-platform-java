f560f024a18438025971b98c4c3bb80119ff8ee3
Diego Marquez <diegomarquezp@google.com>
2023-09-21 18:45:19 +0000

    copy_from to return value instead of `exit`

--- library_generation/utilities.sh
+++ library_generation/utilities.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
 set -xeo pipefail
-util_script_dir=$(dirname "${BASH_SOURCE}")
 
 # Utility functions used in `generate_library.sh` and showcase generation.
 extract_folder_name() {
@@ -76,18 +75,7 @@ remove_grpc_version() {
 
 download_gapic_generator_pom_parent() {
   local gapic_generator_version=$1
-  if [ ! -f "gapic-generator-java-pom-parent-${gapic_generator_version}.pom" ]; then
-    # first, try to fetch the generator pom locally
-    bash -c "source ${util_script_dir}/utilities.sh && copy_from \"$HOME/.m2/repository/com/google/api/gapic-generator-java-pom-parent/${gapic_generator_version}/gapic-generator-java-pom-parent-${gapic_generator_version}.pom\" \
-    \"gapic-generator-java-pom-parent-${gapic_generator_version}.pom\"" || not_found_locally="true"
-    if [[ "${not_found_locally}" == "true" ]];then 
-      # download gapic-generator-java-pom-parent from Google maven central mirror.
-      download_from \
-      "https://maven-central.storage-download.googleapis.com/maven2/com/google/api/gapic-generator-java-pom-parent/${gapic_generator_version}/gapic-generator-java-pom-parent-${gapic_generator_version}.pom" \
-      "gapic-generator-java-pom-parent-${gapic_generator_version}.pom"
-    fi
-  fi
-  # file exists, do not need to download again.
+  download_generator_artifact "${gapic_generator_version}" "gapic-generator-java-pom-parent-${gapic_generator_version}.pom" "gapic-generator-java-pom-parent"
 }
 
 get_grpc_version() {
@@ -114,24 +102,27 @@ download_tools() {
   local protobuf_version=$2
   local grpc_version=$3
   local os_architecture=$4
-  download_generator "${gapic_generator_version}"
+  download_generator_artifact "${gapic_generator_version}" "gapic-generator-java-${gapic_generator_version}.jar"
   download_protobuf "${protobuf_version}" "${os_architecture}"
   download_grpc_plugin "${grpc_version}" "${os_architecture}"
   popd
 }
 
-download_generator() {
+download_generator_artifact() {
   local gapic_generator_version=$1
+  local artifact=$2
+  local project=${3:-"gapic-generator-java"}
   if [ ! -f "gapic-generator-java-${gapic_generator_version}.jar" ]; then
     # first, try to fetch the generator locally
-    bash -c "source ${util_script_dir}/utilities.sh && copy_from \"$HOME/.m2/repository/com/google/api/gapic-generator-java/${gapic_generator_version}/gapic-generator-java-${gapic_generator_version}.jar\" \
-    \"gapic-generator-java-${gapic_generator_version}.jar\"" || not_found_locally="true"
+    local not_found_locally=$(copy_from "$HOME/.m2/repository/com/google/api/${project}/${gapic_generator_version}/${artifact}" \
+      "${artifact}")
     if [[ "${not_found_locally}" == "true" ]];then 
-      # download gapic-generator-java from Google maven central mirror if not
+      # download gapic-generator-java artifact from Google maven central mirror if not
       # found locally
+      echo "${artifact} not found locally. Attempting a download from Maven Central"
       download_from \
-      "https://maven-central.storage-download.googleapis.com/maven2/com/google/api/gapic-generator-java/${gapic_generator_version}/gapic-generator-java-${gapic_generator_version}.jar" \
-      "gapic-generator-java-${gapic_generator_version}.jar"
+      "https://maven-central.storage-download.googleapis.com/maven2/com/google/api/${project}/${gapic_generator_version}/${artifact}" \
+      "${artifact}"
     fi
   fi
 }
@@ -174,17 +165,19 @@ download_from() {
   curl -LJ -o "${save_as}" --fail -m 30 --retry 2 "$url" || download_fail "${save_as}" "${repo}"
 }
 
+# copies the specified file in $1 to $2
+# will print "true" if $1 (local_repo) points to a non-existent file
 copy_from() {
   local local_repo=$1
   local save_as=$2
-  cp "${local_repo}" "${save_as}" || \
-    download_fail "${save_as}" "maven local"
+  local not_found_locally=$(cp "${local_repo}" "${save_as}" && echo "false" || echo "true")
+  echo "${not_found_locally}"
 }
 
 download_fail() {
   local artifact=$1
   local repo=${2:-"maven central mirror"}
-  >&2 echo "Fail to download ${artifact} from ${repo} repository. Please install ${artifact} first if you want to download a SNAPSHOT."
+  >&2 echo "Fail to download ${artifact} from ${repo} repository. Please install ${artifact} first if you want to use a non-published artifact."
   exit 1
 }
 
