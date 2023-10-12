0127e7fa1b9c93f24fb6c583ae62cb575263abde
Diego Marquez <diegomarquezp@google.com>
2023-10-12 15:29:14 +0000

    cleanup of IT

--- library_generation/test/generate_library_integration_test.sh
+++ library_generation/test/generate_library_integration_test.sh
@@ -3,14 +3,16 @@
 set -xeo pipefail
 
 # This script is used to test the result of `generate_library.sh` against generated
-# source code in google-cloud-java repository.
+# source code in the specified repository.
 # Specifically, this script will do
 # 1. checkout the master branch of googleapis/google and WORKSPACE
 # 2. parse version of gapic-generator-java, protobuf and grpc from WORKSPACE
 # 3. generate a library with proto_path and destination_path in a proto_path
 #    list by invoking `generate_library.sh`. GAPIC options to generate a library
 #    will be parsed from proto_path/BUILD.bazel.
-# 4. checkout the master branch google-cloud-java repository and compare the result.
+# 4. depending on whether postprocessing is enabled,
+#   4.1 checkout the master branch of googleapis-gen repository and compare the result, or
+#   4.2 checkout the master branch of google-cloud-java or HW library repository and compare the result
 
 # defaults
 googleapis_gen_url="git@github.com:googleapis/googleapis-gen.git"
@@ -19,6 +21,7 @@ proto_path_list="${script_dir}/resources/proto_path_list.txt"
 library_generation_dir="${script_dir}"/..
 source "${script_dir}/test_utilities.sh"
 output_folder="$(pwd)/output"
+enable_postprocessing="true"
 
 while [[ $# -gt 0 ]]; do
 key="$1"
@@ -47,10 +50,6 @@ esac
 shift # past argument or value
 done
 
-if [ -z $enable_postprocessing ]; then
-  enable_postprocessing="true"
-fi
-
 script_dir=$(dirname "$(readlink -f "$0")")
 source "${script_dir}/../utilities.sh"
 library_generation_dir="${script_dir}"/..
@@ -74,8 +73,6 @@ popd # output
 
 grep -v '^ *#' < "${proto_path_list}" | while IFS= read -r line; do
   proto_path=$(echo "$line" | cut -d " " -f 1 | sed 's/,/ /g')
-  # the first one is the main one
-  first_proto_path=$(echo "${proto_path}" | cut -d " " -f1)
   destination_path=$(echo "$line" | cut -d " " -f 2)
   repository_path=$(echo "$line" | cut -d " " -f 3)
   more_versions_coming=$(echo "${line}" | cut -d " " -f 4)
@@ -83,7 +80,7 @@ grep -v '^ *#' < "${proto_path_list}" | while IFS= read -r line; do
   custom_gapic_name=$(echo "$line" | cut -d " " -f 6)
   # parse GAPIC options from proto_path/BUILD.bazel
   pushd "${output_folder}"
-  proto_build_file_path="${first_proto_path}/BUILD.bazel"
+  proto_build_file_path="${proto_path}/BUILD.bazel"
   proto_only=$(get_proto_only_from_BUILD "${proto_build_file_path}")
   gapic_additional_protos=$(get_gapic_additional_protos_from_BUILD "${proto_build_file_path}")
   transport=$(get_transport_from_BUILD "${proto_build_file_path}")
@@ -92,7 +89,7 @@ grep -v '^ *#' < "${proto_path_list}" | while IFS= read -r line; do
   popd # output_folder
   echo "GAPIC options are transport=${transport}, rest_numeric_enums=${rest_numeric_enums}, include_samples=${include_samples}."
   # generate GAPIC client library
-  echo "Generating library from ${first_proto_path}, to ${destination_path}..."
+  echo "Generating library from ${proto_path}, to ${destination_path}..."
   if [ $enable_postprocessing == "true" ]; then
     if [[ "${repository_path}" == "null" ]]; then
       # we need a repository to compare the generated results with. Skip this
@@ -107,13 +104,14 @@ grep -v '^ *#' < "${proto_path_list}" | while IFS= read -r line; do
         git clone "https://github.com/googleapis/${hw_library}.git"
       fi
       target_folder="${output_folder}/${hw_library}"
-      owlbot_sha=$(grep 'sha256' "${target_folder}/.github/.OwlBot.lock.yaml" | cut -d: -f3)
-    else 
+    else
       echo 'this is a monorepo library'
       pushd "${output_folder}"
       if [ ! -d "google-cloud-java" ]; then
         sparse_clone "https://github.com/googleapis/google-cloud-java.git" "${repository_path} google-cloud-pom-parent google-cloud-jar-parent versions.txt .github"
       fi
+      # compute path from output_folder to source of truth library location
+      # (e.g. google-cloud-java/java-compute)
       repository_path="google-cloud-java/${repository_path}"
       target_folder="${output_folder}/${repository_path}"
     fi
@@ -124,6 +122,7 @@ grep -v '^ *#' < "${proto_path_list}" | while IFS= read -r line; do
     repo_metadata_json_path="${script_dir}/resources/repo_metadatas/$(echo "${repository_path}" | cut -d: -f2).json"
     if [ ! -f "${repo_metadata_json_path}" ]; then
       echo 'using default repo_metadata.json file'
+      # an empty string forces generate_library.sh to infer the default json
       repo_metadata_json_path=""
     fi
 
@@ -160,7 +159,11 @@ grep -v '^ *#' < "${proto_path_list}" | while IFS= read -r line; do
   echo "Generate library finished."
   echo "Compare generation result..."
   pushd "${output_folder}"
-  if [ $enable_postprocessing == "true" ] && [ "${more_versions_coming}" == "false" ]; then
+  if [ $enable_postprocessing == "true" ]; then
+    if [ "${more_versions_coming}" != "false" ]; then
+      # we only do a diff check when all versions have been processed
+      continue
+    fi
     echo "Checking out repository..."
     cp -r ${output_folder}/workspace/* "${target_folder}"
     pushd "${target_folder}"
@@ -179,17 +182,15 @@ grep -v '^ *#' < "${proto_path_list}" | while IFS= read -r line; do
     popd # target_folder
     if [[ ${SOURCE_DIFF_RESULT} == 0 ]] && [[ ${POM_DIFF_RESULT} == 0 ]] ; then
       echo "SUCCESS: Comparison finished, no difference is found."
-      # this is the last api version being processed. Delete google-cloud-java to
-      # allow a sparse clone of the next library
-      # We only perform this action here in case the script has failed to keep
-      # the folder for further investigation
-      if [ "${repository_path}" == "hw:*" ]; then
+      # this is the last api version being processed. Delete google-cloud-java or 
+      # the handwritten repo to allow a sparse clone of the next library
+      if [ "${is_handwritten}" == "true" ]; then
         rm -rdf "${hw_library}"
       else
         rm -rdf google-cloud-java
       fi
     elif [ ${SOURCE_DIFF_RESULT} != 0 ]; then
-      echo "FAILURE: Differences found in proto path: ${first_proto_path}."
+      echo "FAILURE: Differences found in proto path: ${proto_path}."
       exit "${SOURCE_DIFF_RESULT}"
     elif [ ${POM_DIFF_RESULT} != 0 ]; then
       echo "FAILURE: Differences found in generated poms"
@@ -199,9 +200,9 @@ grep -v '^ *#' < "${proto_path_list}" | while IFS= read -r line; do
     # include gapic_metadata.json and package-info.java after
     # resolving https://github.com/googleapis/sdk-platform-java/issues/1986
     echo "Checking out googleapis-gen repository..."
-    sparse_clone "${googleapis_gen_url}" "${first_proto_path}/${destination_path}"
+    sparse_clone "${googleapis_gen_url}" "${proto_path}/${destination_path}"
     SOURCE_DIFF_RESULT=0
-    diff --strip-trailing-cr -r "googleapis-gen/${first_proto_path}/${destination_path}" "${output_folder}/${destination_path}" \
+    diff --strip-trailing-cr -r "googleapis-gen/${proto_path}/${destination_path}" "${output_folder}/${destination_path}" \
       -x "*gradle*" \
       -x "gapic_metadata.json" \
       -x "package-info.java" || SOURCE_DIFF_RESULT=$?
