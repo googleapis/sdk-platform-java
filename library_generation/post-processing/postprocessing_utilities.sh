0c17333007004463d30ed67afdc8ab816d2d0dcf
Diego Marquez <diegomarquezp@google.com>
2023-10-12 15:00:27 +0000

    comments and cleanup on postprocessing

--- library_generation/post-processing/postprocessing_utilities.sh
+++ library_generation/post-processing/postprocessing_utilities.sh
@@ -34,6 +34,15 @@ get_repo_metadata_json_or_default() {
 # 4 - include_samples: used to tell if samples are being generated/processed
 # 5 - scripts_root: location of the generation scripts
 # 6 - destination_path: used to transfer the raw grpc, proto and gapic libraries
+# 7 - api_version: version string of the library (e.g. v1beta1)
+# 8 - transport: used to decide if the grpc library should be processed
+# 9 - repository_path: path from output_folder to the location of the source of
+# truth/pre-existing poms. This can either be a folder in google-cloud-java or
+# the root of a HW library
+# 10 - more_versions_coming: some libraries (e.g. bitable) require other
+# libraries to be present in the workspace before running post-processing. This
+# flag disables the postprocessor when it's 'true', in order to run it only at
+# the final version.
 function run_owlbot_postprocessor {
   workspace=$1
   owlbot_sha=$2
@@ -48,6 +57,7 @@ function run_owlbot_postprocessor {
 
   repository_root=$(echo "${repository_path}" | cut -d/ -f1)
 
+  # read or infer owlbot sha
   if [ -z "${owlbot_sha}" ]; then
     if [ ! -d "${output_folder}/${repository_root}" ];
     then
@@ -57,21 +67,21 @@ function run_owlbot_postprocessor {
     echo "no owlbot_sha provided. Will compute from monorepo's head"
     owlbot_sha=$(grep 'sha256' "${output_folder}/${repository_root}/.github/.OwlBot.lock.yaml" | cut -d: -f3)
   fi
+
   cp "${repo_metadata_json_path}" "${workspace}"/.repo-metadata.json
 
   # call owl-bot-copy
   owlbot_staging_folder="${workspace}/owl-bot-staging"
   owlbot_image="gcr.io/cloud-devrel-public-resources/owlbot-java@sha256:${owlbot_sha}"
-
   distribution_name=$(cat "${repo_metadata_json_path}" | jq -r '.distribution_name // empty' | rev | cut -d: -f1 | rev)
   api_shortname=$(cat "${repo_metadata_json_path}" | jq -r '.api_shortname // empty')
-
-  # render owlbot.py template
+  # render default owlbot.py template
   owlbot_py_content=$(cat ""${scripts_root}"/post-processing/templates/owlbot.py.template")
-
+  # used with owl-bot-staging/
   staging_suffix="${api_version}"
 
 
+  # move generated libraries to the staging folder
   mkdir -p "${owlbot_staging_folder}/${staging_suffix}"
   gapic_folder_name="${folder_name}"
   cp -r "${destination_path}/gapic-${folder_name}-${api_version}" "${owlbot_staging_folder}/${staging_suffix}/${gapic_folder_name}"
@@ -85,7 +95,7 @@ function run_owlbot_postprocessor {
 
   echo "${owlbot_py_content}" > "${workspace}/owlbot.py"
 
-  # copy existing pom files if google-cloud-java is present
+  # copy existing pom, owlbot and version files if the source of truth repo is present
   if [[ -n "${output_folder}/${repository_path}" ]]; then
     rsync -avm \
       --include='*/' \
@@ -106,47 +116,12 @@ function run_owlbot_postprocessor {
     docker run --rm -v "${workspace}:/workspace" --user $(id -u):$(id -g) "${owlbot_image}"
   fi
 
+  # get existing versions.txt from downloaded repository
   if [ -d "${output_folder}/google-cloud-java" ];then
-    # get existing versions.txt from downloaded monorepo
     cp "${output_folder}/google-cloud-java/versions.txt" "${workspace}"
     pushd "${workspace}"
     bash "${scripts_root}/post-processing/apply_current_versions.sh"
     rm versions.txt
     popd # workspace
-  fi
-}
-
-
-# calls several scripts to perform post processing for new libraries after owlbot is
-# used. 
-function new_library_scripts {
-  scripts_root=$1
-  workspace=$2
-  repo_metadata_json_path=$3
-  output_folder=$4
-  repository_path=$5
-
-  if [[ "${is_new_library}" != "true" ]];then
-    return
-  fi
-
-  # postprocessor cleanup
-
-  pushd "${output_folder}"
-  if [ -d google-cloud-java ]; then
-    pushd google-cloud-java
-    jar_parent_pom="$(pwd)/google-cloud-jar-parent/pom.xml"
-    pom_parent_pom="$(pwd)/google-cloud-pom-parent/pom.xml"
-    popd
-    popd
-    bash "${scripts_root}/post-processing/set_parent_pom.sh" "${workspace}/pom.xml" "${jar_parent_pom}" '../google-cloud-jar-parent/pom.xml'
-    workspace_bom=$(find "${workspace}" -wholename '*-bom/pom.xml')
-    pushd "${workspace}"
-    bash "${scripts_root}/post-processing/set_parent_pom.sh" "${workspace_bom}" "${pom_parent_pom}" '../../google-cloud-pom-parent/pom.xml'
-    popd
-
-  else
-    echo 'google-cloud-java not found. Will not update parent poms nor update versions'
-    popd
   fi
 }
