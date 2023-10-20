0ffb4c52cb58f9527ecfef8f53005f8ef4f37fc4
Diego Marquez <diegomarquezp@google.com>
2023-10-20 20:11:20 +0000

    add debugging output for compare_poms

--- library_generation/test/compare_poms.py
+++ library_generation/test/compare_poms.py
@@ -57,18 +57,26 @@ the argument print_whole_trees determines if both trees should be printed
 """
 def compare_xml(file1, file2, print_whole_trees):
   try:
+    eprint('checkpoint 2')
     tree1 = ET.parse(file1)
+    eprint('checkpoint 3')
     tree2 = ET.parse(file2)
+    eprint('checkpoint 4')
   except ET.ParseError as e:
+    eprint('checkpoint 5')
     return f'Error parsing XML: {e}'
   except FileNotFoundError as e:
+    eprint('checkpoint 6')
     return f'Error reading file: {e}'
 
   tree1_elements = []
   tree2_elements = []
 
+  eprint('checkpoint 7')
   append_to_element_list(tree1.getroot(), '/', tree1_elements)
+  eprint('checkpoint 8')
   append_to_element_list(tree2.getroot(), '/', tree2_elements)
+  eprint('checkpoint 9')
 
   tree1_counter = Counter(tree1_elements)
   tree2_counter = Counter(tree2_elements)
@@ -100,7 +108,9 @@ if __name__ == "__main__":
   file1 = sys.argv[1]
   file2 = sys.argv[2]
   print_whole_trees = sys.argv[3]
+  eprint('checkpoint 0')
   has_diff = compare_xml(file1, file2, print_whole_trees)
+  eprint('checkpoint 1')
 
   if has_diff:
     eprint(f'The poms are different')
