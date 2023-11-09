07e6e518a9eac19062370d7f0808c927c3dabcbc
Alice Li <alicejli@google.com>
2023-11-09 15:46:50 -0500

    update integration goldens

--- test/integration/goldens/compute/src/com/google/cloud/compute/v1small/AddressesClient.java
+++ test/integration/goldens/compute/src/com/google/cloud/compute/v1small/AddressesClient.java
@@ -63,88 +63,19 @@ import javax.annotation.Generated;
  * <p>Note: close() needs to be called on the AddressesClient object to clean up resources such as
  * threads. In the example above, try-with-resources is used, which automatically calls close().
  *
- * <table>
- *    <tr>
- *      <th>Method</th>
- *      <th>Description</th>
- *      <th>Method Variants</th>
- *    <tr>
- *      <td>Delete</td>
- *      <td><p>Deletes the specified address resource.</p></td>
- *      <td>
- *      <p>Request object method variants only takes one parameter, a request object, which must be constructed before the call.</p>
- *      <ul>
- *      <li>deleteAsync(DeleteAddressRequest request)
- *      </ul>
- *      <p>Methods that return long-running operations have "Async" method variants that return `OperationFuture` which is used to track polling of the service.</p>
- *      <ul>
- *      <li>deleteAsync(String project)
- *      </ul>
- *      <p>Callable method variants take no parameters and returns an immutable API callable object, which can be used to initiate calls to the service.</p>
- *      <ul>
- *      <li>deleteOperationCallable()
- *      <li>deleteCallable()
- *      </ul>
- *    </td>
- *    </tr>
- *    <tr>
- *      <td>List</td>
- *      <td><p>Retrieves a list of addresses contained within the specified region.</p></td>
- *      <td>
- *      <p>Request object method variants only takes one parameter, a request object, which must be constructed before the call.</p>
- *      <ul>
- *      <li>list(ListAddressesRequest request)
- *      </ul>
- *      <p>"Flattened" method variants have the fields of the request type converted into function parameters to enable multiple ways to call the same method.</p>
- *      <ul>
- *      <li>list(String project)
- *      </ul>
- *      <p>Callable method variants take no parameters and returns an immutable API callable object, which can be used to initiate calls to the service.</p>
- *      <ul>
- *      <li>listPagedCallable()
- *      <li>listCallable()
- *      </ul>
- *    </td>
- *    </tr>
- *    <tr>
- *      <td>Insert</td>
- *      <td><p>Creates an address resource in the specified project by using the data included in the request.</p></td>
- *      <td>
- *      <p>Request object method variants only takes one parameter, a request object, which must be constructed before the call.</p>
- *      <ul>
- *      <li>insertAsync(InsertAddressRequest request)
- *      </ul>
- *      <p>Methods that return long-running operations have "Async" method variants that return `OperationFuture` which is used to track polling of the service.</p>
- *      <ul>
- *      <li>insertAsync(String project)
- *      </ul>
- *      <p>Callable method variants take no parameters and returns an immutable API callable object, which can be used to initiate calls to the service.</p>
- *      <ul>
- *      <li>insertOperationCallable()
- *      <li>insertCallable()
- *      </ul>
- *    </td>
- *    </tr>
- *    <tr>
- *      <td>AggregatedList</td>
- *      <td><p>Retrieves an aggregated list of addresses.</p></td>
- *      <td>
- *      <p>Request object method variants only takes one parameter, a request object, which must be constructed before the call.</p>
- *      <ul>
- *      <li>aggregatedList(AggregatedListAddressesRequest request)
- *      </ul>
- *      <p>"Flattened" method variants have the fields of the request type converted into function parameters to enable multiple ways to call the same method.</p>
- *      <ul>
- *      <li>aggregatedList(String project)
- *      </ul>
- *      <p>Callable method variants take no parameters and returns an immutable API callable object, which can be used to initiate calls to the service.</p>
- *      <ul>
- *      <li>aggregatedListPagedCallable()
- *      <li>aggregatedListCallable()
- *      </ul>
- *    </td>
- *    </tr>
- *  </table>
+ * <p>The surface of this class includes several types of Java methods for each of the API's
+ * methods:
+ *
+ * <ol>
+ *   <li>A "flattened" method. With this type of method, the fields of the request type have been
+ *       converted into function parameters. It may be the case that not all fields are available as
+ *       parameters, and not every API method will have a flattened method entry point.
+ *   <li>A "request object" method. This type of method only takes one parameter, a request object,
+ *       which must be constructed before the call. Not every API method will have a request object
+ *       method.
+ *   <li>A "callable" method. This type of method takes no parameters and returns an immutable API
+ *       callable object, which can be used to initiate calls to the service.
+ * </ol>
  *
  * <p>See the individual methods for example code.
  *
--- test/integration/goldens/iam/src/com/google/iam/v1/IAMPolicyClient.java
+++ test/integration/goldens/iam/src/com/google/iam/v1/IAMPolicyClient.java
@@ -72,65 +72,19 @@ import javax.annotation.Generated;
  * <p>Note: close() needs to be called on the IAMPolicyClient object to clean up resources such as
  * threads. In the example above, try-with-resources is used, which automatically calls close().
  *
- * <table>
- *    <tr>
- *      <th>Method</th>
- *      <th>Description</th>
- *      <th>Method Variants</th>
- *    <tr>
- *      <td>GetIamPolicy</td>
- *      <td><p>Gets the access control policy for a resource.
- *  Returns an empty policy if the resource exists and does not have a policy
- *  set.</p></td>
- *      <td>
- *      <p>Request object method variants only takes one parameter, a request object, which must be constructed before the call.</p>
- *      <ul>
- *      <li>getIamPolicy(GetIamPolicyRequest request)
- *      </ul>
- *      <p>Callable method variants take no parameters and returns an immutable API callable object, which can be used to initiate calls to the service.</p>
- *      <ul>
- *      <li>getIamPolicyCallable()
- *      </ul>
- *    </td>
- *    </tr>
- *    <tr>
- *      <td>TestIamPermissions</td>
- *      <td><p>Returns permissions that a caller has on the specified resource.
- *  If the resource does not exist, this will return an empty set of
- *  permissions, not a `NOT_FOUND` error.
+ * <p>The surface of this class includes several types of Java methods for each of the API's
+ * methods:
  *
- *  Note: This operation is designed to be used for building permission-aware
- *  UIs and command-line tools, not for authorization checking. This operation
- *  may "fail open" without warning.</p></td>
- *      <td>
- *      <p>Request object method variants only takes one parameter, a request object, which must be constructed before the call.</p>
- *      <ul>
- *      <li>testIamPermissions(TestIamPermissionsRequest request)
- *      </ul>
- *      <p>Callable method variants take no parameters and returns an immutable API callable object, which can be used to initiate calls to the service.</p>
- *      <ul>
- *      <li>testIamPermissionsCallable()
- *      </ul>
- *    </td>
- *    </tr>
- *    <tr>
- *      <td>SetIamPolicy</td>
- *      <td><p>Sets the access control policy on the specified resource. Replaces any
- *  existing policy.
- *
- *  Can return `NOT_FOUND`, `INVALID_ARGUMENT`, and `PERMISSION_DENIED` errors.</p></td>
- *      <td>
- *      <p>Request object method variants only takes one parameter, a request object, which must be constructed before the call.</p>
- *      <ul>
- *      <li>setIamPolicy(SetIamPolicyRequest request)
- *      </ul>
- *      <p>Callable method variants take no parameters and returns an immutable API callable object, which can be used to initiate calls to the service.</p>
- *      <ul>
- *      <li>setIamPolicyCallable()
- *      </ul>
- *    </td>
- *    </tr>
- *  </table>
+ * <ol>
+ *   <li>A "flattened" method. With this type of method, the fields of the request type have been
+ *       converted into function parameters. It may be the case that not all fields are available as
+ *       parameters, and not every API method will have a flattened method entry point.
+ *   <li>A "request object" method. This type of method only takes one parameter, a request object,
+ *       which must be constructed before the call. Not every API method will have a request object
+ *       method.
+ *   <li>A "callable" method. This type of method takes no parameters and returns an immutable API
+ *       callable object, which can be used to initiate calls to the service.
+ * </ol>
  *
  * <p>See the individual methods for example code.
  *
