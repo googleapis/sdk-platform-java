fa774a2fae3cdcf96ccdb569e5bfc4fdbef14eb2
diegomarquezp <diegomarquezp@google.com>
2023-11-10 20:17:43 +0000

    add license and comment to fake log handler

--- gax-java/gax/src/test/java/com/google/api/gax/util/FakeLogHandler.java
+++ gax-java/gax/src/test/java/com/google/api/gax/util/FakeLogHandler.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2023 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.api.gax.util;
 
 import java.util.ArrayList;
@@ -6,6 +21,10 @@ import java.util.logging.Handler;
 import java.util.logging.LogRecord;
 import java.util.stream.Collectors;
 
+/*
+ * Convenience class that stores the log entries produced by any logger
+ * It can then be inspected - its entries are a list log records
+ */
 public class FakeLogHandler extends Handler {
   List<LogRecord> records = new ArrayList<>();
 
