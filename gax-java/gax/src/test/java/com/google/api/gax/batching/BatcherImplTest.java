1733aace268c01779439f8e465b1b49b4f550047
Tomo Suzuki <suztomo@google.com>
2023-12-05 12:27:49 -0500

    format

--- gax-java/gax/src/test/java/com/google/api/gax/batching/BatcherImplTest.java
+++ gax-java/gax/src/test/java/com/google/api/gax/batching/BatcherImplTest.java
@@ -906,7 +906,8 @@ public class BatcherImplTest {
         long afterGetCall = System.currentTimeMillis();
         long actualWaitTimeMs = afterGetCall - beforeGetCall;
 
-        // In a flaky test troubleshooting, we observed that "future.get" method might not throw TimeoutException
+        // In a flaky test troubleshooting, we observed that "future.get" method might not throw
+        // TimeoutException
         // in a timely manner. It's because the main thread is not always having CPU resource.
         // As long as the "future.get" does not return within the timeout, this test is good.
         logger.info("future.get(10 ms) unexpectedly returned. Wait time: " + actualWaitTimeMs);
