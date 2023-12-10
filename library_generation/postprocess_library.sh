86e6a3fae2440970c332494efd0e8d06d022de89
diegomarquezp <diegomarquezp@google.com>
2023-12-10 21:43:41 +0000

    cleanup

--- library_generation/postprocess_library.sh
+++ library_generation/postprocess_library.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 #
-# Main functions to interact with owlbot post-processor and postprocessing
-# scripts
+# Main functions to interact with owlbot post-processor
 
-
-# Runs the owlbot post-processor docker image. The resulting post-processed
+# Runs the java owlbot post-processor. The resulting post-processed
 # library gets stored in the $postprocessing_target argument
 # Arguments
 # 1 - postprocessing_target: path where the postprocessor will run. This folder
@@ -28,7 +26,7 @@ for owlbot_file in ".repo-metadata.json" "owlbot.py" ".OwlBot.yaml"
 do
   if [[ $(find "${postprocessing_target}" -name '.repo-metadata.json' | wc -l) -eq 0 ]]; then
     echo "necessary file for postprocessing '${owlbot_file}' was not found in postprocessing_target"
-    echo "please provide a postprocessing_target that is owlbot compatible"
+    echo "please provide a postprocessing_target folder that is java owlbot compatible"
     exit 1
   fi
 done
@@ -39,7 +37,8 @@ proto_path=$(get_proto_path_from_preprocessed_sources "${preprocessed_sources_pa
 eval "$(pyenv init --path)"
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
-# create python virtualenv
+
+# create and python virtualenv
 python_version=$(cat "${scripts_root}/configuration/python-version")
 if [ $(pyenv versions | grep "${python_version}" | wc -l) -eq 0 ]; then
   pyenv install "${python_version}"
@@ -52,13 +51,12 @@ pyenv activate "postprocessing"
 # call owl-bot-copy
 owlbot_staging_folder="${postprocessing_target}/owl-bot-staging"
 mkdir -p "${owlbot_staging_folder}"
-
-
 echo 'Running owl-bot-copy'
 pre_processed_libs_folder=$(mktemp -d)
 # By default (thanks to generation templates), .OwlBot.yaml `deep-copy` section
 # references a wildcard pattern matching a folder
-# ending with `-java` at the leaf of proto_path. 
+# ending with `-java` at the leaf of proto_path. We then use a generated-java
+# folder that will be picked up by copy-code
 mkdir -p "${pre_processed_libs_folder}/${proto_path}/generated-java"
 copy_directory_if_exists "${preprocessed_sources_path}" "proto" \
   "${pre_processed_libs_folder}/${proto_path}/generated-java/proto-google-cloud-library"
@@ -70,7 +68,7 @@ copy_directory_if_exists "${preprocessed_sources_path}" "samples" \
   "${pre_processed_libs_folder}/${proto_path}/generated-java/samples"
 pushd "${pre_processed_libs_folder}"
 # create an empty repository so owl-bot-copy can process this as a repo
-# (cannot process non-git-repositories)
+# (it cannot process non-git-repositories)
 git init
 git commit --allow-empty -m 'empty commit'
 popd # pre_processed_libs_folder
@@ -114,9 +112,7 @@ python3 -m pip install -r requirements.in
 popd # synthtool
 popd # temp dir
 
-# now we use the image to call owlbot.py
-echo 'processing owlbot.py'
-
+# we install the owlbot requirements
 pushd "${scripts_root}/owlbot/src/"
 python3 -m pip install -r requirements.in
 popd # owlbot/src
@@ -124,5 +120,5 @@ popd # owlbot/src
 # run the postprocessor
 echo 'running owl-bot post-processor'
 pushd "${postprocessing_target}"
-bash "${scripts_root}/owlbot/bin/entrypoint.sh" "${scripts_root}" "${versions_file}" "${synthtool_image_id}"
+bash "${scripts_root}/owlbot/bin/entrypoint.sh" "${scripts_root}" "${versions_file}"
 popd # postprocessing_target
