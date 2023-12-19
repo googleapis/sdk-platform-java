61db4c8d081c33cef69a76a1dafefc434267e253
Alice Li <alicejli@google.com>
2023-12-19 15:22:48 -0500

    add todo

--- gapic-generator-java/src/main/java/com/google/api/generator/gapic/protoparser/Parser.java
+++ gapic-generator-java/src/main/java/com/google/api/generator/gapic/protoparser/Parser.java
@@ -1161,8 +1161,7 @@ public class Parser {
 
   /**
    * Converts a serviceYaml file to a map of methods and autopopulated fields
-   *
-   * @param
+   * TODO: Confirm whether or not wildcards need to be supported. If so, update this logic.
    */
   @VisibleForTesting
   static ImmutableMap<String, List<String>> parseAutoPopulatedMethodsAndFields(
