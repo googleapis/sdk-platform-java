e31cf7f8539c0cb0cf3b07042a8444d843440678
diegomarquezp <diegomarquezp@google.com>
2024-01-03 17:05:03 +0000

    correct argument preparation

--- library_generation/test/generate_library_integration_test.sh
+++ library_generation/test/generate_library_integration_test.sh
@@ -83,6 +83,8 @@ grep -v '^ *#' < "${proto_path_list}" | while IFS= read -r line; do
   for proto_path in "${proto_paths[@]}"; do
     sparse_clone "${googleapis_gen_url}" "${proto_path}"
     queries="proto_path=${proto_path}"
+    queries="${queries},gapic_generator_version=${gapic_generator_version}"
+    queries="${queries},protobuf_version=${protobuf_version}"
     # parse destination_path
     destination_path=$(compute_destination_path "${proto_path}" "${output_folder}")
     # parse GAPIC options from proto_path/BUILD.bazel
