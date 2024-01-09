fd024d5598193f923f5ac4c9d36c61508fa16bec
Lawrence Qiu <lawrenceqiu@google.com>
2024-01-09 13:06:17 -0500

    chore: Remove setting the default endpoint in StubSettings

---
+++ showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/ITEndpointContext.java
@@ -0,0 +1,70 @@
+package com.google.showcase.v1beta1.it;
+
+import com.google.common.truth.Truth;
+import com.google.showcase.v1beta1.EchoClient;
+import com.google.showcase.v1beta1.EchoSettings;
+import com.google.showcase.v1beta1.it.util.TestClientInitializer;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+import org.junit.Test;
+
+/**
+ * This IT tests the different user configurations allowed and their effects on endpoint and
+ * universe domain resolution.
+ *
+ * <p>This test will be enhanced in the future when the settings are able to return the resolved
+ * endpoint and universe domain values.
+ */
+public class ITEndpointContext {
+  public static final String SHOWCASE_DEFAULT_ENDPOINT = "localhost:7469";
+
+  // Default (no configuration)
+  @Test
+  public void endpointResolution_default() throws InterruptedException, IOException {
+    EchoClient echoClient = null;
+    try {
+      echoClient = EchoClient.create();
+      Truth.assertThat(echoClient.getSettings().getEndpoint()).isEqualTo(SHOWCASE_DEFAULT_ENDPOINT);
+    } finally {
+      if (echoClient != null) {
+        echoClient.close();
+        echoClient.awaitTermination(
+            TestClientInitializer.AWAIT_TERMINATION_SECONDS, TimeUnit.SECONDS);
+      }
+    }
+  }
+
+  // User configuration
+  @Test
+  public void endpointResolution_userConfiguration() throws InterruptedException, IOException {
+    String customEndpoint = "test.com:123";
+    EchoClient echoClient = null;
+    try {
+      EchoSettings echoSettings = EchoSettings.newBuilder().setEndpoint(customEndpoint).build();
+      echoClient = EchoClient.create(echoSettings);
+      Truth.assertThat(echoClient.getSettings().getEndpoint()).isEqualTo(customEndpoint);
+    } finally {
+      if (echoClient != null) {
+        echoClient.close();
+        echoClient.awaitTermination(
+            TestClientInitializer.AWAIT_TERMINATION_SECONDS, TimeUnit.SECONDS);
+      }
+    }
+  }
+
+  // Default in Builder (no configuration)
+  @Test
+  public void endpointResolution_defaultBuilder() {
+    EchoSettings.Builder echoSettingsBuilder = EchoSettings.newBuilder();
+    Truth.assertThat(echoSettingsBuilder.getEndpoint()).isEqualTo(SHOWCASE_DEFAULT_ENDPOINT);
+  }
+
+  // User configuration in Builder
+  @Test
+  public void endpointResolution_userConfigurationBuilder() {
+    String customEndpoint = "test.com:123";
+    EchoSettings.Builder echoSettingsBuilder =
+        EchoSettings.newBuilder().setEndpoint(customEndpoint);
+    Truth.assertThat(echoSettingsBuilder.getEndpoint()).isEqualTo(customEndpoint);
+  }
+}
