6847489ae983c970bb5bc1252f5396278b69a39b
Joe Wang <106995533+JoeWang1127@users.noreply.github.com>
2024-01-18 16:22:07 +0000

    chore: add custom artifacts (#2377)
    * chore: add custom artifacts in handwritten libraries
    
    * change comments.
    
    * consolidate hw pom into one

--- java-shared-dependencies/unmanaged-dependency-check/src/main/java/com/google/cloud/UnmanagedDependencyCheck.java
+++ java-shared-dependencies/unmanaged-dependency-check/src/main/java/com/google/cloud/UnmanagedDependencyCheck.java
@@ -18,9 +18,14 @@ import org.eclipse.aether.version.InvalidVersionSpecificationException;
  * A utility class to check unmanaged dependencies in BOM.
  */
 public class UnmanagedDependencyCheck {
-  // regex of handwritten artifacts
-  private final static String downstreamArtifact = "(com.google.cloud:google-.*)|(com.google.api.grpc:(grpc|proto)-google-.*)";
-
+  // Regex of handwritten artifacts.
+  // There are customized artifacts defined in some handwritten libraries, e.g., firestore and
+  // datastore, add entries in regex to exclude these artifacts.
+  private final static String downstreamArtifact =
+      "(com.google.cloud:google-.*)|"
+          + "(com.google.api.grpc:(grpc|proto)-google-.*)|"
+          + "(com.google.cloud:proto-google-cloud-firestore-bundle-.*)|"
+          + "(com.google.cloud.datastore:datastore-.*-proto-client)";
 
   /**
    * @param args An array with two elements.<p> The first string is the path of Java shared
--- java-shared-dependencies/unmanaged-dependency-check/src/test/resources/local-install.sh
+++ java-shared-dependencies/unmanaged-dependency-check/src/test/resources/local-install.sh
@@ -4,3 +4,4 @@ mvn install -f shared-dependency-3.18.0-pom.xml
 mvn install -f gax-example-pom.xml
 mvn install -f nested-dependency-pom.xml
 mvn install -f transitive-dependency-pom.xml
+mvn install -f google-internal-artifact-test-case-pom.xml
