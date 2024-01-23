d40c562d92ab57ae7e75cd86e20b88fd383dc31c
Joe Wang <joewa@google.com>
2024-01-22 20:58:56 -0500

    change pom in tests

--- java-shared-dependencies/unmanaged-dependency-check/src/test/resources/local-install.sh
+++ java-shared-dependencies/unmanaged-dependency-check/src/test/resources/local-install.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-mvn install -f shared-dependency--pom.xml
+mvn install -f shared-dependency-pom.xml
 mvn install -f gax-example-pom.xml
 mvn install -f nested-dependency-pom.xml
 mvn install -f transitive-dependency-pom.xml
