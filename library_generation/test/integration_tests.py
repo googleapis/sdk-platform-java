f7613c10b594a0b87ea639f39af5da71e4060ecc
JoeWang1127 <joewa@google.com>
2024-02-03 02:25:01 +0000

    add integration test

---
+++ library_generation/test/integration_tests.py
@@ -0,0 +1,87 @@
+# Copyright 2024 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     https://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+import shutil
+import subprocess
+import unittest
+from distutils.dir_util import copy_tree
+from distutils.file_util import copy_file
+from pathlib import Path
+from typing import List
+from typing import Dict
+from library_generation.generate_repo import generate_from_yaml
+from library_generation.model.generation_config import from_yaml
+from library_generation.utilities import get_library_name
+
+config_name = "generation_config.yaml"
+script_dir = os.path.dirname(os.path.realpath(__file__))
+# for simplicity, the configuration files should be in a relative directory
+# within config_dir named {repo}/generation_config.yaml, where repo is
+# the name of the repository the target libraries live.
+config_dir = f"{script_dir}/resources/integration"
+golden_dir = f"{config_dir}/golden"
+repo_prefix = "https://github.com/googleapis"
+committish_list = ["chore/test-hermetic-build"]  # google-cloud-java
+
+
+class IntegrationTest(unittest.TestCase):
+    def test_generate_repo(self):
+        shutil.rmtree(f"{golden_dir}", ignore_errors=True)
+        os.makedirs(f"{golden_dir}", exist_ok=True)
+        config_files = self.__get_config_files(config_dir)
+        i = 0
+        for repo, config_file in config_files.items():
+            repo_dest = f"{golden_dir}/{repo}"
+            self.__pull_repo_to(Path(repo_dest), repo, committish_list[i])
+            library_names = self.__get_library_names_from_config(config_file)
+            # prepare golden files
+            for library_name in library_names:
+                copy_tree(f"{repo_dest}/{library_name}", f"{golden_dir}/{library_name}")
+            copy_tree(
+                f"{repo_dest}/gapic-libraries-bom", f"{golden_dir}/gapic-libraries-bom"
+            )
+            copy_file(f"{repo_dest}/pom.xml", golden_dir)
+            generate_from_yaml(
+                generation_config_yaml=config_file, repository_path=repo_dest
+            )
+            # remove google-cloud-java
+            i += 1
+
+    @staticmethod
+    def __pull_repo_to(dest: Path, repo: str, committish: str):
+        repo_url = f"{repo_prefix}/{repo}"
+        subprocess.check_call(["git", "clone", repo_url, dest])
+        # subprocess.check_call(["git", "checkout", committish])
+
+    @staticmethod
+    def __get_library_names_from_config(config_path: str) -> List[str]:
+        config = from_yaml(config_path)
+        library_names = []
+        for library in config.libraries:
+            library_names.append(f"java-{get_library_name(library)}")
+
+        return library_names
+
+    @staticmethod
+    def __get_config_files(path: str) -> Dict[str, str]:
+        config_files = {}
+        for sub_dir in Path(path).resolve().iterdir():
+            repo = sub_dir.name
+            if repo == "golden":
+                continue
+            config = f"{sub_dir}/{config_name}"
+            config_files[repo] = config
+
+        return config_files
