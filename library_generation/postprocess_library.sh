63a7a48a4772748304a96281608a5e1ba97d64d3
JoeWang1127 <joewa@google.com>
2024-02-03 03:14:42 +0000

    remove pyenv

--- library_generation/postprocess_library.sh
+++ library_generation/postprocess_library.sh
@@ -50,22 +50,6 @@ do
   fi
 done
 
-
-# ensure pyenv scripts are available
-#eval "$(pyenv init --path)"
-#eval "$(pyenv init -)"
-#eval "$(pyenv virtualenv-init -)"
-
-# create and activate the python virtualenv
-#python_version=$(cat "${scripts_root}/configuration/python-version")
-#if [ $(pyenv versions | grep "${python_version}" | wc -l) -eq 0 ]; then
-#  pyenv install "${python_version}"
-#fi
-#if [ $(pyenv virtualenvs | grep "${python_version}" | grep "postprocessing" | wc -l) -eq 0 ];then
-#  pyenv virtualenv "${python_version}" "postprocessing"
-#fi
-#pyenv activate "postprocessing"
-
 if [[ -z "${owlbot_cli_source_folder}" ]]; then
   owlbot_cli_source_folder=$(mktemp -d)
   build_owlbot_cli_source_folder "${postprocessing_target}" "${owlbot_cli_source_folder}" "${preprocessed_sources_path}"
