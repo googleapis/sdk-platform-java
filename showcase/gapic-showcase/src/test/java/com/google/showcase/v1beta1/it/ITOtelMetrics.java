1b80dfab8cd5791f1ac8fb07da5e162999df1a8d
Deepankar Dixit <deepankardixit@google.com>
2024-02-09 19:08:05 +0000

    chore: fix lint

--- showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/ITOtelMetrics.java
+++ showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/ITOtelMetrics.java
@@ -16,74 +16,38 @@
 
 package com.google.showcase.v1beta1.it;
 
-import static org.junit.Assert.assertThrows;
 
 import com.google.api.client.http.javanet.NetHttpTransport;
-import com.google.api.core.ApiFuture;
-import com.google.api.core.BetaApi;
-import com.google.api.core.ListenableFutureToApiFuture;
-import com.google.api.core.SettableApiFuture;
 import com.google.api.gax.core.GaxProperties;
 import com.google.api.gax.core.NoCredentialsProvider;
-import com.google.api.gax.retrying.RetrySettings;
-import com.google.api.gax.retrying.RetryingFuture;
-import com.google.api.gax.rpc.CancelledException;
-import com.google.api.gax.rpc.NotFoundException;
 import com.google.api.gax.rpc.StatusCode.Code;
-import com.google.api.gax.rpc.UnaryCallable;
 import com.google.api.gax.tracing.ApiTracerFactory;
 import com.google.api.gax.tracing.MetricsTracerFactory;
 import com.google.api.gax.tracing.OpentelemetryMetricsRecorder;
-import com.google.common.collect.ImmutableSet;
-import com.google.common.util.concurrent.ListenableFuture;
-import com.google.common.util.concurrent.SettableFuture;
 import com.google.rpc.Status;
-import io.grpc.ManagedChannelBuilder;
-import com.google.showcase.v1beta1.BlockRequest;
-import com.google.showcase.v1beta1.BlockResponse;
 import com.google.showcase.v1beta1.EchoClient;
 import com.google.showcase.v1beta1.EchoRequest;
-import com.google.showcase.v1beta1.EchoResponse;
 import com.google.showcase.v1beta1.EchoSettings;
-import com.google.showcase.v1beta1.stub.EchoStubSettings;
-import com.google.showcase.v1beta1.WaitResponse;
-import com.google.showcase.v1beta1.it.util.TestClientInitializer;
 import io.opentelemetry.api.OpenTelemetry;
 import io.opentelemetry.api.metrics.Meter;
-import io.opentelemetry.api.metrics.ObservableLongMeasurement;
 import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
 import io.opentelemetry.sdk.OpenTelemetrySdk;
 import io.opentelemetry.sdk.metrics.SdkMeterProvider;
-import io.opentelemetry.sdk.metrics.data.MetricData;
-import io.opentelemetry.sdk.metrics.export.MetricReader;
 import io.opentelemetry.sdk.metrics.export.PeriodicMetricReader;
 import io.opentelemetry.sdk.resources.Resource;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
-import java.util.concurrent.Callable;
-import java.util.concurrent.CancellationException;
-import java.util.concurrent.ExecutionException;
-import java.util.concurrent.Future;
-import java.util.concurrent.TimeUnit;
-import org.junit.AfterClass;
-import org.junit.Before;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 public class ITOtelMetrics {
 
   // Helper function for creating opentelemetry object defined below, used in each test.
   @Test
-  public void testHttpJson_OperationSucceded_recordsMetrics()
-      throws Exception {
+  public void testHttpJson_OperationSucceded_recordsMetrics() throws Exception {
 
-    //initialize the otel-collector
-    Process process = Runtime.getRuntime().exec("../scripts/start_otelcol.sh ../opentelemetry-helper/configs/testHttpJson_OperationSucceded.yaml");
+    // initialize the otel-collector
+    Process process =
+        Runtime.getRuntime()
+            .exec(
+                "../scripts/start_otelcol.sh ../opentelemetry-helper/configs/testHttpJson_OperationSucceded.yaml");
     process.waitFor();
 
     EchoSettings httpJsonEchoSettings =
@@ -104,40 +68,41 @@ public class ITOtelMetrics {
             .setError(Status.newBuilder().setCode(Code.OK.ordinal()).build())
             .build();
     client.echoCallable().futureCall(requestWithNoError).get();
-    //wait for the metrics to get uploaded
+    // wait for the metrics to get uploaded
     Thread.sleep(3000);
-    //verify the metrics and cleanup
-    Process cleanup = Runtime.getRuntime().exec("../scripts/verify_metrics.sh ../opentelemetry-helper/metrics/testHttpJson_OperationSucceded.txt");
+    // verify the metrics and cleanup
+    Process cleanup =
+        Runtime.getRuntime()
+            .exec(
+                "../scripts/verify_metrics.sh ../opentelemetry-helper/metrics/testHttpJson_OperationSucceded.txt");
     process.waitFor();
-
   }
 
   private static ApiTracerFactory createOpenTelemetryTracerFactory() {
     // OTLP Metric Exporter setup
-    OtlpGrpcMetricExporter metricExporter = OtlpGrpcMetricExporter.builder()
-        .setEndpoint("http://localhost:4317")
-        .build();
+    OtlpGrpcMetricExporter metricExporter =
+        OtlpGrpcMetricExporter.builder().setEndpoint("http://localhost:4317").build();
 
     // Periodic Metric Reader configuration
-    PeriodicMetricReader metricReader = PeriodicMetricReader.builder(metricExporter)
-        .setInterval(java.time.Duration.ofSeconds(2))
-        .build();
+    PeriodicMetricReader metricReader =
+        PeriodicMetricReader.builder(metricExporter)
+            .setInterval(java.time.Duration.ofSeconds(2))
+            .build();
 
     // OpenTelemetry SDK Configuration
     Resource resource = Resource.builder().build();
-    SdkMeterProvider sdkMeterProvider = SdkMeterProvider.builder()
-        .registerMetricReader(metricReader)
-        .setResource(resource)
-        .build();
+    SdkMeterProvider sdkMeterProvider =
+        SdkMeterProvider.builder().registerMetricReader(metricReader).setResource(resource).build();
 
-    OpenTelemetry openTelemetry = OpenTelemetrySdk.builder()
-        .setMeterProvider(sdkMeterProvider)
-        .build();
+    OpenTelemetry openTelemetry =
+        OpenTelemetrySdk.builder().setMeterProvider(sdkMeterProvider).build();
 
     // Meter Creation
-    Meter meter = openTelemetry.meterBuilder("gax")
-        .setInstrumentationVersion(GaxProperties.getGaxVersion())
-        .build();
+    Meter meter =
+        openTelemetry
+            .meterBuilder("gax")
+            .setInstrumentationVersion(GaxProperties.getGaxVersion())
+            .build();
 
     // OpenTelemetry Metrics Recorder
     OpentelemetryMetricsRecorder otelMetricsRecorder = new OpentelemetryMetricsRecorder(meter);
