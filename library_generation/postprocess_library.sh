f096c3c248a3b010c82671211517341fd199f9ab
diegomarquezp <diegomarquezp@google.com>
2024-02-16 00:26:15 +0000

    allow mounting volumes via env vars

--- library_generation/postprocess_library.sh
+++ library_generation/postprocess_library.sh
@@ -22,7 +22,7 @@
 # different logic
 # 8 - configuration_yaml_path: path to the configuration yaml containing library
 # generation information for this library
-set -eo pipefail
+set -eox pipefail
 scripts_root=$(dirname "$(readlink -f "$0")")
 
 postprocessing_target=$1
@@ -67,10 +67,29 @@ else
   owlbot_yaml_relative_path=".github/.OwlBot.yaml"
 fi
 
+# When running docker inside docker, we run into the issue of volume bindings
+# being mapped from the host machine to the child container (instead of the
+# parent container to child container) because we bind the `docker.sock` socket
+# to the parent container (i.e. docker calls use the host's filesystem context)
+# We solve this by referencing environment variables that will be arbitrarily
+# set to produce the correct volume mapping.
+#
+# The workflow is: to check if we are in a docker container (via passed env var)
+# and use managed volumes (docker volume create) instead of bindings
+# (-v /path:/other-path). The volume names are also received as env vars.
+
+repo_binding="${postprocessing_target}"
+preprocessed_libraries_binding="${owlbot_cli_source_folder}"
+if [[ -n "${RUNNING_IN_DOCKER}" ]]; then
+  set -u # temporarily fail on unset variables
+  repo_binding="${REPO_BINDING_VOLUME}"
+  set +u
+fi
+
 docker run --rm \
   --user "$(id -u)":"$(id -g)" \
-  -v "${postprocessing_target}:/repo" \
-  -v "${owlbot_cli_source_folder}:/pre-processed-libraries" \
+  -v "${repo_binding}:/repo" \
+  -v "${preprocessed_libraries_binding}:/pre-processed-libraries" \
   -w /repo \
   --env HOME=/tmp \
   gcr.io/cloud-devrel-public-resources/owlbot-cli@"${owlbot_cli_image_sha}" \
