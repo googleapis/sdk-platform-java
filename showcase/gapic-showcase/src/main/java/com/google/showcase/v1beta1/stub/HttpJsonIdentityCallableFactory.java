894d0132cea9c724dc2b7940f3aeb7c2c3f466ea
Lawrence Qiu <lawrenceqiu@google.com>
2024-03-07 17:13:29 -0500

    chore: Update showcase tests

--- showcase/gapic-showcase/src/main/java/com/google/showcase/v1beta1/stub/HttpJsonIdentityCallableFactory.java
+++ showcase/gapic-showcase/src/main/java/com/google/showcase/v1beta1/stub/HttpJsonIdentityCallableFactory.java
@@ -81,7 +81,7 @@ public class HttpJsonIdentityCallableFactory
           ClientContext clientContext,
           OperationsStub operationsStub) {
     UnaryCallable<RequestT, Operation> innerCallable =
-        HttpJsonCallableFactory.createUnaryCallable(
+        HttpJsonCallableFactory.createBaseUnaryCallable(
             httpJsonCallSettings, callSettings.getInitialCallSettings(), clientContext);
     HttpJsonOperationSnapshotCallable<RequestT, Operation> initialCallable =
         new HttpJsonOperationSnapshotCallable<RequestT, Operation>(
