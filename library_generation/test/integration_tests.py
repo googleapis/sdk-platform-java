44a77ae5ec65e4712e11302de62fefd4b2c3b747
JoeWang1127 <joewa@google.com>
2024-03-09 03:10:25 +0000

    format python code

--- library_generation/test/integration_tests.py
+++ library_generation/test/integration_tests.py
@@ -76,7 +76,8 @@ class IntegrationTest(unittest.TestCase):
                 cmp(
                     f"{config_dir}/{repo}/pr-description-golden.txt",
                     f"{description_file}",
-                ), "The generated PR description does not match the expected golden file"
+                ),
+                "The generated PR description does not match the expected golden file",
             )
             os.remove(f"{description_file}")
 
