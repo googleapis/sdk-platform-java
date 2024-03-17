5348539342edc510c932034cfa7b49bae45ae993
JoeWang1127 <joewa@google.com>
2024-03-17 22:23:09 +0000

    add setUp method in ut

--- library_generation/test/utils/generation_config_comparator_unit_tests.py
+++ library_generation/test/utils/generation_config_comparator_unit_tests.py
@@ -17,54 +17,59 @@ from library_generation.model.generation_config import GenerationConfig
 from library_generation.utils.generation_config_comparator import ChangeType
 from library_generation.utils.generation_config_comparator import compare_config
 
-baseline_config = GenerationConfig(
-    gapic_generator_version="",
-    googleapis_commitish="",
-    owlbot_cli_image="",
-    synthtool_commitish="",
-    template_excludes=[],
-    path_to_yaml="",
-    grpc_version="",
-    protobuf_version="",
-    libraries=[]
-)
-latest_config = GenerationConfig(
-    gapic_generator_version="",
-    googleapis_commitish="",
-    owlbot_cli_image="",
-    synthtool_commitish="",
-    template_excludes=[],
-    path_to_yaml="",
-    grpc_version="",
-    protobuf_version="",
-    libraries=[]
-)
-
 
 class GenerationConfigComparatorTest(unittest.TestCase):
+    def setUp(self) -> None:
+        self.baseline_config = GenerationConfig(
+            gapic_generator_version="",
+            googleapis_commitish="",
+            owlbot_cli_image="",
+            synthtool_commitish="",
+            template_excludes=[],
+            path_to_yaml="",
+            grpc_version="",
+            protobuf_version="",
+            libraries=[],
+        )
+        self.latest_config = GenerationConfig(
+            gapic_generator_version="",
+            googleapis_commitish="",
+            owlbot_cli_image="",
+            synthtool_commitish="",
+            template_excludes=[],
+            path_to_yaml="",
+            grpc_version="",
+            protobuf_version="",
+            libraries=[],
+        )
+
     def test_compare_config_not_change(self):
         result = compare_config(
-            baseline_config=baseline_config,
-            latest_config=latest_config,
+            baseline_config=self.baseline_config,
+            latest_config=self.latest_config,
         )
         self.assertTrue(len(result) == 0)
 
     def test_compare_config_googleapis_update(self):
-        baseline_config.googleapis_commitish = "1a45bf7393b52407188c82e63101db7dc9c72026"
-        latest_config.googleapis_commitish = "1e6517ef4f949191c9e471857cf5811c8abcab84"
+        self.baseline_config.googleapis_commitish = (
+            "1a45bf7393b52407188c82e63101db7dc9c72026"
+        )
+        self.latest_config.googleapis_commitish = (
+            "1e6517ef4f949191c9e471857cf5811c8abcab84"
+        )
         result = compare_config(
-            baseline_config=baseline_config,
-            latest_config=latest_config,
+            baseline_config=self.baseline_config,
+            latest_config=self.latest_config,
         )
         self.assertTrue(ChangeType.GOOGLEAPIS_COMMIT in result)
         self.assertEqual([], result[ChangeType.GOOGLEAPIS_COMMIT])
 
     def test_compare_config_generator_update(self):
-        baseline_config.gapic_generator_version = "1.2.3"
-        latest_config.gapic_generator_version = "1.2.4"
+        self.baseline_config.gapic_generator_version = "1.2.3"
+        self.latest_config.gapic_generator_version = "1.2.4"
         result = compare_config(
-            baseline_config=baseline_config,
-            latest_config=latest_config,
+            baseline_config=self.baseline_config,
+            latest_config=self.latest_config,
         )
         self.assertTrue(ChangeType.GENERATOR in result)
         self.assertEqual([], result[ChangeType.GENERATOR])
