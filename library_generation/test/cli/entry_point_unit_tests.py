22eb1279435fc160b547154166bb32458e035637
Joe Wang <joewa@google.com>
2024-04-04 11:49:17 -0400

    restore unit tests

--- library_generation/test/cli/entry_point_unit_tests.py
+++ library_generation/test/cli/entry_point_unit_tests.py
@@ -11,29 +11,15 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-import os
 import unittest
 from click.testing import CliRunner
 from library_generation.cli.entry_point import generate
 
-script_dir = os.path.dirname(os.path.realpath(__file__))
-resource_dir = os.path.join(script_dir, "..", "resources", "entry_point")
-
 
 class EntryPointTest(unittest.TestCase):
-    def setUp(self) -> None:
-        os.chdir(script_dir)
-
     def test_entry_point_without_config_raise_file_exception(self):
         runner = CliRunner()
         # noinspection PyTypeChecker
         result = runner.invoke(generate, ["--repository-path=."])
         self.assertEqual(1, result.exit_code)
         self.assertEqual(FileNotFoundError, result.exc_info[0])
-
-    def test_entry_point_with_default_config_success(self):
-        os.chdir(resource_dir)
-        runner = CliRunner()
-        # noinspection PyTypeChecker
-        result = runner.invoke(generate, ["--repository-path=."])
-        self.assertEqual(0, result.exit_code)
