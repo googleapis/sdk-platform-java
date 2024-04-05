3a557193d9234f90fcdc15c9f385b275b7605940
Blake Li <blakeli@google.com>
2024-04-05 18:28:57 -0400

    refactor: Revert the change in postprocessing.

--- library_generation/postprocess_library.sh
+++ library_generation/postprocess_library.sh
@@ -143,7 +143,7 @@ git fetch --all
 git reset --hard "${synthtool_commitish}"
 
 python3 -m pip install -e .
-python3 -m pip install -r requirements.txt
+python3 -m pip install -r requirements.in
 popd # synthtool
 popd # temp dir
 
