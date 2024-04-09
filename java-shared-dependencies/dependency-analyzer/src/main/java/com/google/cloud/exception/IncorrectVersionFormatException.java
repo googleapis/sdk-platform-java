8dc7ed7a63067a73e7464c576accc0225acaa906
Joe Wang <joewa@google.com>
2024-04-08 20:52:24 -0400

    add version name check

---
+++ java-shared-dependencies/dependency-analyzer/src/main/java/com/google/cloud/exception/IncorrectVersionFormatException.java
@@ -0,0 +1,9 @@
+package com.google.cloud.exception;
+
+public class IncorrectVersionFormatException extends Exception{
+
+  public IncorrectVersionFormatException(String message) {
+    super(message);
+  }
+
+}
--- java-shared-dependencies/dependency-analyzer/src/main/java/com/google/cloud/model/PkgManagement.java
+++ java-shared-dependencies/dependency-analyzer/src/main/java/com/google/cloud/model/PkgManagement.java
@@ -1,5 +1,14 @@
 package com.google.cloud.model;
 
 public enum PkgManagement {
-  MAVEN
+  MAVEN;
+
+  public static boolean checkDependencyName(PkgManagement system, String name) {
+    if (system == MAVEN) {
+      String[] strs = name.split(":");
+      return strs.length == 2;
+    }
+
+    return false;
+  }
 }
