5808687fa79fbe351880d09f1b67c9badedf946e
Joe Wang <joewa@google.com>
2024-04-09 09:18:12 -0400

    add not recognized license

--- java-shared-dependencies/dependency-analyzer/src/main/java/com/google/cloud/model/License.java
+++ java-shared-dependencies/dependency-analyzer/src/main/java/com/google/cloud/model/License.java
@@ -4,13 +4,18 @@ import static com.google.cloud.model.LicenseCategory.NOTICE;
 import static com.google.cloud.model.LicenseCategory.Restricted;
 
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 
 public enum License {
   APACHE_2_0(Set.of(NOTICE)),
   BCL(Set.of(Restricted, NOTICE)),
   GL2PS(Set.of(Restricted, NOTICE)),
-  MIT(Set.of(NOTICE));
+  MIT(Set.of(NOTICE)),
+  NOT_RECOGNIZED(Set.of());
+
+  private final static Logger LOGGER = Logger.getLogger(License.class.getName());
 
   private final Set<LicenseCategory> categories;
 
@@ -23,7 +28,12 @@ public enum License {
         .toUpperCase()
         .replace('-', '_')
         .replace('.', '_');
-    return License.valueOf(value);
+    try {
+      return License.valueOf(value);
+    } catch (IllegalArgumentException exception) {
+      LOGGER.log(Level.WARNING, String.format("%s is not recognized as any of the known license.", licenseStr));
+      return NOT_RECOGNIZED;
+    }
   }
 
   public Set<LicenseCategory> getCategories() {
