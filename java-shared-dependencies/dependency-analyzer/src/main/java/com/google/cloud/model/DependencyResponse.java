4842f99e68a2bdfffb76cf1ca0a0b71edb212362
Joe Wang <joewa@google.com>
2024-04-19 08:21:51 -0400

    change DependencyResponse to record

--- java-shared-dependencies/dependency-analyzer/src/main/java/com/google/cloud/model/DependencyResponse.java
+++ java-shared-dependencies/dependency-analyzer/src/main/java/com/google/cloud/model/DependencyResponse.java
@@ -6,18 +6,15 @@ import java.util.List;
 /**
  * A resolved dependency graph for the given package version.
  * <p>
- * For more information, please refer to <a href="https://docs.deps.dev/api/v3/#getdependencies">GetDependencies</a>.
+ * For more information, please refer to <a
+ * href="https://docs.deps.dev/api/v3/#getdependencies">GetDependencies</a>.
+ *
+ * @param nodes The first node is the root of the graph.
  */
-public class DependencyResponse {
+public record DependencyResponse(List<Node> nodes) {
 
-  // The first node is the root of the graph.
-  private final List<Node> nodes;
-
-  public DependencyResponse(List<Node> nodes) {
-    this.nodes = nodes;
-  }
-
-  public List<Node> getNodes() {
+  @Override
+  public List<Node> nodes() {
     return ImmutableList.copyOf(nodes);
   }
 }
