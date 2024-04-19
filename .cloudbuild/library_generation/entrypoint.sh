7ce954879d07faaab980591cc58cece009d8b993
diegomarquezp <diegomarquezp@google.com>
2024-04-19 17:36:21 +0000

    remove unused entrypoint file

--- .cloudbuild/library_generation/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# wrapper script for the hermetic build cli entrypoint that sets environment
-# variables that can be read by any user calling the container
-set -ex
-export HOME=/home
-export MAVEN_HOME=/home/.m2
-python /src/cli/entry_point.py generate $@
-
