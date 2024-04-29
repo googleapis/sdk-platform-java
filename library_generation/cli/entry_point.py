aa84ec46805623ce6da00bf19d8e637674427714
Joe Wang <joewa@google.com>
2024-04-29 15:28:19 -0400

    chore: add a log in config checker

--- library_generation/cli/entry_point.py
+++ library_generation/cli/entry_point.py
@@ -161,6 +161,7 @@ def validate_generation_config(generation_config_path: str) -> None:
         generation_config_path = "generation_config.yaml"
     try:
         from_yaml(os.path.abspath(generation_config_path))
+        print(f"{generation_config_path} is validated without error.")
     except ValueError as err:
         print(err)
         sys.exit(1)
