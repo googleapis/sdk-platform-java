84654837c0760e6d37b510efaa77d757b2dd0279
Min Zhu <zhumin@google.com>
2024-05-06 06:03:50 +0000

    test: add test to grpcrest service client composer.

--- gapic-generator-java/src/test/java/com/google/api/generator/gapic/composer/grpcrest/ServiceClientClassComposerTest.java
+++ gapic-generator-java/src/test/java/com/google/api/generator/gapic/composer/grpcrest/ServiceClientClassComposerTest.java
@@ -23,48 +23,43 @@ import com.google.api.generator.test.framework.GoldenFileWriter;
 import com.google.api.generator.test.protoloader.GrpcRestTestProtoLoader;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.Collection;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
 
+@RunWith(Parameterized.class)
 public class ServiceClientClassComposerTest {
+  @Parameterized.Parameters
+  public static Collection<Object[]> data() {
+    return Arrays.asList(
+        new Object[][] {
+          {"EchoClient", GrpcRestTestProtoLoader.instance().parseShowcaseEcho(), 0},
+          {"EchoEmpty", GrpcRestTestProtoLoader.instance().parseShowcaseEcho(), 1},
+          {"WickedClient", GrpcRestTestProtoLoader.instance().parseShowcaseWicked(), 0},
+          {"ApiVersionClient", GrpcRestTestProtoLoader.instance().parseApiVersionTesting(), 0},
+        });
+  }
+
+  @Parameterized.Parameter public String name;
+
+  @Parameterized.Parameter(1)
+  public GapicContext context;
+
+  @Parameterized.Parameter(2)
+  public int serviceIndex;
+
   @Test
   public void generateServiceClasses() {
-    GapicContext context = GrpcRestTestProtoLoader.instance().parseShowcaseEcho();
-    Service echoProtoService = context.services().get(0);
+    Service echoProtoService = context.services().get(serviceIndex);
     GapicClass clazz = ServiceClientClassComposer.instance().generate(context, echoProtoService);
 
     JavaWriterVisitor visitor = new JavaWriterVisitor();
     clazz.classDefinition().accept(visitor);
-    GoldenFileWriter.saveCodegenToFile(this.getClass(), "EchoClient.golden", visitor.write());
+    GoldenFileWriter.saveCodegenToFile(this.getClass(), name + ".golden", visitor.write());
     Path goldenFilePath =
-        Paths.get(GoldenFileWriter.getGoldenDir(this.getClass()), "EchoClient.golden");
-    Assert.assertCodeEquals(goldenFilePath, visitor.write());
-  }
-
-  @Test
-  public void generateServiceClassesEmpty() {
-    GapicContext context = GrpcRestTestProtoLoader.instance().parseShowcaseEcho();
-    Service echoProtoService = context.services().get(1);
-    GapicClass clazz = ServiceClientClassComposer.instance().generate(context, echoProtoService);
-
-    JavaWriterVisitor visitor = new JavaWriterVisitor();
-    clazz.classDefinition().accept(visitor);
-    GoldenFileWriter.saveCodegenToFile(this.getClass(), "EchoEmpty.golden", visitor.write());
-    Path goldenFilePath =
-        Paths.get(GoldenFileWriter.getGoldenDir(this.getClass()), "EchoEmpty.golden");
-    Assert.assertCodeEquals(goldenFilePath, visitor.write());
-  }
-
-  @Test
-  public void generateServiceClassesWicked() {
-    GapicContext context = GrpcRestTestProtoLoader.instance().parseShowcaseWicked();
-    Service wickedProtoService = context.services().get(0);
-    GapicClass clazz = ServiceClientClassComposer.instance().generate(context, wickedProtoService);
-
-    JavaWriterVisitor visitor = new JavaWriterVisitor();
-    clazz.classDefinition().accept(visitor);
-    GoldenFileWriter.saveCodegenToFile(this.getClass(), "WickedClient.golden", visitor.write());
-    Path goldenFilePath =
-        Paths.get(GoldenFileWriter.getGoldenDir(this.getClass()), "WickedClient.golden");
+        Paths.get(GoldenFileWriter.getGoldenDir(this.getClass()), name + ".golden");
     Assert.assertCodeEquals(goldenFilePath, visitor.write());
   }
 }
