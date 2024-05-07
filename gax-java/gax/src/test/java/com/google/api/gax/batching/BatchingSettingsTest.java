7c8a644e4a63d4ed712a939f766bccbe073ca102
diegomarquezp <diegomarquezp@google.com>
2024-05-07 16:39:44 +0000

    add tests for BatchingSettingsTest

---
+++ gax-java/gax/src/test/java/com/google/api/gax/batching/BatchingSettingsTest.java
@@ -0,0 +1,20 @@
+package com.google.api.gax.batching;
+
+import static com.google.api.gax.util.TimeConversionTestUtils.testDurationMethod;
+
+import org.junit.Test;
+
+public class BatchingSettingsTest {
+
+  private final BatchingSettings.Builder SETTINGS_BUILDER = BatchingSettings.newBuilder();
+
+  @Test
+  public void testDelayThreshold() {
+    testDurationMethod(123l,
+        jt -> SETTINGS_BUILDER.setDelayThreshold(jt).build(),
+        tt -> SETTINGS_BUILDER.setDelayThreshold(tt).build(),
+        o -> o.getDelayThresholdDuration(),
+        o -> o.getDelayThreshold()
+    );
+  }
+}
