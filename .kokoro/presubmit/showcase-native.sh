93fbdbb18be410f3dfe5118fe9566e5a9035dad8
Burke Davison <burkedavison@google.com>
2024-05-07 14:28:34 -0400

    ci: parallelize downstream and showcase native testing

--- .kokoro/presubmit/showcase-native.sh
+++ .kokoro/presubmit/showcase-native.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# Copyright 2019 Google LLC
+# Copyright 2024 Google LLC
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -14,11 +14,6 @@
 # limitations under the License.
 
 set -eo pipefail
-
-if [ -z "${MODULES_UNDER_TEST}" ]; then
-  echo "MODULES_UNDER_TEST must be set to run downstream-build.sh"
-  exit 1
-fi
 
 ## Get the directory of the build script
 scriptDir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
@@ -40,16 +35,27 @@ mvn install --projects '!gapic-generator-java' \
 
 SHARED_DEPS_VERSION=$(parse_pom_version java-shared-dependencies)
 
-# Update the shared-dependencies version in google-cloud-jar-parent
-git clone "https://github.com/googleapis/google-cloud-java.git" --depth=1
-update_all_poms_dependency google-cloud-java google-cloud-shared-dependencies "$SHARED_DEPS_VERSION"
+# Run showcase integration tests in GraalVM
+pushd showcase/gapic-showcase
+SHOWCASE_VERSION=$(mvn help:evaluate -Dexpression=gapic-showcase.version -q -DforceStdout)
+popd
+# Start showcase server
+mkdir -p /usr/src/showcase
+curl \
+  --location https://github.com/googleapis/gapic-showcase/releases/download/v"${SHOWCASE_VERSION}"/gapic-showcase-"${SHOWCASE_VERSION}"-linux-amd64.tar.gz \
+  --output /usr/src/showcase/showcase-"${SHOWCASE_VERSION}"-linux-amd64.tar.gz
+pushd /usr/src/showcase/
+tar -xf showcase-*
+./gapic-showcase run &
+popd
 
-# Run the updated java-shared-dependencies BOM against google-cloud-java integration tests
-cd google-cloud-java
-source ./.kokoro/common.sh
-RETURN_CODE=0
-setup_application_credentials
-setup_cloud "$MODULES_UNDER_TEST"
-run_graalvm_tests "$MODULES_UNDER_TEST"
-# Exit must occur in google-cloud-java directory to correctly destroy IT resources
-exit "$RETURN_CODE"
+# Run showcase tests with `native` profile
+pushd showcase
+mvn test -Pnative,-showcase \
+  -Denforcer.skip=true \
+  -Dcheckstyle.skip \
+  -Dfmt.skip \
+  -ntp -B \
+  -Dorg.slf4j.simpleLogger.showDateTime=true \
+  -Dorg.slf4j.simpleLogger.dateTimeFormat=HH:mm:ss:SSS
+popd
