315952e0748543fdcc592b25b1e88057c15ff557
Joe Wang <joewa@google.com>
2024-05-22 18:45:37 -0400

    use junit-pioneer

--- gax-java/gax/src/test/java/com/google/api/gax/rpc/EndpointContextTest.java
+++ gax-java/gax/src/test/java/com/google/api/gax/rpc/EndpointContextTest.java
@@ -40,6 +40,7 @@ import io.grpc.Status;
 import java.io.IOException;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junitpioneer.jupiter.SetEnvironmentVariable;
 import org.mockito.Mockito;
 
 class EndpointContextTest {
@@ -339,6 +340,7 @@ class EndpointContextTest {
   // For this test running locally or in CI, check that the Env Var is set properly.
   // This test should only run when the maven profile `EnvVarTest` is enabled.
   @Test
+  @SetEnvironmentVariable(key = "GOOGLE_CLOUD_UNIVERSE_DOMAIN", value = "random.com")
   void endpointContextBuild_universeDomainEnvVarSet() throws IOException {
     String envVarUniverseDomain = "random.com";
     EndpointContext endpointContext =
@@ -354,9 +356,9 @@ class EndpointContextTest {
   // For this test running locally or in CI, check that the Env Var is set properly.
   // This test should only run when the maven profile `EnvVarTest` is enabled.
   @Test
+  @SetEnvironmentVariable(key = "GOOGLE_CLOUD_UNIVERSE_DOMAIN", value = "random.com")
   void endpointContextBuild_multipleUniverseDomainConfigurations_clientSettingsHasPriority()
       throws IOException {
-    // This test has `GOOGLE_CLOUD_UNIVERSE_DOMAIN` = `random.com`
     String clientSettingsUniverseDomain = "clientSettingsUniverseDomain.com";
     EndpointContext endpointContext =
         defaultEndpointContextBuilder
