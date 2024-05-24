ed779f6611875b8a6294e4f03c904b3046086a42
JoeWang1127 <joewa@google.com>
2024-05-24 20:47:51 +0000

    fix unit tests

--- library_generation/test/generate_pr_description_unit_tests.py
+++ library_generation/test/generate_pr_description_unit_tests.py
@@ -82,7 +82,6 @@ class GeneratePrDescriptionTest(unittest.TestCase):
                 gapic_generator_version="",
                 googleapis_commitish=new_commit_sha,
                 libraries_bom_version="",
-                template_excludes=[],
                 grpc_version="",
                 protoc_version="",
                 # use empty libraries to make sure no qualified commit between
