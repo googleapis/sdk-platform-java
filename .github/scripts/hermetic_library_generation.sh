0ff7c553c81821987c8478b060642bcb32337d1b
JoeWang1127 <joewa@google.com>
2024-06-14 22:13:06 +0000

    chore: test protoc 27.1

--- .github/scripts/hermetic_library_generation.sh
+++ .github/scripts/hermetic_library_generation.sh
@@ -104,9 +104,7 @@ docker run \
   -u "$(id -u):$(id -g)" \
   -v "$(pwd):${workspace_name}" \
   -v "$HOME"/.m2:/home/.m2 \
-  gcr.io/cloud-devrel-public-resources/java-library-generation:"${image_tag}" \
-  --baseline-generation-config-path="${workspace_name}/${baseline_generation_config}" \
-  --current-generation-config-path="${workspace_name}/${generation_config}"
+  gcr.io/cloud-devrel-public-resources/java-library-generation:"${image_tag}"
 
 # commit the change to the pull request.
 rm -rdf output googleapis "${baseline_generation_config}"
