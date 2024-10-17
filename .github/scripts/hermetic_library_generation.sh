758f0bc25530e4ae854b38e75f143e6e34111fcd
Joe Wang <joewa@google.com>
2024-10-17 12:15:44 -0400

    change entrypoint

--- .github/scripts/hermetic_library_generation.sh
+++ .github/scripts/hermetic_library_generation.sh
@@ -110,8 +110,9 @@ docker run \
   --rm \
   -u "$(id -u):$(id -g)" \
   -v "$(pwd):${workspace_name}" \
-  --entrypoint python /src/cli/generate_release_note.py generate \
+  --entrypoint python \
   gcr.io/cloud-devrel-public-resources/java-library-generation:"${image_tag}" \
+  /src/cli/generate_release_note.py generate \
   --baseline-generation-config-path="${workspace_name}/${baseline_generation_config}" \
   --current-generation-config-path="${workspace_name}/${generation_config}" \
   --repository-path="${workspace_name}"
