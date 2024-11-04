be1ff27216f61d66ffffcee5b465fb67bcb2b88b
diegomarquezp <diegomarquezp@google.com>
2024-11-04 22:14:41 +0000

    restore timestamp offset parser

--- java-core/google-cloud-core/src/main/java/com/google/cloud/Timestamp.java
+++ java-core/google-cloud-core/src/main/java/com/google/cloud/Timestamp.java
@@ -55,7 +55,7 @@ public final class Timestamp implements Comparable<Timestamp>, Serializable {
       new DateTimeFormatterBuilder()
           .appendOptional(DateTimeFormatter.ISO_LOCAL_DATE_TIME)
           .optionalStart()
-          .appendZoneOrOffsetId()
+          .appendOffsetId()
           .optionalEnd()
           .toFormatter()
           .withZone(ZoneOffset.UTC);
