b6417ae719dde3927fa59e1ad0179a0476091285
Min Zhu <zhumin@google.com>
2024-11-25 16:45:08 -0500

    implement conditional logging, use JUL when no binding. log formatting needs more work.

--- gax-java/gax-grpc/src/main/java/com/google/api/gax/grpc/GrpcLoggingInterceptor.java
+++ gax-java/gax-grpc/src/main/java/com/google/api/gax/grpc/GrpcLoggingInterceptor.java
@@ -2,14 +2,18 @@ package com.google.api.gax.grpc;
 
 import com.google.api.gax.logging.LoggingUtils;
 import com.google.gson.Gson;
+import com.google.gson.JsonObject;
 import io.grpc.*;
+import java.util.HashMap;
+import java.util.Map;
 import org.slf4j.Logger;
-import org.slf4j.MDC;
 
 public class GrpcLoggingInterceptor implements ClientInterceptor {
 
   private static final Logger logger = LoggingUtils.getLogger(GrpcLoggingInterceptor.class);
   private static final Gson gson = new Gson();
+  private JsonObject serviceAndRpc = new JsonObject();
+  private JsonObject requestLogData = new JsonObject();
 
   @Override
   public <ReqT, RespT> ClientCall<ReqT, RespT> interceptCall(
@@ -20,44 +24,69 @@ public class GrpcLoggingInterceptor implements ClientInterceptor {
 
       @Override
       public void start(Listener<RespT> responseListener, Metadata headers) {
-        if (LoggingUtils.isLoggingEnabled()) {
-          // Capture request details
-          String serviceName = method.getServiceName();
-          String methodName = method.getFullMethodName();
+        // if (LoggingUtils.isLoggingEnabled()) {
+        // Capture request details
+        String serviceName = method.getServiceName();
+        String methodName = method.getFullMethodName();
 
-          // Add request details to MDC// Example system
-          MDC.put("serviceName", serviceName);
-          MDC.put("rpcName", methodName);
-          // Capture and log headers
-          headers
-              .keys()
-              .forEach(
-                  key -> {
-                    Metadata.Key<String> metadataKey =
-                        Metadata.Key.of(key, Metadata.ASCII_STRING_MARSHALLER);
-                    String headerValue = headers.get(metadataKey);
-                    MDC.put("request.headers:" + key, headerValue);
-                  });
+        serviceAndRpc.addProperty("serviceName", serviceName);
+        serviceAndRpc.addProperty("rpcName", methodName);
 
-          logger.debug("Sending gRPC request");
-        }
+        JsonObject responseLogData = new JsonObject();
+        // Add request details to MDC// Example system
+        // MDC.put("serviceName", serviceName);
+        // MDC.put("rpcName", methodName);
+        // Capture and log headers
+        headers
+            .keys()
+            .forEach(
+                key -> {
+                  Metadata.Key<String> metadataKey =
+                      Metadata.Key.of(key, Metadata.ASCII_STRING_MARSHALLER);
+                  String headerValue = headers.get(metadataKey);
+                  // MDC.put("request.headers:" + key, headerValue);
+                  requestLogData.addProperty("request.headers:" + key, headerValue);
+                });
+
+        requestLogData.addProperty("message", "Sending gRPC request");
+        // logger.debug("Sending gRPC request");
+        // }
 
         super.start(
             new ForwardingClientCallListener.SimpleForwardingClientCallListener<RespT>(
                 responseListener) {
               @Override
               public void onMessage(RespT message) {
-                MDC.put("response.payload", gson.toJson(message));
-                logger.debug("Received gRPC response.");
 
+                responseLogData.addProperty("response.payload", gson.toJson(message));
                 super.onMessage(message);
               }
 
               @Override
               public void onClose(Status status, Metadata trailers) {
-                MDC.put("response.status", status.getCode().name());
-                logger.info("gRPC request finished with status: {}", status);
-                MDC.clear(); // Clear MDC after the request
+                responseLogData.addProperty("response.status", status.getCode().name());
+                responseLogData.addProperty(
+                    "message", String.format("gRPC request finished with status: %s", status));
+
+                // Create a JsonObject for response headers
+                JsonObject responseHeaders = new JsonObject();
+
+                // Access and add response headers to the JsonObject
+                trailers
+                    .keys()
+                    .forEach(
+                        key -> {
+                          Metadata.Key<String> metadataKey =
+                              Metadata.Key.of(key, Metadata.ASCII_STRING_MARSHALLER);
+                          String headerValue = trailers.get(metadataKey);
+                          responseHeaders.addProperty(key, headerValue);
+                        });
+                responseLogData.add("response.headers", responseHeaders);
+
+                logger.debug(
+                    gson.toJson(LoggingUtils.mergeJsonObject(serviceAndRpc, responseLogData)));
+                // logger.info("gRPC request finished with status: {}", status);
+                // MDC.clear(); // Clear MDC after the request
                 super.onClose(status, trailers);
               }
             },
@@ -66,7 +95,17 @@ public class GrpcLoggingInterceptor implements ClientInterceptor {
 
       @Override
       public void sendMessage(ReqT message) {
-        MDC.put("request.payload", gson.toJson(message));
+        // MDC.put("request.payload", gson.toJson(message));
+        requestLogData.addProperty("request.payload", gson.toJson(message));
+
+        Map<String, String> map = new HashMap<>();
+        serviceAndRpc
+            .entrySet()
+            .forEach(entry -> map.put(entry.getKey(), entry.getValue().getAsString()));
+        // MDC.setContextMap(map);
+        // logger.info(new MapMessage(map));
+        logger.info(gson.toJson(LoggingUtils.mergeJsonObject(serviceAndRpc, requestLogData)));
+        // MDC.clear();
         super.sendMessage(message);
       }
     };
--- gax-java/gax/src/main/java/com/google/api/gax/logging/LoggingUtils.java
+++ gax-java/gax/src/main/java/com/google/api/gax/logging/LoggingUtils.java
@@ -1,48 +1,353 @@
 package com.google.api.gax.logging;
 
-import java.util.Map;
+import com.google.gson.JsonObject;
+import java.util.logging.Level;
+import org.slf4j.ILoggerFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.slf4j.MDC;
-import org.slf4j.event.Level;
+import org.slf4j.Marker;
+import org.slf4j.helpers.FormattingTuple;
+import org.slf4j.helpers.MessageFormatter;
 
 public class LoggingUtils {
 
+  private static final java.util.logging.Logger LOGGER =
+      java.util.logging.Logger.getLogger(LoggingUtils.class.getName());
+
+  private LoggingUtils() {}
+
   public static Logger getLogger(Class<?> clazz) {
-    return LoggerFactory.getLogger(clazz);
+
+    Logger logger;
+
+    if (isLoggingEnabled()) {
+
+      ILoggerFactory loggerFactory = LoggerFactory.getILoggerFactory();
+      if (loggerFactory != null && !(loggerFactory instanceof org.slf4j.helpers.NOPLoggerFactory)) {
+        // An SLF4j binding is present
+        // You can get the logger and use it:
+        logger = LoggerFactory.getLogger(clazz);
+        logger.debug("SLF4J BINDING FOUND!!!!!");
+        // ...
+      } else {
+        // No SLF4j binding found
+        // Implement your fallback logic here
+        logger = new JulWrapperLogger(clazz.getName());
+        logger.info("No SLF4J providers were found, fall back to JUL.");
+      }
+    } else {
+      //  use SLF4j's NOP logger regardless of bindings
+      logger = LoggerFactory.getLogger(org.slf4j.Logger.ROOT_LOGGER_NAME);
+    }
+    return logger;
   }
 
   public static boolean isLoggingEnabled() {
     String enableLogging = System.getenv("GOOGLE_SDK_JAVA_LOGGING");
+    LOGGER.info("GOOGLE_SDK_JAVA_LOGGING=" + enableLogging);
     return "true".equalsIgnoreCase(enableLogging);
   }
 
-  public static void log(Logger logger, Level level, String msg, Map<String, String> contextMap) {
+  public static JsonObject mergeJsonObject(JsonObject jsonObject1, JsonObject jsonObject2) {
+    JsonObject mergedObject = jsonObject1.deepCopy();
+    jsonObject2.entrySet().forEach(entry -> mergedObject.add(entry.getKey(), entry.getValue()));
+    return mergedObject;
+  }
+  // JulWrapperLogger implementation
+  static class JulWrapperLogger implements Logger {
 
-    if (LoggingUtils.isLoggingEnabled()) {
-      MDC.setContextMap(contextMap);
-      switch (level) {
-        case TRACE:
-          logger.trace(msg);
-          break;
-        case DEBUG:
-          logger.debug(msg);
-          break;
-        case INFO:
-          logger.info(msg);
-          break;
-        case WARN:
-          logger.warn(msg);
-          break;
-        case ERROR:
-          logger.error(msg);
-          break;
-        default:
-          logger.info(msg);
-          // Default to INFO level if level is invalid
-          break;
-      }
-      MDC.clear();
+    private final java.util.logging.Logger julLogger;
+
+    public JulWrapperLogger(String name) {
+      this.julLogger = java.util.logging.Logger.getLogger(name);
     }
+
+    @Override
+    public String getName() {
+      return julLogger.getName();
+    }
+
+    @Override
+    public boolean isTraceEnabled() {
+      return julLogger.isLoggable(java.util.logging.Level.FINEST);
+    }
+
+    @Override
+    public void trace(String msg) {
+      julLogger.log(java.util.logging.Level.FINEST, msg);
+    }
+
+    @Override
+    public void trace(String s, Object o) {}
+
+    @Override
+    public void trace(String s, Object o, Object o1) {}
+
+    @Override
+    public void trace(String s, Object... objects) {}
+
+    @Override
+    public void trace(String s, Throwable throwable) {}
+
+    @Override
+    public boolean isTraceEnabled(Marker marker) {
+      return false;
+    }
+
+    @Override
+    public void trace(Marker marker, String s) {}
+
+    @Override
+    public void trace(Marker marker, String s, Object o) {}
+
+    @Override
+    public void trace(Marker marker, String s, Object o, Object o1) {}
+
+    @Override
+    public void trace(Marker marker, String s, Object... objects) {}
+
+    @Override
+    public void trace(Marker marker, String s, Throwable throwable) {}
+
+    @Override
+    public boolean isDebugEnabled() {
+      return julLogger.isLoggable(Level.FINE);
+    }
+
+    @Override
+    public void debug(String msg) {
+
+      if (isDebugEnabled()) {
+        julLogger.log(java.util.logging.Level.FINE, msg);
+      }
+    }
+
+    @Override
+    public void debug(String format, Object arg) {
+      if (isDebugEnabled()) {
+        FormattingTuple ft = MessageFormatter.format(format, arg);
+        julLogger.log(Level.FINE, ft.getMessage());
+      }
+    }
+
+    @Override
+    public void debug(String s, Object o, Object o1) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void debug(String s, Object... objects) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void debug(String s, Throwable throwable) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public boolean isDebugEnabled(Marker marker) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void debug(Marker marker, String s) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void debug(Marker marker, String s, Object o) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void debug(Marker marker, String s, Object o, Object o1) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void debug(Marker marker, String s, Object... objects) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void debug(Marker marker, String s, Throwable throwable) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public boolean isInfoEnabled() {
+      return julLogger.isLoggable(Level.INFO);
+    }
+
+    @Override
+    public void info(String msg) {
+      if (isInfoEnabled()) {
+        julLogger.log(java.util.logging.Level.INFO, msg);
+      }
+    }
+
+    @Override
+    public void info(String format, Object arg) {
+      if (isInfoEnabled()) {
+        FormattingTuple ft = MessageFormatter.format(format, arg);
+        julLogger.log(java.util.logging.Level.INFO, ft.getMessage());
+      }
+    }
+
+    @Override
+    public void info(String s, Object o, Object o1) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void info(String s, Object... objects) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void info(String s, Throwable throwable) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public boolean isInfoEnabled(Marker marker) {
+      return true;
+    }
+
+    @Override
+    public void info(Marker marker, String s) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void info(Marker marker, String s, Object o) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void info(Marker marker, String s, Object o, Object o1) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void info(Marker marker, String s, Object... objects) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void info(Marker marker, String s, Throwable throwable) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public boolean isWarnEnabled() {
+      return true;
+    }
+
+    @Override
+    public void warn(String msg) {
+      julLogger.log(Level.WARNING, msg);
+    }
+
+    @Override
+    public void warn(String s, Object o) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void warn(String s, Object... objects) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void warn(String s, Object o, Object o1) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void warn(String s, Throwable throwable) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public boolean isWarnEnabled(Marker marker) {
+      return false;
+    }
+
+    @Override
+    public void warn(Marker marker, String s) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void warn(Marker marker, String s, Object o) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void warn(Marker marker, String s, Object o, Object o1) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void warn(Marker marker, String s, Object... objects) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void warn(Marker marker, String s, Throwable throwable) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public boolean isErrorEnabled() {
+      return false;
+    }
+
+    @Override
+    public void error(String s) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void error(String s, Object o) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void error(String s, Object o, Object o1) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void error(String s, Object... objects) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public void error(String s, Throwable throwable) {
+      throw new UnsupportedOperationException("This method is not supported.");
+    }
+
+    @Override
+    public boolean isErrorEnabled(Marker marker) {
+      return false;
+    }
+
+    @Override
+    public void error(Marker marker, String s) {}
+
+    @Override
+    public void error(Marker marker, String s, Object o) {}
+
+    @Override
+    public void error(Marker marker, String s, Object o, Object o1) {}
+
+    @Override
+    public void error(Marker marker, String s, Object... objects) {}
+
+    @Override
+    public void error(Marker marker, String s, Throwable throwable) {}
   }
 }
