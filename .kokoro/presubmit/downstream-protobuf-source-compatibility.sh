6ca58dc879037660e906db181f7af15da1a78586
Lawrence Qiu <lawrenceqiu@google.com>
2024-12-05 12:07:22 -0500

    ci: Add Downstream Protobuf-Java Source Compatibility Test

---
+++ .kokoro/presubmit/downstream-protobuf-source-compatibility.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# Copyright 2023 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -eo pipefail
+
+# Comma-delimited list of repos to test with the local java-shared-dependencies
+if [ -z "${REPOS_UNDER_TEST}" ]; then
+  echo "REPOS_UNDER_TEST must be set to run downstream-protobuf-source-compatibility.sh"
+  exit 1
+fi
+
+# Version of Protobuf-Java runtime to compile with
+if [ -z "${PROTOBUF_RUNTIME_VERSION}" ]; then
+  echo "PROTOBUF_RUNTIME_VERSION must be set to run downstream-protobuf-source-compatibility.sh"
+  exit 1
+fi
+
+# Get the directory of the build script
+scriptDir=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+cd "${scriptDir}/../.." # cd to the root of this repo
+source "$scriptDir/common.sh"
+setup_maven_mirror
+
+for repo in ${REPOS_UNDER_TEST//,/ }; do # Split on comma
+  # Perform source-compatibility testing on main (latest changes)
+  git clone "https://github.com/googleapis/$repo.git" --depth=1
+  pushd "$repo"
+
+  # Compile the Handwritten Library with the Protobuf-Java version to test source compatibility
+  # Run unit tests to help check for any behavior differences (dependant on coverage)
+  mvn clean test -B -V -ntp \
+      -Dclirr.skip=true \
+      -Denforcer.skip=true \
+      -Dmaven.javadoc.skip=true \
+      -Dprotobuf.version=${PROTOBUF_RUNTIME_VERSION} \
+      -T 1C
+  popd
+done
