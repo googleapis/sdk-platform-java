3a839bf802e9bd1b7398e4d2ed4202e29211a38f
diegomarquezp <diegomarquezp@google.com>
2025-01-21 22:50:11 +0000

    script cleanup, use function to download showcase

--- java-showcase/scripts/generate_showcase.sh
+++ java-showcase/scripts/generate_showcase.sh
@@ -3,8 +3,48 @@
 # from the current state of the repo in order to test local changes.
 set -ex
 
-readonly ROOT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../../"
+trap cleanup ERR
+
+readonly ROOT_DIR="$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )/../.."
 pushd "${ROOT_DIR}"
+
+get_version_from_pom() {
+  target_pom="$1"
+  key="$2"
+  # prints the result to stdout
+  grep -e "<${key}>" "${target_pom}" | cut -d'>' -f2 | cut -d'<' -f1
+}
+
+append_showcase_to_api_defs() {
+  api_def_dir="$1"
+  # append showcase definitions to googleapis repository
+  export showcase_def_dir=$(mktemp -d)
+  git clone https://github.com/googleapis/gapic-showcase.git "${showcase_def_dir}"
+  # looks at sdk-platform-java/java-showcase/gapic-showcase/pom.xml to extract the
+  # version of gapic-showcase
+  # see https://github.com/googleapis/gapic-showcase/releases
+  showcase_version=$(get_version_from_pom \
+    "${ROOT_DIR}/java-showcase/gapic-showcase/pom.xml" "gapic-showcase.version"
+  )
+  # compliance_suite.json is a symbolic link outside of the schema folder, so we
+  # replace it with the actual contents in its original location.
+  compliance_suite_path="${showcase_def_dir}/schema/google/showcase/v1beta1/compliance_suite.json"
+  unlink "${compliance_suite_path}"
+  cp "${showcase_def_dir}/server/services/compliance_suite.json" "${compliance_suite_path}"
+  # we complete the BUILD.bazel in gapic-showcase with our java_library in order
+  # to generate the gapic portion.
+  cat "${ROOT_DIR}/java-showcase/scripts/resources/BUILD.partial.bazel" >> "${showcase_def_dir}/schema/google/showcase/v1beta1/BUILD.bazel"
+  cp -r "${showcase_def_dir}/schema" "${api_def_dir}/"
+}
+
+cleanup() {
+  if [[ -z "${api_def_dir}" ]]; then
+    rm -rf "${api_def_dir}"
+  fi
+  if [[ -z "${showcase_def_dir}" ]]; then
+    rm -rf "${showcase_def_dir}"
+  fi
+}
 
 while [[ $# -gt 0 ]]; do
 key="$1"
@@ -25,6 +65,7 @@ if [ -z "${replace}" ]; then
   replace="false"
 fi
 
+
 # download api definitions from googleapis repository
 googleapis_commitish=$(grep googleapis_commitish generation_config.yaml | cut -d ":" -f 2 | xargs)
 api_def_dir=$(mktemp -d)
@@ -37,30 +78,7 @@ git checkout "${googleapis_commitish}"
 rm -rf ".git/"
 popd
 
-# append showcase definitions to googleapis repository
-showcase_def_dir=$(mktemp -d)
-git clone https://github.com/googleapis/gapic-showcase.git "${showcase_def_dir}"
-get_version_from_pom() {
-  target_pom="$1"
-  key="$2"
-  # prints the result to stdout
-  grep -e "<${key}>" "${target_pom}" | cut -d'>' -f2 | cut -d'<' -f1
-}
-# looks at sdk-platform-java/showcase/gapic-showcase/pom.xml to extract the
-# version of gapic-showcase
-# see https://github.com/googleapis/gapic-showcase/releases
-showcase_version=$(get_version_from_pom \
-  "showcase/gapic-showcase/pom.xml" "gapic-showcase.version"
-)
-# compliance_suite.json is a symbolic link outside of the schema folder, so we
-# replace it with the actual contents in its original location.
-compliance_suite_path="${showcase_def_dir}/schema/google/showcase/v1beta1/compliance_suite.json"
-unlink "${compliance_suite_path}"
-cp "${showcase_def_dir}/server/services/compliance_suite.json" "${compliance_suite_path}"
-# we complete the BUILD.bazel in gapic-showcase with our java_library in order
-# to generate the gapic portion.
-cat showcase/scripts/resources/BUILD.partial.bazel >> "${showcase_def_dir}/schema/google/showcase/v1beta1/BUILD.bazel"
-cp -r "${showcase_def_dir}/schema" "${api_def_dir}/"
+append_showcase_to_api_defs "${api_def_dir}"
 
 echo "building docker image"
 DOCKER_BUILDKIT=1 docker build --file .cloudbuild/library_generation/library_generation.Dockerfile --iidfile image-id .
@@ -68,13 +86,15 @@ DOCKER_BUILDKIT=1 docker build --file .cloudbuild/library_generation/library_gen
 if [[ "${replace}" == "true" ]]; then
   generated_files_dir="${ROOT_DIR}"
 else
-  generated_files_dir=$(mktemp -d)
+  export generated_files_dir=$(mktemp -d)
   # here we store the generated library location for upstream scripts to use
   # it.
-  echo "${generated_files_dir}/showcase" > "${ROOT_DIR}/generated-showcase-location"
+  echo "${generated_files_dir}/java-showcase" > "${ROOT_DIR}/generated-showcase-location"
   # we prepare the temp folder with the minimal setup to perform an incremental
   # generation.
-  cp -r generation_config.yaml showcase/ versions.txt "${generated_files_dir}"
+  pushd "${ROOT_DIR}"
+  cp -r generation_config.yaml java-showcase/ versions.txt "${generated_files_dir}"
+  popd #ROOT_DIR
 fi
 
 GENERATOR_VERSION=$(mvn help:evaluate -Dexpression=project.version -q -DforceStdout -pl gapic-generator-java)
--- java-showcase/scripts/showcase_utilities.sh
+++ java-showcase/scripts/showcase_utilities.sh
@@ -3,5 +3,6 @@
 function cleanup {
   script_dir=$1
   cd "${script_dir}"
-  rm -rdf output gapic-generator-java*
+  rm -rf output gapic-generator-java*
+  rm -rf "${script_dir}/../../generated-showcase-location"
 }
