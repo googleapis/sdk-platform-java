dbe30517844478de836bb6fc368b639365127887
Lawrence Qiu <lawrenceqiu@google.com>
2025-02-24 15:59:19 -0500

    chore: Clean up script

--- .kokoro/nightly/downstream-protobuf-binary-compatibility.sh
+++ .kokoro/nightly/downstream-protobuf-binary-compatibility.sh
@@ -29,16 +29,17 @@ if [ -z "${PROTOBUF_RUNTIME_VERSION}" ]; then
   exit 1
 fi
 
-# Create a Map of Non-Cloud APIs (Key: Maven Artifact ID Prefix, Value: Maven Group ID)
-declare -A nonCloudAPIs
-nonCloudAPIs["grafeas"]="com.google.cloud"
-nonCloudAPIs["google-maps"]="com.google.maps"
-nonCloudAPIs["google-shopping"]="com.google.shopping"
+# Create a Map of possible API names (Key: Maven Artifact ID Prefix, Value: Maven Group ID)
+declare -A api_maven_map
+api_maven_map["google-cloud"]="com.google.cloud"
+api_maven_map["grafeas"]="io.grafeas"
+api_maven_map["google-maps"]="com.google.maps"
+api_maven_map["google-shopping"]="com.google.shopping"
 
 # cloud-opensource-java contains the Linkage Checker tool
 git clone https://github.com/GoogleCloudPlatform/cloud-opensource-java.git
 pushd cloud-opensource-java
-mvn -B -ntp clean compile
+mvn -B -ntp clean compile -T 1C
 # Linkage Checker tool resides in the /dependencies subfolder
 pushd dependencies
 
@@ -47,38 +48,32 @@ for repo in ${REPOS_UNDER_TEST//,/ }; do # Split on comma
   git clone "https://github.com/googleapis/$repo.git" --depth=1
   pushd "$repo"
   # Install all modules to ~/.m2 (there can be multiple relevant artifacts to test i.e. core, admin, control)
-  mvn -B -ntp clean install -T 1C -DskipTests -Dclirr.skip -Denforcer.skip
+  mvn -B -ntp install -T 1C -DskipTests -Dclirr.skip -Denforcer.skip
 
-  ARTIFACT_LIST=""
-  # Match all artifacts that start with google-cloud to exclude any proto and grpc modules.
-  # Additionally, exclude any matches to BOM artifacts or emulators
-  # The artifact list will look like "com.google.cloud:google-cloud-accessapproval:2.60.0-SNAPSHOT,com.google.cloud:google-cloud-aiplatform:3.60.0-SNAPSHOT,"
-  CLOUD_ARTIFACT_LIST=$(cat "versions.txt" | grep "^google-cloud" | grep -vE "(bom|emulator|google-cloud-java)" | awk -F: "{\$1=\"com.google.cloud:\"\$1; \$2=\"\"; print}" OFS=: | sed 's/::/:/' | tr '\n' ',')
-  # Remove the trailing comma after the last entry
-  CLOUD_ARTIFACT_LIST=${CLOUD_ARTIFACT_LIST%,}
-  if [ -n "${CLOUD_ARTIFACT_LIST}" ]; then
-    ARTIFACT_LIST="${ARTIFACT_LIST},${CLOUD_ARTIFACT_LIST}"
-  fi
+  artifact_list=""
+  for artifact_id_prefix in "${!api_maven_map[@]}"; do
+    group_id="${api_maven_map[${artifact_id_prefix}]}"
 
-  # This logic is only for google-cloud-java as there are non-cloud APIs included
-  if [ "${repo}" == "google-cloud-java" ]; then
-    for key in "${!nonCloudAPIs[@]}"; do
-      value="${nonCloudAPIs[$key]}"
-      NON_CLOUD_ARTIFACT_LIST=$(cat "versions.txt" | grep "^${key}" | grep -vE "(bom|emulator|google-cloud-java)" | awk -F: "{\$1=\"${value}:\"\$1; \$2=\"\"; print}" OFS=: | sed 's/::/:/' | tr '\n' ',')
-      NON_CLOUD_ARTIFACT_LIST=${NON_CLOUD_ARTIFACT_LIST%,}
-      if [ -n "${NON_CLOUD_ARTIFACT_LIST}" ]; then
-        ARTIFACT_LIST="${ARTIFACT_LIST},${NON_CLOUD_ARTIFACT_LIST}"
-      fi
-    done
-  fi
+    # Match all artifacts that start with google-cloud to exclude any proto and grpc modules.
+    # Additionally, exclude any matches to BOM artifacts or emulators
+    # The artifact list will look like "com.google.cloud:google-cloud-accessapproval:2.60.0-SNAPSHOT,com.google.cloud:google-cloud-aiplatform:3.60.0-SNAPSHOT,"
+    repo_artifact_list=$(cat "versions.txt" | grep "^${artifact_id_prefix}" | grep -vE "(bom|emulator|google-cloud-java)" | awk -F: "{\$1=\"${group_id}:\"\$1; \$2=\"\"; print}" OFS=: | sed 's/::/:/' | tr '\n' ',')
+
+    # Only google-cloud-java has non-cloud APIs. Only add if there are artifacts to be added
+    if [ -n "${repo_artifact_list}" ]; then
+      # Remove the trailing comma after the last entry
+      repo_artifact_list=${repo_artifact_list%,}
+      artifact_list="${artifact_list},${repo_artifact_list}"
+    fi
+  done
 
   # Linkage Checker /dependencies
   popd
 
-  if [ -n "${ARTIFACT_LIST}" ]; then
-    echo "Found artifacts ${ARTIFACT_LIST}"
+  if [ -n "${artifact_list}" ]; then
+    echo "Found artifacts ${artifact_list}"
     # The `-s` argument filters the linkage check problems that stem from the artifact
-    program_args="-r --artifacts ${ARTIFACT_LIST},com.google.protobuf:protobuf-java:${PROTOBUF_RUNTIME_VERSION},com.google.protobuf:protobuf-java-util:${PROTOBUF_RUNTIME_VERSION} -s ${ARTIFACT_LIST}"
+    program_args="-r --artifacts ${artifact_list},com.google.protobuf:protobuf-java:${PROTOBUF_RUNTIME_VERSION},com.google.protobuf:protobuf-java-util:${PROTOBUF_RUNTIME_VERSION} -s ${artifact_list}"
     echo "Linkage Checker Program Arguments: ${program_args}"
     mvn -B -ntp exec:java -Dexec.mainClass="com.google.cloud.tools.opensource.classpath.LinkageCheckerMain" -Dexec.args="${program_args}"
   fi
