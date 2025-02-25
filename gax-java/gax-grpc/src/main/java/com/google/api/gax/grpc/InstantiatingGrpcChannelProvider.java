c433ce9a61425acb546ed7f378f9f77109ddccc3
Luwei Ge <lwge@google.com>
2025-02-25 22:17:17 +0000

    allow no credentials in normal TLS

--- gax-java/gax-grpc/src/main/java/com/google/api/gax/grpc/InstantiatingGrpcChannelProvider.java
+++ gax-java/gax-grpc/src/main/java/com/google/api/gax/grpc/InstantiatingGrpcChannelProvider.java
@@ -676,9 +676,14 @@ public final class InstantiatingGrpcChannelProvider implements TransportChannelP
           builder = Grpc.newChannelBuilder(endpoint, channelCredentials);
         } else {
           // Use default TLS credentials if we cannot initialize channel credentials via DCA or S2A.
-          channelCredentials =
-              CompositeChannelCredentials.create(
-                  TlsChannelCredentials.create(), MoreCallCredentials.from(credentials));
+          // This is the only case we allow the credentials to be null, although it should always
+          // be supplied in general.
+          channelCredentials = TlsChannelCredentials.create();
+          if (credentials != null) {
+            channelCredentials =
+                CompositeChannelCredentials.create(
+                    channelCredentials, MoreCallCredentials.from(credentials));
+          }
           builder = Grpc.newChannelBuilderForAddress(serviceAddress, port, channelCredentials);
         }
       }
