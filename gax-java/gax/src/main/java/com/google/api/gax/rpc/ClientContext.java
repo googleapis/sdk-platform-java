59f3bf7e2bbfbc08a6fe61d76463c1cd06b14c04
Riya Mehta <riyamehta@google.com>
2025-03-03 16:05:06 -0800

    move withUseS2A above needsMtlsEndpoint check.

--- gax-java/gax/src/main/java/com/google/api/gax/rpc/ClientContext.java
+++ gax-java/gax/src/main/java/com/google/api/gax/rpc/ClientContext.java
@@ -222,11 +222,11 @@ public abstract class ClientContext {
     if (transportChannelProvider.needsEndpoint()) {
       transportChannelProvider = transportChannelProvider.withEndpoint(endpoint);
     }
+    transportChannelProvider = transportChannelProvider.withUseS2A(endpointContext.useS2A());
     if (transportChannelProvider.needsMtlsEndpoint()) {
       transportChannelProvider =
           transportChannelProvider.withMtlsEndpoint(endpointContext.mtlsEndpoint());
     }
-    transportChannelProvider = transportChannelProvider.withUseS2A(endpointContext.useS2A());
     TransportChannel transportChannel = transportChannelProvider.getTransportChannel();
 
     ApiCallContext defaultCallContext =
