ea6076572a3d0dc4b0a4a24579afdf46758160e5
Lawrence Qiu <lawrenceqiu@google.com>
2025-03-07 17:04:54 -0500

    ci: Speed up Source Compatibility checks by running specific modules

--- .kokoro/nightly/downstream-protobuf-source-compatibility.sh
+++ .kokoro/nightly/downstream-protobuf-source-compatibility.sh
@@ -36,11 +36,26 @@ for repo in ${REPOS_UNDER_TEST//,/ }; do # Split on comma
 
   # Compile the Handwritten Library with the Protobuf-Java version to test source compatibility
   # Run unit tests to help check for any behavior differences (dependant on coverage)
-  mvn clean test -B -V -ntp \
+  if [ "${repo}" == "google-cloud-java" ]; then
+    # For google-cloud-java, only test specific handwritten libraries
+    mvn clean test -B -V -ntp \
+      -Dclirr.skip=true \
+      -Denforcer.skip=true \
+      -Dmaven.javadoc.skip=true \
+      -Dprotobuf.version=${PROTOBUF_RUNTIME_VERSION} \
+      # -am command also builds anything these libraries depend on (i.e. proto-* and grpc-* sub modules)
+      # Specify the nested `google-cloud-*` path (except for grafeas as it doesn't have one) because maven
+      # will only build the parent folder and ignore all the sub-modules inside
+      -pl java-grafeas,java-vertexai/google-cloud-vertexai,java-resourcemanager/google-cloud-resourcemanager -am \
+      -T 1C
+  else
+    mvn clean test -B -V -ntp \
       -Dclirr.skip=true \
       -Denforcer.skip=true \
       -Dmaven.javadoc.skip=true \
       -Dprotobuf.version=${PROTOBUF_RUNTIME_VERSION} \
       -T 1C
+  fi
+
   popd
 done
