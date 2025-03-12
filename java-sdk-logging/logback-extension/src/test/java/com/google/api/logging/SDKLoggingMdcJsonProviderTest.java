4181927a17ce935fca5ba5aa68af2247a51e97a4
Joe Wang <joewa@google.com>
2025-03-12 16:13:21 -0400

    add formatter

--- java-sdk-logging/logback-extension/src/test/java/com/google/api/logging/SDKLoggingMdcJsonProviderTest.java
+++ java-sdk-logging/logback-extension/src/test/java/com/google/api/logging/SDKLoggingMdcJsonProviderTest.java
@@ -10,21 +10,16 @@ import com.fasterxml.jackson.core.JsonGenerator;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class SDKLoggingMdcJsonProviderTest {
-  private SDKLoggingMdcJsonProvider provider = new SDKLoggingMdcJsonProvider();
 
-  private JsonGenerator generator = mock(JsonGenerator.class);
-
-  private ILoggingEvent event = mock(ILoggingEvent.class);
-
-  private Map<String, String> mdc;
-
-  @BeforeEach
-  public void setup() {
-    mdc = new HashMap<>();
+  @Test
+  void testWriteJsonFormat() throws IOException {
+    SDKLoggingMdcJsonProvider provider = new SDKLoggingMdcJsonProvider();
+    JsonGenerator generator = mock(JsonGenerator.class);
+    ILoggingEvent event = mock(ILoggingEvent.class);
+    Map<String, String> mdc = new HashMap<>();
     mdc.put("json1", "{\n"
         + "  \"@version\": \"1\",\n"
         + "  \"textPayload\": \"Received response\",\n"
@@ -34,13 +29,8 @@ public class SDKLoggingMdcJsonProviderTest {
         + "  }\n"
         + "}");
     when(event.getMDCPropertyMap()).thenReturn(mdc);
-  }
-
-  @Test
-  void testUnwrapped() throws IOException {
 
     provider.writeTo(generator, event);
-
     verify(generator).writeFieldName("json1");
     verify(generator).writeTree(any());
   }
