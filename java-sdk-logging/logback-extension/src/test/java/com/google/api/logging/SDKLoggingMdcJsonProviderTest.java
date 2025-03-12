b32e11b193f1dea2ffeda7b5e7ad8d81c0428d8a
Joe Wang <joewa@google.com>
2025-03-12 13:54:34 -0400

    add test

--- java-sdk-logging/logback-extension/src/test/java/com/google/api/logging/SDKLoggingMdcJsonProviderTest.java
+++ java-sdk-logging/logback-extension/src/test/java/com/google/api/logging/SDKLoggingMdcJsonProviderTest.java
@@ -1,31 +1,47 @@
 package com.google.api.logging;
 
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import ch.qos.logback.classic.spi.ILoggingEvent;
 import com.fasterxml.jackson.core.JsonGenerator;
-import java.util.LinkedHashMap;
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 import org.junit.jupiter.api.BeforeEach;
-import org.mockito.Mock;
+import org.junit.jupiter.api.Test;
 
 public class SDKLoggingMdcJsonProviderTest {
   private SDKLoggingMdcJsonProvider provider = new SDKLoggingMdcJsonProvider();
 
-  @Mock
-  private JsonGenerator generator;
+  private JsonGenerator generator = mock(JsonGenerator.class);
 
-  @Mock
-  private ILoggingEvent event;
+  private ILoggingEvent event = mock(ILoggingEvent.class);
 
   private Map<String, String> mdc;
 
   @BeforeEach
   public void setup() {
-    mdc = new LinkedHashMap<>();
-    mdc.put("name1", "value1");
-    mdc.put("name2", "value2");
-    mdc.put("name3", "value3");
+    mdc = new HashMap<>();
+    mdc.put("json1", "{\n"
+        + "  \"@version\": \"1\",\n"
+        + "  \"textPayload\": \"Received response\",\n"
+        + "  \"response.payload\": {\n"
+        + "    \"name\": \"example\",\n"
+        + "    \"state\": \"ACTIVE\"\n"
+        + "  }\n"
+        + "}");
     when(event.getMDCPropertyMap()).thenReturn(mdc);
+  }
+
+  @Test
+  void testUnwrapped() throws IOException {
+
+    provider.writeTo(generator, event);
+
+    verify(generator).writeFieldName("json1");
+    verify(generator).writeTree(any());
   }
 }
