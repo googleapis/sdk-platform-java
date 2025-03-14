710705b9eea7d826ebcde22fe9db2b4bb82f0aff
Joe Wang <joewa@google.com>
2025-03-14 14:55:01 -0400

    add a empty class for test prep

---
+++ java-showcase/gapic-showcase/src/test/java/com/google/showcase/v1beta1/it/logging/TestMdcAppender.java
@@ -0,0 +1,15 @@
+package com.google.showcase.v1beta1.it.logging;
+
+import ch.qos.logback.classic.spi.ILoggingEvent;
+import ch.qos.logback.core.UnsynchronizedAppenderBase;
+import com.google.api.logging.SDKLoggingMdcJsonProvider;
+
+public class TestMdcAppender extends UnsynchronizedAppenderBase<ILoggingEvent> {
+
+  private final SDKLoggingMdcJsonProvider provider = new SDKLoggingMdcJsonProvider();
+
+  @Override
+  protected void append(ILoggingEvent eventObject) {
+
+  }
+}
