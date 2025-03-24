5db2d927b9b1f48564ebca2ee4a51fb24b61fde8
Lawrence Qiu <lawrenceqiu@google.com>
2025-03-24 11:42:49 -0400

    ci: Compile with Java 11 and run tests with Java 8

--- .kokoro/nightly/downstream-protobuf-binary-compatibility.sh
+++ .kokoro/nightly/downstream-protobuf-binary-compatibility.sh
@@ -64,6 +64,7 @@ function build_program_arguments() {
 
 java -version
 
+# TODO(https://github.com/GoogleCloudPlatform/cloud-opensource-java/issues/2395): Java 17+ support for Linkage Checker
 # cloud-opensource-java contains the Linkage Checker tool
 git clone https://github.com/GoogleCloudPlatform/cloud-opensource-java.git
 pushd cloud-opensource-java
