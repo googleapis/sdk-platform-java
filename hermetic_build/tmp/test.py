f562de91bf74b618b98caa7ad7226537e973d84c
Min Zhu <zhumin@google.com>
2025-03-26 16:28:07 -0400

    add results for comparison run. rm test file.

--- hermetic_build/tmp/test.py
+++
@@ -1,7 +0,0 @@
-import filecmp
-
-try:
-    print(filecmp.ndiff(["line1", "line2"], ["line1", "line3"]))
-    print("filecmp.ndiff() exists!")
-except AttributeError:
-    print("filecmp.ndiff() does not exist.")
