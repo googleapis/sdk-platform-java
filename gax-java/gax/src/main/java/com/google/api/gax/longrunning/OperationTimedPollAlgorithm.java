8b4e6c0e49b7a2e838e707bfaad176db5d54e9e5
mpeddada1 <mpeddada@google.com>
2025-07-29 17:39:21 +0000

    fix: improve error messaging for LRO CancellationException

--- gax-java/gax/src/main/java/com/google/api/gax/longrunning/OperationTimedPollAlgorithm.java
+++ gax-java/gax/src/main/java/com/google/api/gax/longrunning/OperationTimedPollAlgorithm.java
@@ -91,7 +91,9 @@ public class OperationTimedPollAlgorithm extends ExponentialRetryAlgorithm {
     if (LOGGER.isLoggable(Level.WARNING)) {
       LOGGER.log(
           Level.WARNING,
-          "The task has been cancelled. Please refer to "
+          "The task has been cancelled. Possible reasons include a timeout or "
+              + "exceeding the maximum number of retry attempts."
+              + " Please refer to "
               + LRO_TROUBLESHOOTING_LINK
               + " for more information");
     }
