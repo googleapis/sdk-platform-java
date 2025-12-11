fd4cfca585d38504e0fbad9403a300c8f76661db
Noah Dietz <ndietz@google.com>
2025-12-11 19:26:15 +0000

    fix: add api_version breadcrumb to client docs

--- gapic-generator-java/src/main/java/com/google/api/generator/gapic/composer/comment/ServiceClientCommentComposer.java
+++ gapic-generator-java/src/main/java/com/google/api/generator/gapic/composer/comment/ServiceClientCommentComposer.java
@@ -108,9 +108,18 @@ public class ServiceClientCommentComposer {
       String secondaryTransport) {
     JavaDocComment.Builder classHeaderJavadocBuilder = JavaDocComment.builder();
     if (service.hasDescription()) {
+      String description = service.description();
+
+      // Include google.api.api_version breadcrumb comment.
+      if (service.hasApiVersion()) {
+        description =
+            String.format(
+                "%s\n\nThis client uses %s version %s.",
+                description, service.apiShortName(), service.apiVersion());
+      }
+
       String descriptionComment =
-          CommentFormatter.formatAsJavaDocComment(
-              service.description(), SERVICE_DESCRIPTION_SUMMARY_PATTERN);
+          CommentFormatter.formatAsJavaDocComment(description, SERVICE_DESCRIPTION_SUMMARY_PATTERN);
       classHeaderJavadocBuilder = classHeaderJavadocBuilder.addUnescapedComment(descriptionComment);
     }
 
