fb57471df0743974184d864ba19c597c47f40a32
Diego Marquez <diegomarquezp@google.com>
2026-02-13 18:32:50 -0500

    chore: adjust javadoc in TraceManager

--- gax-java/gax/src/main/java/com/google/api/gax/tracing/TraceManager.java
+++ gax-java/gax/src/main/java/com/google/api/gax/tracing/TraceManager.java
@@ -35,7 +35,7 @@ import com.google.api.core.InternalApi;
 import java.util.Map;
 
 /**
- * Provides an interface for tracing recording. The implementer is expected to use an observability
+ * Provides an interface for tracing management. The implementer is expected to use an observability
  * framework, e.g. OpenTelemetry. There should be only one instance of TraceManager per client.
  */
 @BetaApi
