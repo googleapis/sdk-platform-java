d9c4d8086e1f8ab186525d1aa97c2c0974d72008
Diego Marquez <diegomarquezp@google.com>
2026-03-02 15:01:39 -0500

    chore: fix license

--- gax-java/gax/src/main/java/com/google/api/gax/tracing/TracedClientStreamingCallable.java
+++ gax-java/gax/src/main/java/com/google/api/gax/tracing/TracedClientStreamingCallable.java
@@ -9,7 +9,7 @@
  * notice, this list of conditions and the following disclaimer.
  *     * Redistributions in binary form must reproduce the above
  * copyright notice, this list of conditions and the following disclaimer
- * in the documentation and/or intermediate materials provided with the
+ * in the documentation and/or other materials provided with the
  * distribution.
  *     * Neither the name of Google LLC nor the names of its
  * contributors may be used to endorse or promote products derived from
