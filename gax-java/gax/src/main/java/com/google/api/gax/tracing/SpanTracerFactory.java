6fbcc0825be87f622dfa2e4f1b61ca011ec658fb
Diego Marquez <diegomarquezp@google.com>
2026-03-02 11:52:10 -0500

    feat: SpanTracerFactory to implement newTracer with context

--- gax-java/gax/src/main/java/com/google/api/gax/tracing/SpanTracerFactory.java
+++ gax-java/gax/src/main/java/com/google/api/gax/tracing/SpanTracerFactory.java
@@ -76,6 +76,23 @@ public class SpanTracerFactory implements ApiTracerFactory {
   }
 
   @Override
+  public ApiTracer newTracer(ApiTracer parent, ApiTracerContext apiTracerContext, OperationType operationType) {
+    ApiTracerContext context = this.apiTracerContext.merge(apiTracerContext);
+
+    String attemptSpanName;
+    if (context.transport() == ApiTracerContext.Transport.GRPC) {
+      attemptSpanName = context.rpcMethod();
+    } else {
+      // TODO(diegomarquezp): this is a placeholder for the HTTP span name and will be adjusted as the
+      // feature is developed.
+      attemptSpanName = context.getClientName() + "/" + context.getMethodName() + "/attempt";
+    }
+
+    SpanTracer spanTracer = new SpanTracer(traceManager, context, attemptSpanName);
+    return spanTracer;
+  }
+
+  @Override
   public ApiTracerContext getApiTracerContext() {
     return apiTracerContext;
   }
