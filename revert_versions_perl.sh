9cdbb3778ed704f34e6d7bf0566b0949b7f90ed0
Diego <diegomarquezp@google.com>
2026-04-08 15:45:54 -0400

    chore: revert versions to snapshots using script

---
+++ revert_versions_perl.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+# revert_versions_perl.sh
+
+declare -A versions
+versions["gax"]="2.76.1-SNAPSHOT"
+versions["gax-grpc"]="2.76.1-SNAPSHOT"
+versions["api-common"]="2.59.1-SNAPSHOT"
+versions["proto-google-iam-v3"]="1.62.1-SNAPSHOT"
+versions["proto-google-iam-v3beta"]="1.62.1-SNAPSHOT"
+versions["grpc-google-iam-v3"]="1.62.1-SNAPSHOT"
+versions["grpc-google-iam-v3beta"]="1.62.1-SNAPSHOT"
+versions["google-cloud-dns"]="2.33.0-SNAPSHOT"
+versions["google-cloud-service-control"]="1.35.0-SNAPSHOT"
+versions["google-cloud-tasks"]="2.35.0-SNAPSHOT"
+versions["gapic-generator-java"]="2.68.1-SNAPSHOT"
+
+for feature in "${!versions[@]}"; do
+  version="${versions[$feature]}"
+  echo "Reverting $feature to $version"
+  find . -type f \( -name "pom.xml" -o -name "*.yaml" -o -name "*.Dockerfile" \) -exec perl -pi -e "s|<version>.*</version>\s*<!--\s*\{x-version-update:$feature:current\}\s*-->|<version>$version</version><!-- {x-version-update:$feature:current} -->|g" {} +
+done
+
+# Handle special cases for google-cloud-shared-dependencies
+echo "Handling special cases for google-cloud-shared-dependencies"
+find . -type f \( -name "*.yaml" -o -name "pom.xml" \) -exec perl -pi -e "s|_SHARED_DEPENDENCIES_VERSION: '.*' # \{x-version-update:google-cloud-shared-dependencies:current\}|_SHARED_DEPENDENCIES_VERSION: '3.58.1-SNAPSHOT' # {x-version-update:google-cloud-shared-dependencies:current}|g" {} +
+find . -type f \( -name "*.yaml" -o -name "pom.xml" \) -exec perl -pi -e "s|value: \"gcr.io/cloud-devrel-public-resources/graalvm_sdk_platform_(a\|b\|c):.*\" # \{x-version-update:google-cloud-shared-dependencies:current\}|value: \"gcr.io/cloud-devrel-public-resources/graalvm_sdk_platform_\$1:3.58.1-SNAPSHOT\" # {x-version-update:google-cloud-shared-dependencies:current}|g" {} +
+find . -type f -name "pom.xml" -exec perl -pi -e "s|<google-cloud-shared-dependencies.version>.*</google-cloud-shared-dependencies.version>\s*<!--\s*\{x-version-update:google-cloud-shared-dependencies:current\}\s*-->|<google-cloud-shared-dependencies.version>3.58.1-SNAPSHOT</google-cloud-shared-dependencies.version> <!-- {x-version-update:google-cloud-shared-dependencies:current} -->|g" {} +
+find . -type f -name "pom.xml" -exec perl -pi -e "s|<version>.*</version>\s*<!--\s*\{x-version-update:google-cloud-shared-dependencies:current\}\s*-->|<version>3.58.1-SNAPSHOT</version><!-- {x-version-update:google-cloud-shared-dependencies:current} -->|g" {} +
